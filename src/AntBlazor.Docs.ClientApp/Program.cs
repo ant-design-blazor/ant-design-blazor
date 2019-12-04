3e4ea9786c5761bb743dc78c8c9ba6394cd51810
ElderJames <shunjiey@hotmail.com>
2019-12-04 16:46:08 +0800

    add the base project architecture

---
+++ src/AntBlazor.Docs.ClientApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace AntBlazor.Docs.ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
