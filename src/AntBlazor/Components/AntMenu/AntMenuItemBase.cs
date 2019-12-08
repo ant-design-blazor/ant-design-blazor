d3a5b6bc758aef1993b347680a35c94499fa9232
ElderJames <shunjiey@hotmail.com>
2019-12-09 00:25:22 +0800

    fix menu

---
+++ src/AntBlazor/Components/AntMenu/AntMenuItemBase.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor
+{
+    public class AntMenuItemBase : AntDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool nzDisabled { get; set; } = false;
+
+        [Parameter]
+        public bool nzSelected { get; set; } = false;
+
+        [Parameter]
+        public int? nzPaddingLeft { get; set; }
+
+        [Parameter]
+        public bool nzMatchRouterExact { get; set; } = false;
+
+        [Parameter]
+        public bool nzMatchRouter { get; set; } = false;
+
+        [CascadingParameter]
+        public AntMenu Menu { get; set; }
+
+        [CascadingParameter]
+        public AntSubMenu SubMenu { get; set; }
+
+        public bool isInDropDown { get; set; }
+
+        private int originalPadding;
+
+        public AntMenuItemBase()
+        {
+        }
+
+        private void SetClassMap()
+        {
+            string prefixName = isInDropDown ? "ant-dropdown-menu-item" : "ant-menu-item";
+            ClassMapper.Add(prefixName)
+                .If($"{prefixName}-selected", () => nzSelected)
+                .If($"{prefixName}-disabled", () => nzDisabled);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            int? padding = null;
+            if (Menu.nzMode == NzDirectionVHIType.inline)
+            {
+                if (nzPaddingLeft != null)
+                {
+                    padding = nzPaddingLeft;
+                }
+                else
+                {
+                    int level = SubMenu?.Level + 1 ?? 1;
+                    padding = level * this.Menu.nzInlineIndent;
+                }
+            }
+            else
+            {
+                padding = originalPadding;
+            }
+
+            if (padding != null)
+            {
+                Style += $"padding-left:{padding}px;";
+            }
+
+            SetClassMap();
+        }
+    }
+}
---
+++ src/AntBlazor/Components/AntMenu/AntSubMenuBase.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor
+{
+    public class AntSubMenuBase : AntDomComponentBase
+    {
+        public int Level { get; set; } = 1;
+
+        [CascadingParameter]
+        protected AntMenu Menu { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string nzMenuClassName { get; set; }
+
+        [Parameter]
+        public int? nzPaddingLeft { get; set; }
+
+        [Parameter]
+        public string nzTitle { get; set; }
+
+        [Parameter]
+        public string nzIcon { get; set; }
+
+        [Parameter]
+        public bool nzOpen { get; set; } = false;
+
+        [Parameter]
+        public bool nzDisabled { get; set; } = false;
+
+        [Parameter]
+        public EventCallback<bool> nzOpenChange { get; set; }
+
+        protected ClassMapper TitleDivClass { get; } = new ClassMapper();
+
+        private string placement = "rightTop";
+        private int triggerWidth;
+        private string expandState = "collapsed";
+        private string[] overlayPositions = new[] { "" };
+        private bool isChildMenuSelected = false;
+        private bool isMouseHover = false;
+        private bool open;
+
+        private void SetClassMap()
+        {
+            string prefixName = Menu.isInDropDown ? "ant-dropdown-menu-submenu" : "ant-menu-submenu";
+            ClassMapper.Add(prefixName)
+                .If($"{prefixName}-disabled", () => nzDisabled)
+                .If($"{prefixName}-open", () => nzOpen)
+                .If($"{prefixName}-selected", () => isChildMenuSelected)
+                .Add($"{prefixName}-{Menu.nzMode}")
+                .If($"{prefixName}-active", () => isMouseHover && !nzDisabled)
+                ;
+        }
+
+        protected override void OnInitialized()
+        {
+            this.SetClassMap();
+            this.open = nzOpen;
+            base.OnInitializedAsync();
+        }
+
+        protected void setMouseEnterState(bool value)
+        {
+            this.isMouseHover = value;
+            this.SetClassMap();
+        }
+
+        protected void clickSubMenuTitle()
+        {
+            if (Menu.nzMode == NzDirectionVHIType.inline && !Menu.isInDropDown && !this.nzDisabled)
+            {
+                open = !this.nzOpen;
+            }
+        }
+    }
+}
