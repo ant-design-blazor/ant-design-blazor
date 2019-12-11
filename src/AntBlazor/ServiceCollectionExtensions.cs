3b5682b982d77d393396b91510f29b46f7f3b200
ElderJames <shunjiey@hotmail.com>
2019-12-11 17:17:58 +0800

    fix(icon): request path for remote files

---
+++ src/AntBlazor/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AntBlazor
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAntBlazor(this IServiceCollection services)
+        {
+            services.TryAddSingleton(new HttpClient());
+            return services;
+        }
+    }
+}
