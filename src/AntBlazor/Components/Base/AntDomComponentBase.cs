9e2a14c91c22ac26347cd82e101200f7c62d6338
ElderJames <shunjiey@hotmail.com>
2019-12-13 17:45:01 +0800

    fix: js interop for getting dom property values

--- src/AntBlazor/Components/Base/AntDomComponentBase.cs
+++ src/AntBlazor/Components/Base/AntDomComponentBase.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
 namespace AntBlazor
 {
