f471e8d756d8ba04abc97667f5909a1381de088e
ElderJames <shunjiey@hotmail.com>
2019-12-17 14:47:00 +0800

    refactor(icon): change parameter case

--- src/AntBlazor/Components/AntIcon/AntIconBase.cs
+++ src/AntBlazor/Components/AntIcon/AntIconBase.cs
@@ -10,31 +10,31 @@ namespace AntBlazor
     public class AntIconBase : AntDomComponentBase
     {
         [Parameter]
-        public bool Spin { get; set; }
+        public bool spin { get; set; }
 
         [Parameter]
-        public int Rotate { get; set; } = 0;
+        public int rotate { get; set; } = 0;
 
         [Parameter]
-        public string Type { get; set; }
+        public string type { get; set; }
 
         [Parameter]
-        public string Theme { get; set; } = AntIconThemeType.Outline; //'fill' | 'outline' | 'twotone';
+        public string theme { get; set; } = AntIconThemeType.Outline; //'fill' | 'outline' | 'twotone';
 
         [Parameter]
-        public string TwotoneColor { get; set; }
+        public string twotoneColor { get; set; }
 
         [Parameter]
-        public string IconFont { get; set; }
+        public string iconFont { get; set; }
 
         [Parameter]
-        public string Width { get; set; } = "1em";
+        public string width { get; set; } = "1em";
 
         [Parameter]
-        public string Height { get; set; } = "1em";
+        public string height { get; set; } = "1em";
 
         [Parameter]
-        public string Fill { get; set; } = "currentColor";
+        public string fill { get; set; } = "currentColor";
 
         [CascadingParameter]
         public AntButton Button { get; set; }
@@ -60,9 +60,9 @@ namespace AntBlazor
         {
             string prefixName = "anticon";
             ClassMapper.Add(prefixName)
-                .If($"{prefixName}-spin", () => Spin || this.Type == "loading");
+                .If($"{prefixName}-spin", () => spin || this.type == "loading");
 
-            SvgStyle = $"focusable=\"false\" width=\"{Width}\" height=\"{Height}\" fill=\"{Fill}\"";
+            SvgStyle = $"focusable=\"false\" width=\"{width}\" height=\"{height}\" fill=\"{fill}\"";
         }
 
         protected override async Task OnInitializedAsync()
@@ -85,14 +85,14 @@ namespace AntBlazor
 
         protected async Task SetupSvgImg()
         {
-            if (SvgCache.TryGetValue($"{Theme}-{Type}", out var svg))
+            if (SvgCache.TryGetValue($"{theme}-{type}", out var svg))
             {
                 _iconSvg = svg;
             }
             else
             {
-                _iconSvg = await httpClient.GetStringAsync(new Uri(baseUrl, $"_content/AntBlazor/icons/{Theme.ToLower()}/{Type.ToLower()}.svg"));
-                SvgCache.TryAdd($"{Theme}-{Type}", _iconSvg);
+                _iconSvg = await httpClient.GetStringAsync(new Uri(baseUrl, $"_content/AntBlazor/icons/{theme.ToLower()}/{type.ToLower()}.svg"));
+                SvgCache.TryAdd($"{theme}-{type}", _iconSvg);
             }
 
             SvgImg = _iconSvg.Insert(_iconSvg.IndexOf("svg", StringComparison.Ordinal) + 3, $" {SvgStyle} ");
