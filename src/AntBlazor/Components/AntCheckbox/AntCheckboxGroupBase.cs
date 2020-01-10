454c5f24dffe6897bd79e5e50178b6891861a453
ElderJames <shunjiey@hotmail.com>
2020-01-11 01:10:05 +0800

    feat(checkbox): add checkbox

---
+++ src/AntBlazor/Components/AntCheckbox/AntCheckboxGroupBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor
+{
+    public class AntCheckboxGroupBase : AntDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public Action<object> onChange;
+
+        public Action onTouched;
+
+        [Parameter]
+        public CheckBoxOption[] options { get; set; } = Array.Empty<CheckBoxOption>();
+
+        [Parameter]
+        public bool disabled { get; set; }
+
+        public AntCheckboxGroupBase()
+        {
+            ClassMapper.Add("ant-checkbox-group");
+        }
+
+        public void onOptionChange()
+        {
+            this.onChange(this.options);
+        }
+    }
+}
