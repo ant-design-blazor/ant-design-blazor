357ee221fcda8180d653ba6e82ad4f5710353ce1
rabbitism <popmessiah@hotmail.com>
2020-03-09 19:19:51 +0800

    <feat> Initialize Typography

---
+++ components/typography/AntTypographyBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    internal abstract class AntTypographyBase:AntComponentBase
+    {
+        [Parameter]
+        public bool copyable { get; set; } = false;
+
+
+    }
+}
---
+++ components/typography/TextElementType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor.typography
+{
+    public static class TextElementType
+    {
+        public const string Secondary = "secondary";
+    }
+}
