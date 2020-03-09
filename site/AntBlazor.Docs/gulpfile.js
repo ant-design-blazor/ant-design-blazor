65ed6faffec70f56a604fb253157a5132a3bdb55
ElderJames <shunjiey@hotmail.com>
2020-03-09 18:16:44 +0800

    chore: add commit lint & adjust build directory

---
+++ site/AntBlazor.Docs/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require("gulp"),
+  cleanCss = require("gulp-clean-css"),
+  less = require("gulp-less");
+
+gulp.task("default", function () {
+  return gulp.src('docs.less')
+    .pipe(less({ javascriptEnabled: true }))
+    .pipe(cleanCss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('wwwroot/css'));
+});
