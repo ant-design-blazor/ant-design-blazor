644731afc567c8505b96797258807d3d8d22be2a
rabbitism <popmessiah@hotmail.com>
2020-03-09 19:19:51 +0800

    <feat> Initialize Typography

---
+++ components/typography/AntTitleBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class AntTitleBase : AntDomComponentBase
+    {
+        [Parameter]
+        public int level { get; set; } = 1;
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            SetClassMap();
+        }
+
+        protected void SetClassMap()
+        {
+            ClassMapper.Clear()
+                .Add("ant-typography");
+            
+        }
+    }
+}
