e850ee3a1d329e275f11e6e4cfa9fc9569fe470b
rabbitism <popmessiah@hotmail.com>
2020-03-10 11:04:55 +0800

    feat(typography) Add AntTitle and AntText

--- components/typography/TextElementType.cs
+++ components/typography/TextElementType.cs
@@ -7,5 +7,7 @@ namespace AntBlazor.typography
     public static class TextElementType
     {
         public const string Secondary = "secondary";
+        public const string Warning = "warning";
+        public const string Danger = "danger";
     }
 }
