1a60a01016b8472123e985ce87ed18f92812ef19
Bin Dong <popmessiah@hotmail.com>
2020-03-10 13:29:03 +0800

    feat(typograhy): Add AntTitle and AntText, pending on edit and copy (#7)
    * <doc> Update Button documentation page
    
    Bin Dong
    
    * fix: ci
    
    * <feat> Initialize Typography
    
    * feat(typography) Add AntTitle and AntText
    
    * feat (typography): Add AntParagraph, Update documentation
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/typography/TextElementType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor.typography
+{
+    public static class TextElementType
+    {
+        public const string Secondary = "secondary";
+        public const string Warning = "warning";
+        public const string Danger = "danger";
+    }
+}
