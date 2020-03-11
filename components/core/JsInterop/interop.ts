aff015226092368bdc2d5daa51d5740f17e356b5
ElderJames <shunjiey@hotmail.com>
2020-03-11 16:49:25 +0800

    refactor: getDom funciton

--- components/core/JsInterop/interop.ts
+++ components/core/JsInterop/interop.ts
@@ -1,75 +1,75 @@
-﻿export function getDomInfo(element) {
-  var result = {};
+﻿export function getDom(element) {
+    if (!element) {
+        element = document.body;
+    } else if (typeof element === 'string') {
+        element = document.querySelector(element);
+    }
+    return element;
+}
 
-  if (!element) {
-    element = document.body;
-  } else if (typeof element === 'string') {
-    element = document.querySelector(element);
-  }
+export function getDomInfo(element) {
+    var result = {};
 
-  for (var key in element) {
-    var item = element[key];
-    if (!item) continue;
-    if (typeof item === 'string' || (typeof item === 'number' && !isNaN(item)))
-      result[key] = item;
-  }
+    var dom = getDom(element);
 
-  return result;
+    for (var key in dom) {
+        var item = dom[key];
+        if (!item) continue;
+        if (typeof item === 'string' || (typeof item === 'number' && !isNaN(item))) result[key] = item;
+    }
+
+    return result;
 }
 
 export function getBoundingClientRect(element) {
-  if (!element) {
-    element = document.body;
-  } else if (typeof element === 'string') {
-    element = document.querySelector(element);
-  }
-
-  return element.getBoundingClientRect();
+    let dom = getDom(element);
+    return dom.getBoundingClientRect();
 }
 
-export function addDomEventListener(dom, eventName, invoker) {
-  let callback = (args) => {
-    invoker.invokeMethodAsync('Invoke', args);
-  };
+export function addDomEventListener(element, eventName, invoker) {
+    let callback = args => {
+        invoker.invokeMethodAsync('Invoke', args);
+    };
 
-  if (dom == 'window') {
-    window.addEventListener(eventName, callback);
-  } else {
-    (document.querySelector(dom) as HTMLElement).addEventListener(eventName, callback);
-  }
+    if (element == 'window') {
+        window.addEventListener(eventName, callback);
+    } else {
+        let dom = getDom(element);
+        (document.querySelector(dom) as HTMLElement).addEventListener(eventName, callback);
+    }
 }
 
 export function antMatchMedia(query) {
-  return window.matchMedia(query).matches;
+    return window.matchMedia(query).matches;
 }
 
 export function copy(text): Promise<any> {
-  return new Promise<any>((resolve, reject) => {
-    let copyTextArea = null;
-    try {
-      copyTextArea = this.document.createElement('textarea');
-      copyTextArea.style.all = 'unset';
-      copyTextArea.style.position = 'fixed';
-      copyTextArea.style.top = '0';
-      copyTextArea.style.clip = 'rect(0, 0, 0, 0)';
-      copyTextArea.style.whiteSpace = 'pre';
-      copyTextArea.style.webkitUserSelect = 'text';
-      copyTextArea.style.MozUserSelect = 'text';
-      copyTextArea.style.msUserSelect = 'text';
-      copyTextArea.style.userSelect = 'text';
-      this.document.body.appendChild(copyTextArea);
-      copyTextArea.value = text;
-      copyTextArea.select();
+    return new Promise<any>((resolve, reject) => {
+        let copyTextArea = null;
+        try {
+            copyTextArea = this.document.createElement('textarea');
+            copyTextArea.style.all = 'unset';
+            copyTextArea.style.position = 'fixed';
+            copyTextArea.style.top = '0';
+            copyTextArea.style.clip = 'rect(0, 0, 0, 0)';
+            copyTextArea.style.whiteSpace = 'pre';
+            copyTextArea.style.webkitUserSelect = 'text';
+            copyTextArea.style.MozUserSelect = 'text';
+            copyTextArea.style.msUserSelect = 'text';
+            copyTextArea.style.userSelect = 'text';
+            this.document.body.appendChild(copyTextArea);
+            copyTextArea.value = text;
+            copyTextArea.select();
 
-      const successful = this.document.execCommand('copy');
-      if (!successful) {
-        reject(text);
-      }
-      resolve(text);
-    } finally {
-      if (copyTextArea) {
-        this.document.body.removeChild(copyTextArea);
-      }
-    }
-  });
+            const successful = this.document.execCommand('copy');
+            if (!successful) {
+                reject(text);
+            }
+            resolve(text);
+        } finally {
+            if (copyTextArea) {
+                this.document.body.removeChild(copyTextArea);
+            }
+        }
+    });
 }
