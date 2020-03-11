a409a7f6029f809d527be3ac52a41940f3f0cbf2
ElderJames <shunjiey@hotmail.com>
2020-03-11 17:15:15 +0800

    docs: add htmlRender & markdown component

---
+++ components/core/Internal/ComponentRenderedText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public readonly struct ComponentRenderedText
+    {
+        public ComponentRenderedText(int componentId, IEnumerable<string> tokens)
+        {
+            ComponentId = componentId;
+            Tokens = tokens;
+        }
+
+        public int ComponentId { get; }
+
+        public IEnumerable<string> Tokens { get; }
+    }
+}
