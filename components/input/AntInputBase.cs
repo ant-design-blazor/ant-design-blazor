f57b9dbbfb77466cf27743740ce1dd706503b425
Brian Ding <Brian_Ding@outlook.com>
2020-03-12 20:34:06 +0800

    add component input

---
+++ components/input/AntInputBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class AntInputBase : AntInputComponentBase<string>
+    {
+        [Parameter]
+        public string size { get; set; } = AntInputSize.Default;
+
+        protected override void OnInitialized()
+        {
+            Debug.WriteLine($"size: {size}");
+            base.OnInitialized();
+
+            //this.presetColor = this.isPresetColor(this.color);
+            string prefix = "ant-input";
+            this.ClassMapper.Clear()
+                .If($"{prefix}-lg", () => size == AntInputSize.Large)
+                .If($"{prefix}-sm", () => size == AntInputSize.Small)
+                ;
+        }
+    }
+}
---
+++ components/input/AntInputSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public class AntInputSize
+    {
+        public const string Default = "default";
+        public const string Large = "large";
+        public const string Small = "small";
+    }
+}
