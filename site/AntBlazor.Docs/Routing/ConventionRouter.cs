8ca44d2ce530a0b00964052c20740609a1c95a02
ElderJames <shunjiey@hotmail.com>
2020-03-13 14:10:33 +0800

    docs: add convention router

---
+++ site/AntBlazor.Docs/Routing/ConventionRouter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace AntBlazor.Docs.Routing
+{
+    public class ConventionRouter : IComponent, IHandleAfterRender, IDisposable
+    {
+        private RenderHandle _renderHandle;
+        private bool _navigationInterceptionEnabled;
+        private string _location;
+
+        [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private INavigationInterception NavigationInterception { get; set; }
+        [Inject] private RouteManager RouteManager { get; set; }
+
+        [Parameter] public RenderFragment NotFound { get; set; }
+        [Parameter] public RenderFragment<RouteData> Found { get; set; }
+
+        public void Attach(RenderHandle renderHandle)
+        {
+            _renderHandle = renderHandle;
+            _location = NavigationManager.Uri;
+            NavigationManager.LocationChanged += HandleLocationChanged;
+        }
+
+        public Task SetParametersAsync(ParameterView parameters)
+        {
+            parameters.SetParameterProperties(this);
+
+            if (Found == null)
+            {
+                throw new InvalidOperationException($"The {nameof(ConventionRouter)} component requires a value for the parameter {nameof(Found)}.");
+            }
+
+            if (NotFound == null)
+            {
+                throw new InvalidOperationException($"The {nameof(ConventionRouter)} component requires a value for the parameter {nameof(NotFound)}.");
+            }
+
+            RouteManager.Initialise();
+            Refresh();
+
+            return Task.CompletedTask;
+        }
+
+        public Task OnAfterRenderAsync()
+        {
+            if (!_navigationInterceptionEnabled)
+            {
+                _navigationInterceptionEnabled = true;
+                return NavigationInterception.EnableNavigationInterceptionAsync();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= HandleLocationChanged;
+        }
+
+        private void HandleLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            _location = args.Location;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var relativeUri = NavigationManager.ToBaseRelativePath(_location);
+            var parameters = ParseQueryString(relativeUri);
+
+            if (relativeUri.IndexOf('?') > -1)
+            {
+                relativeUri = relativeUri.Substring(0, relativeUri.IndexOf('?'));
+            }
+
+            var segments = relativeUri.Trim().Split('/', StringSplitOptions.RemoveEmptyEntries);
+            var matchResult = RouteManager.Match(segments);
+
+            if (matchResult.IsMatch)
+            {
+                var routeData = new RouteData(
+                    matchResult.MatchedRoute.Handler,
+                    parameters);
+
+                _renderHandle.Render(Found(routeData));
+            }
+            else
+            {
+                _renderHandle.Render(NotFound);
+            }
+        }
+
+        private Dictionary<string, object> ParseQueryString(string uri)
+        {
+            var querystring = new Dictionary<string, object>();
+
+            foreach (string kvp in uri.Substring(uri.IndexOf("?") + 1).Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (kvp != "" && kvp.Contains("="))
+                {
+                    var pair = kvp.Split('=');
+                    querystring.Add(pair[0], pair[1]);
+                }
+            }
+
+            return querystring;
+        }
+    }
+}
---
+++ site/AntBlazor.Docs/Routing/RouteManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor.Docs.Routing
+{
+    public class RouteManager
+    {
+        public Route[] Routes { get; private set; }
+
+        public void Initialise()
+        {
+            var pageComponentTypes = Assembly.GetExecutingAssembly()
+                .ExportedTypes
+                .Where(t => t.Namespace != null && (t.IsSubclassOf(typeof(ComponentBase))
+                                                    && t.Namespace.Contains(".Pages")));
+
+            var routesList = new List<Route>();
+            foreach (var pageType in pageComponentTypes)
+            {
+                if (pageType.FullName == null)
+                    continue;
+
+                var newRoute = new Route
+                {
+                    UriSegments = pageType.FullName.Substring(pageType.FullName.IndexOf("Pages", StringComparison.Ordinal) + 6).Split('.'),
+                    Handler = pageType
+                };
+
+                routesList.Add(newRoute);
+            }
+
+            Routes = routesList.ToArray();
+        }
+
+        public MatchResult Match(string[] segments)
+        {
+            if (segments.Length == 0)
+            {
+                var indexRoute = Routes.SingleOrDefault(x => x.Handler.FullName != null && x.Handler.FullName.ToLower().EndsWith("index"));
+                return MatchResult.Match(indexRoute);
+            }
+
+            foreach (var route in Routes)
+            {
+                var matchResult = route.Match(segments);
+
+                if (matchResult.IsMatch)
+                {
+                    return matchResult;
+                }
+            }
+
+            return MatchResult.NoMatch();
+        }
+    }
+}
