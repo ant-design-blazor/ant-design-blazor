0017ee1cf7a3d4bfc6cf4371154035407725113e
Brian Ding <Brian_Ding@outlook.com>
2020-03-13 20:40:03 +0800

    implement onChange/onPressEnter for AntInput

--- components/input/AntInputBase.cs
+++ components/input/AntInputBase.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace AntBlazor
 {
@@ -15,10 +17,19 @@ namespace AntBlazor
         public string placeholder { get; set; }
 
         [Parameter]
+        public string defaultValue { get; set; }
+
+        [Parameter]
         public string prefix { get; set; }
 
         [Parameter]
         public string suffix { get; set; }
+
+        [Parameter]
+        public EventCallback<ChangeEventArgs> onChange { get; set; }
+
+        [Parameter]
+        public EventCallback<KeyboardEventArgs> onPressEnter { get; set; }
 
         protected override void OnInitialized()
         {
@@ -30,6 +41,22 @@ namespace AntBlazor
                 .Add($"{prefixCls}")
                 .If($"{prefixCls}-lg", () => size == AntInputSize.Large)
                 .If($"{prefixCls}-sm", () => size == AntInputSize.Small);
+        }
+
+        protected async Task OnChangeAsync(ChangeEventArgs args)
+        {
+            if (onChange.HasDelegate)
+            {
+                await onChange.InvokeAsync(args);
+            }
+        }
+
+        protected async Task OnPressEnterAsync(KeyboardEventArgs args)
+        {
+            if (args.Code == "Enter" && onPressEnter.HasDelegate)
+            {
+                await onPressEnter.InvokeAsync(args);
+            }
         }
     }
 }
