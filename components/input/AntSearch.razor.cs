71167a0caecd4a8dffdf59de261bfed4428ae5d1
Yuanpeng Ding <Brian_Ding@outlook.com>
2020-03-16 16:26:34 +0800

    add AntSearch

---
+++ components/input/AntSearch.razor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+namespace AntBlazor
+{
+    public partial class AntSearch : AntInputBase
+    {
+        [Parameter]
+        public EventCallback<EventArgs> onSearch { get; set; }
+
+        protected override void SetClasses()
+        {
+            base.SetClasses();
+
+            if (!Attributes.ContainsKey("enterButton"))
+            {
+                suffix = "search";
+            }
+            else
+            {
+                if (Attributes["enterButton"].ToString() == "Search")
+                {
+                    AddOnAfter = new RenderFragment((builder) =>
+                    {
+                        //<AntButton type="primary" icon="search" /></AntButton>
+                        builder.OpenElement(0, "AntButton");
+                        builder.AddAttribute(1, "type", "primary");
+                        builder.AddContent(2, "Search");
+                        builder.CloseElement();
+                    });
+
+                    StateHasChanged();
+                }
+                else
+                {
+                    AddOnAfter = new RenderFragment((builder) =>
+                    {
+                        //<AntButton type="primary" icon="search" /></AntButton>
+                        builder.OpenElement(0, "AntButton");
+                        builder.AddAttribute(1, "type", "primary");
+                        builder.AddAttribute(2, "icon", "search");
+                        builder.CloseElement();
+                    });
+
+                    StateHasChanged();
+                }
+            }
+        }
+
+        private async void Search()
+        {
+            if (onSearch.HasDelegate)
+            {
+                await onSearch.InvokeAsync(EventArgs.Empty);
+            }
+        }
+    }
+}
