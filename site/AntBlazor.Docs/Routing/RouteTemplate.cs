c6a630424d1069a7b9537ee7cbf3fd07c6e29e4b
ElderJames <shunjiey@hotmail.com>
2020-03-17 13:42:54 +0800

    docs: dynaimc route for markdown docs

---
+++ site/AntBlazor.Docs/Routing/RouteTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AntBlazor.Docs.Routing
+{
+    [DebuggerDisplay("{TemplateText}")]
+    internal class RouteTemplate
+    {
+        public RouteTemplate(string templateText, TemplateSegment[] segments)
+        {
+            TemplateText = templateText;
+            Segments = segments;
+        }
+
+        public string TemplateText { get; }
+
+        public TemplateSegment[] Segments { get; }
+    }
+}
