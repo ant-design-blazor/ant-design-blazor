e1f1b22f842e03657f53ae18af572a3ac5de12a4
Yuanpeng Ding <Brian_Ding@outlook.com>
2020-03-20 15:02:23 +0800

    improve AntTextArea

---
+++ components/input/AntTextArea.razor.cs
@@ -0,0 +1,48 @@
+﻿using AntBlazor.JsInterop;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public partial class AntTextArea : AntInput
+    {
+        private RenderFragment _hidden;
+
+        private ElementReference _hiddenEle;
+
+        [Parameter]
+        public bool autoSize { get; set; }
+
+        [Parameter]
+        public EventCallback<object> onResize { get; set; }
+
+        protected override async void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override async void OnInputAsync(ChangeEventArgs args)
+        {
+            // do not call base method to avoid lost focus
+            //base.OnInputAsync(args);
+
+            if (autoSize)
+            {
+                await ChangeSizeAsync();
+            }
+        }
+
+        private async Task ChangeSizeAsync()
+        {
+            // Ant-design use a hidden textarea to calculate row height, totalHeight = rows * rowHeight
+            // TODO: compare with maxheight
+            Element element = await JsInvokeAsync<Element>(JSInteropConstants.getDomInfo, inputEl);
+            Style = $"height: {element.scrollHeight}px;overflow-y: hidden;";
+        }
+
+        protected async Task OnResizeAsync()
+        {
+
+        }
+    }
+}
