d0876cc3f739428554bc62a500c8c2176e7a5b66
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2020-03-22 15:11:34 +0100

    (feat): Add Component Badge (#27)
    Based on Online Docs.
    
    Co-authored-by: Benjamin Vertonghen <benjamin.vertonghen@apped.be>
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/badge/AntBadgeStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public static class AntBadgeStatus
+    {
+        public const string Default = "default";
+        public const string Success = "success";
+        public const string Processing = "processing ";
+        public const string Error = "error ";
+        public const string Warning = "warning ";
+    }
+}
