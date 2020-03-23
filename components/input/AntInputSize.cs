9c98b27e3b5fa1607e2923d1f1504dae228822d9
Brian Ding <brian_ding@outlook.com>
2020-03-23 09:31:08 +0800

    add component input (#33)
    * add input component
    
    * fix menu

---
+++ components/input/AntInputSize.cs
@@ -0,0 +1,9 @@
+﻿namespace AntBlazor
+{
+    public static class AntInputSize
+    {
+        public const string Default = "default";
+        public const string Large = "large";
+        public const string Small = "small";
+    }
+}
