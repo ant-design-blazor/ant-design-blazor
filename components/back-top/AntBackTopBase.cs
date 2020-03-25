6bc6865c8d2f23a955b856571fd39d8d3c842ce4
garvin <onadream@hotmail.com>
2020-03-25 23:23:58 +0800

    feat : add back to top

---
+++ components/back-top/AntBackTopBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public class AntBackTopBase : AntDomComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClickCallback { get; set; }
+
+        protected async Task OnClick(MouseEventArgs args)
+        {
+            //JsInvokeAsync<Element>(JSInteropConstants.getDomInfo, Ref);
+        }
+
+
+    }
+}
