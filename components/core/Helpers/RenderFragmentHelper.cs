494a046c90cbb1205c5a7dfe98b4debc211c3dc0
AlseinX <xyh951115@live.com>
2020-04-14 19:55:06 +0800

    feat: end of work in 20200414

---
+++ components/core/Helpers/RenderFragmentHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public static class RenderFragmentHelper
+    {
+        public static RenderFragment ToRenderFragment(this string value) => builder => builder.AddContent(1, value);
+    }
+}
--- components/table/style/RowSelection.cs
+++ components/table/style/RowSelection.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,24 +12,29 @@ namespace AntBlazor
         Radio
     }
 
-    public struct RowSelection<TData>
+    public sealed class RowSelection<TData>
     {
         public const string DEFAULT_COLUMN_WIDTH = "40px";
 
         public RowSelectionType Type { get; set; }
 
-        public CssSizeLength ColumnWidth { get; set; }
+        public CssSizeLength ColumnWidth { get; set; } = DEFAULT_COLUMN_WIDTH;
 
-        public bool Fixed { get; set; }
+        public bool Fixed { get; set; } = false;
 
-        public Func<TData, CheckboxOption> GetCheckboxOptions { get; set; }
+        public Func<TData, CheckboxOption> GetCheckboxOptions { get; set; } = _ => new CheckboxOption();
+
+        public bool HideDefaultSelections { get; set; } = false;
+
+        public Func<bool, TData, int, RenderFragment, RenderFragment> RenderCell { get; set; } = (c, d, i, o) => o;
+
+        public IList<TData> SelectedRows { get; } = new List<TData>();
+
+        public IList<(RenderFragment text, Func<TData> onSelect)> Selections = new List<(RenderFragment, Func<TData>)>();
 
         public RowSelection(RowSelectionType type)
         {
             Type = type;
-            ColumnWidth = DEFAULT_COLUMN_WIDTH;
-            Fixed = default;
-            GetCheckboxOptions = default;
         }
 
         public static implicit operator RowSelection<TData>(string value) => value.ToLowerInvariant() switch
