41810622b34c2c9dfc2a30fb511a45c4811be39b
Brian Ding <brian_ding@outlook.com>
2020-04-16 12:05:59 +0800

    feat: add component tabs (#67)
    * fix autosize
    
    * feat(module: tabs): add AntTabs
    
    * chore(module:input): code improvement
    
    * implement tab
    
    * implement disable tab
    
    * implement scroll tabs
    
    * resume tabs demo page
    
    * implement tabs prev/next calcaultion logic
    
    * update nav bar render logic
    
    * feat (module: tabs): implement rerender logic
    
    * feat:(module: tabs): update render logic
    
    * feat: (module: tabs): implement extra content
    
    * feat(module: tabs): add panes dynamically
    
    * feat(module: tabs): implement add tabs dynamically
    
    * feat(module: tabs): implement remove tabs
    
    * feat(module: tabs): Customized trigger of new tab
    
    * fix(module: tabs): stop event propagation
    
    * fix (module: tabs): close icon disappea

---
+++ components/tabs/AntTabPane.razor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor
+{
+    public partial class AntTabPane : AntDomComponentBase
+    {
+        private const string PrefixCls = "ant-tabs-tab";
+        private AntTabs _parent;
+
+        internal ClassMapper ClassMapper = new ClassMapper();
+        internal bool IsActive { get; set; }
+
+        [CascadingParameter]
+        internal AntTabs Parent
+        {
+            get
+            {
+                return _parent;
+            }
+            set
+            {
+                if (_parent == null)
+                {
+                    _parent = value;
+                    _parent.AddTabPane(this);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Forced render of content in tabs, not lazy render after clicking on tabs
+        /// </summary>
+        [Parameter]
+        public bool ForceRender { get; set; } = false;
+
+        /// <summary>
+        /// TabPane's key
+        /// </summary>
+        [Parameter]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Show text in <see cref="AntTabPane"/>'s head
+        /// </summary>
+        [Parameter]
+        public RenderFragment Tab { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public bool Closable { get; set; } = true;
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            ClassMapper.Clear().
+                Add(PrefixCls)
+                .If($"{PrefixCls}-active", () => IsActive)
+                .If($"{PrefixCls}-disabled", () => Disabled);
+        }
+    }
+}
---
+++ components/tabs/AntTabPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public static class AntTabPosition
+    {
+        public const string Top = "top";
+        public const string Right = "right";
+        public const string Bottom = "bottom";
+        public const string Left = "left";
+    }
+}
