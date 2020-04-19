8ac2ec202bcde6e4b2a7de3d8ec27b20b15dd064
钟迪龙 <musicvs@163.com>
2020-04-19 14:38:36 +0800

    feat: add component empty

---
+++ components/empty/AntEmptyBase.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class AntEmptyBase : AntDomComponentBase
+    {
+        [Parameter] public string prefixCls { get; set; } = "ant-empty";
+        [Parameter] public string imageStyle { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public OneOf<string, bool, RenderFragment> description { get; set; } = "暂无数据";
+
+        [Parameter]
+        public OneOf<string, RenderFragment> image { get; set; } = AntEmpty.PRESENTED_IMAGE_DEFAULT;
+
+        protected void SetClass()
+        {
+            this.ClassMapper.Clear()
+                .Add(prefixCls)
+                .If($"{prefixCls}-normal", () => image.IsT1 && image.AsT1 == AntEmpty.PRESENTED_IMAGE_SIMPLE)
+               //.If($"{prefixCls}-rtl", () => direction == "rlt") -- TODO
+               ;
+        }
+
+        protected override void OnInitialized()
+        {
+            this.SetClass();
+
+            base.OnInitialized();
+        }
+
+        protected override void OnParametersSet()
+        {
+            this.SetClass();
+
+            base.OnParametersSet();
+        }
+    }
+}
