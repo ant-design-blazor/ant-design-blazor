ed4e1dab63fea37ab40d9710590502f7a276d8c1
钟迪龙 <musicvs@163.com>
2020-04-20 20:55:24 +0800

    feat(module:date-picker): add date type view

---
+++ components/date-picker/AntDatePickerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class AntDatePickerType
+    {
+        public const string Date = "date";
+        public const string Week = "week";
+        public const string Month = "month";
+        public const string Quarter = "quarter";
+        public const string Year = "year";
+    }
+}
