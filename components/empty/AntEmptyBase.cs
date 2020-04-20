88fcc40c0cf4335d35ed1671f9eb580ab8a09725
笨木头 <musicvs@163.com>
2020-04-20 10:03:32 +0800

    feat: add component empty and doc (#78)
    * feat: add component empty
    
    * docs: add empty doc
    
    * feat(module:empty): add direction parameter
    
    * feat(module:empty): change property to upper

---
+++ components/empty/AntEmptyBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using OneOf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class AntEmptyBase : AntDomComponentBase
+    {
+        [Parameter] public string PrefixCls { get; set; } = "ant-empty";
+        /// <summary>
+        /// "ltr"|"rtl"
+        /// </summary>
+        [Parameter] public string Direction { get; set; } = "ltr";
+        [Parameter] public string ImageStyle { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public OneOf<string, bool, RenderFragment> Description { get; set; } = "暂无数据";
+
+        [Parameter]
+        public OneOf<string, RenderFragment> Image { get; set; } = AntEmpty.PRESENTED_IMAGE_DEFAULT;
+
+        protected void SetClass()
+        {
+            this.ClassMapper.Clear()
+                .Add(PrefixCls)
+                .If($"{PrefixCls}-normal", () => Image.IsT1 && Image.AsT1 == AntEmpty.PRESENTED_IMAGE_SIMPLE)
+                .If($"{PrefixCls}-{Direction}", () => Direction.IsIn("ltr", "rlt"))
+               ;
+        }
+
+        protected override void OnInitialized()
+        {
+            this.SetClass();
+
+            base.OnInitialized();
+        }
+
+        protected override void OnParametersSet()
+        {
+            this.SetClass();
+
+            base.OnParametersSet();
+        }
+    }
+}
