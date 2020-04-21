d9fb1f05d2850457c3026b9a503bafe5b3de4722
钟迪龙 <musicvs@163.com>
2020-04-21 20:24:38 +0800

    feat(module: date-picker): add panel base class

---
+++ components/date-picker/AntDatePickerPanelBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    internal class AntDatePickerPanelBase : AntDomComponentBase
+    {
+        [CascadingParameter]
+        public AntDatePicker DatePicker { get; set; }
+
+        [Parameter]
+        public EventCallback<DateTime> OnSelect { get; set; }
+
+
+        protected void OnSelectDate(DateTime date)
+        {
+            OnSelect.InvokeAsync(date);
+        }
+
+        protected void OnSelectYear(DateTime date)
+        {
+            DateTime selectDate = new DateTime(
+                date.Year,
+                DatePicker.CurrentSelectDate.Month,
+                DatePicker.CurrentSelectDate.Day
+            );
+
+            OnSelect.InvokeAsync(selectDate);
+        }
+
+        protected void OnSelectMonth(DateTime date)
+        {
+            DateTime selectDate = new DateTime(
+                DatePicker.CurrentSelectDate.Year,
+                date.Month,
+                DatePicker.CurrentSelectDate.Day
+            );
+
+            OnSelect.InvokeAsync(selectDate);
+        }
+
+        protected void OnSelectDay(DateTime date)
+        {
+            DateTime selectDate = new DateTime(
+                DatePicker.CurrentSelectDate.Year,
+                DatePicker.CurrentSelectDate.Month,
+                date.Day
+            );
+
+            OnSelect.InvokeAsync(selectDate);
+        }
+
+    }
+}
