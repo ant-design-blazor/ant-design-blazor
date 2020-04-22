187df1b1539cc763f11c9e0f72b4a388537ecbf5
钟迪龙 <musicvs@163.com>
2020-04-23 07:19:11 +0800

    feat(module: date-picker): add Parameters

---
+++ components/date-picker/AntDatePickerPlaceholder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    internal class AntDatePickerPlaceholder
+    {
+        public const string Date = "请选择日期";
+        public const string Week = "请选择周";
+        public const string Month = "请选择月份";
+        public const string Quarter = "请选择季度";
+        public const string Year = "请选择年份";
+    }
+}
