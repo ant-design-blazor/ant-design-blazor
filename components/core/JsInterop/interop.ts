7db5e9b8816e1c558e40dbcdc54f2397fdf3a60f
Brian Ding <Brian_Ding@outlook.com>
2020-04-22 18:56:52 +0800

    chore: update js addEventListener & stringify event args

--- components/core/JsInterop/interop.ts
+++ components/core/JsInterop/interop.ts
@@ -1,95 +1,104 @@
 ﻿export function getDom(element) {
-  if (!element) {
-    element = document.body;
-  } else if (typeof element === 'string') {
-    element = document.querySelector(element);
-  }
-  return element;
+    if (!element) {
+        element = document.body;
+    } else if (typeof element === 'string') {
+        element = document.querySelector(element);
+    }
+    return element;
 }
 
 export function getDomInfo(element) {
-  var result = {};
+    var result = {};
 
-  var dom = getDom(element);
+    var dom = getDom(element);
 
-  for (var key in dom) {
-    var item = dom[key];
-    if (!item) continue;
-    if (typeof item === 'string' || (typeof item === 'number' && !isNaN(item))) result[key] = item;
-  }
+    for (var key in dom) {
+        var item = dom[key];
+        if (!item) continue;
+        if (typeof item === 'string' || (typeof item === 'number' && !isNaN(item))) result[key] = item;
+    }
 
-  return result;
+    return result;
 }
 
 export function getBoundingClientRect(element) {
-  let dom = getDom(element);
-  return dom.getBoundingClientRect();
+    let dom = getDom(element);
+    return dom.getBoundingClientRect();
 }
 
 export function addDomEventListener(element, eventName, invoker) {
-  let callback = args => {
-    invoker.invokeMethodAsync('Invoke', args);
-  };
+    let callback = args => {
+        const obj = {};
+        for (let k in args) {
+            obj[k] = args[k];
+        }
+        let json = JSON.stringify(obj, (k, v) => {
+            if (v instanceof Node) return 'Node';
+            if (v instanceof Window) return 'Window';
+            return v;
+        }, ' ');
+        invoker.invokeMethodAsync('Invoke', json);
+    };
 
-  if (element == 'window') {
-    window.addEventListener(eventName, callback);
-  } else {
-    let dom = getDom(element);
-    (document.querySelector(dom) as HTMLElement).addEventListener(eventName, callback);
-  }
+    if (element == 'window') {
+        window.addEventListener(eventName, callback);
+    } else {
+        let dom = getDom(element);
+        (document.querySelector(dom) as HTMLElement).addEventListener(eventName, callback);
+    }
 }
 
 export function matchMedia(query) {
-  return window.matchMedia(query).matches;
+    return window.matchMedia(query).matches;
 }
 
 function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
 
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
 
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
 
-  document.body.removeChild(textArea);
+    document.body.removeChild(textArea);
 }
 export function copy(text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
-  }
-  navigator.clipboard.writeText(text).then(function () {
-    console.log('Async: Copying to clipboard was successful!');
-  }, function (err) {
-    console.error('Async: Could not copy text: ', err);
-  });
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
 }
 
 export function focus(selector) {
-  let dom = getDom(selector);
-  dom.focus();
+    let dom = getDom(selector);
+    dom.focus();
 }
 
 export function blur(selector) {
-  let dom = getDom(selector);
-  dom.blur();
+    let dom = getDom(selector);
+    dom.blur();
 }
 
 export function log(text) {
-  console.log(text);
+    console.log(text);
 }
 
 export function BackTop(element) {
