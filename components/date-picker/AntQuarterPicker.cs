1fb3ee2fd483311548381f2b368b12aa77495d0b
钟迪龙 <musicvs@163.com>
2020-04-23 13:41:11 +0800

    feat(module: date-picker): add year/month/week/quarter picker class

---
+++ components/date-picker/AntQuarterPicker.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AntBlazor
+{
+    public class AntQuarterPicker : AntDatePicker
+    {
+        public AntQuarterPicker()
+        {
+            Picker = AntDatePickerType.Quarter;
+        }
+    }
+}
