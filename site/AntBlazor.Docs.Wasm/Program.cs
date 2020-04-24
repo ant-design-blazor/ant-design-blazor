27d1786044d96c5b31707c4f0f90ea4d75dbb458
ElderJames <shunjiey@hotmail.com>
2020-04-24 10:34:54 +0800

    chore: update to 3.2 preivew 5

--- site/AntBlazor.Docs.Wasm/Program.cs
+++ site/AntBlazor.Docs.Wasm/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,7 +13,8 @@ namespace AntBlazor.Docs.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             builder.Services.AddAntBlazorDocs();
 
             await builder.Build().RunAsync();
