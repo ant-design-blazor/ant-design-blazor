869af0a8774a81b6573de65ad451450a19ec8c6f
笨木头 <musicvs@163.com>
2020-04-28 15:56:56 +0800

    feat: add DatePicker and TimePicker (#85)
    * feat(module: date-picker): add AntDatePicker
    
    * feat(module: date-picker): add AntTimePicker
    
    * docs(module: date-picker): add demos

---
+++ components/date-picker/types/AntDatePickerDisabledTime.cs
@@ -0,0 +1,17 @@
+﻿namespace AntBlazor
+{
+    public class AntDatePickerDisabledTime
+    {
+        internal int[] _disabledHours;
+        internal int[] _disabledMinutes;
+        internal int[] _disabledSeconds;
+
+        public AntDatePickerDisabledTime(
+            int[] disabledHours, int[] disabledMinutes, int[] disabledSeconds)
+        {
+            _disabledHours = disabledHours;
+            _disabledMinutes = disabledMinutes;
+            _disabledSeconds = disabledSeconds;
+        }
+    }
+}
