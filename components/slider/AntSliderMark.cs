35ea80009a481ccafad10317a0896313ae67054a
Brian Ding <Brian_Ding@outlook.com>
2020-04-29 18:34:17 +0800

    feat (module: slider): add component slider

---
+++ components/slider/AntSliderMark.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace AntBlazor
+{
+    public class AntSliderMark
+    {
+        public double Key { get; }
+        public RenderFragment Value { get; }
+        public string Style { get; }
+
+        public AntSliderMark(double key, string value)
+        {
+            Key = key;
+            Value = (b) => b.AddContent(0, value);
+        }
+
+        public AntSliderMark(double key, RenderFragment value, string style)
+        {
+            Key = key;
+            Value = value;
+            Style = style;
+        }
+    }
+}
