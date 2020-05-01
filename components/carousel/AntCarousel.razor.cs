9534357eb9cb88a55a03287cf246e9d92693b328
Brian Ding <Brian_Ding@outlook.com>
2020-05-01 13:45:33 +0800

    implmement acitvate carousel button

--- components/carousel/AntCarousel.razor.cs
+++ components/carousel/AntCarousel.razor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AntBlazor.JsInterop;
 using Microsoft.AspNetCore.Components;
 
 namespace AntBlazor
@@ -12,6 +13,7 @@ namespace AntBlazor
         private string _trackStyle = "width: 5562px; opacity: 1; transform: translate3d(-618px, 0px, 0px);";
         private string _slickStyle = "outline: none; width: 618px;";
         private string _slickClonedStyle = "width: 618px;";
+        private ElementReference _ref;
 
         private List<AntCarouselSlick> _slicks = new List<AntCarouselSlick>();
 
@@ -23,6 +25,17 @@ namespace AntBlazor
         public RenderFragment ChildContent { get; set; }
 
         #endregion Parameters
+
+        protected async override Task OnFirstAfterRenderAsync()
+        {
+            await base.OnFirstAfterRenderAsync();
+
+            DomRect carouselRect = await JsInvokeAsync<DomRect>(JSInteropConstants.getBoundingClientRect, _ref);
+            int width = (int)carouselRect.width;
+            _trackStyle = $"width: {width * (_slicks.Count * 2 + 1)}px; opacity: 1; transform: translate3d({-width}px, 0px, 0px);";
+            _slickStyle = $"outline: none; width: {width}px;";
+            _slickClonedStyle = $"width: {width}px;";
+        }
 
         internal void AddSlick(AntCarouselSlick slick)
         {
