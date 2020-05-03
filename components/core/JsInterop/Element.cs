39243180711cda270aefb0b069fe51e3740b89e6
钟迪龙 <mutouzdl@hotmail.com>
2020-05-03 16:05:57 +0800

    feat: add js methods

--- components/core/JsInterop/Element.cs
+++ components/core/JsInterop/Element.cs
@@ -2,6 +2,10 @@
 {
     public class Element
     {
+        public int absoluteTop { get; set; }
+
+        public int absoluteLeft { get; set; }
+
         public int offsetTop { get; set; }
 
         public int offsetLeft { get; set; }
