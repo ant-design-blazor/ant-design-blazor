78b952cb207cd03ff18fdb2d7597a027b08a76cd
Brian Ding <brian_ding@outlook.com>
2020-05-06 17:33:11 +0800

    feat: add component carousel (#96)
    * feat (module: carousel): add component carousel
    
    * implmement acitvate carousel button
    
    * update active slick
    
    * feat(module: carousel): add DotPosition
    
    * feat(module: carousel): add dot position demo
    
    * feat(module: carousel): implement fade in effect
    
    * feat(module: carousel): animation for autoplay from last to the first

---
+++ components/carousel/AntCarouselEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public static class AntCarouselEffect
+    {
+        public const string ScrollX = "scrollx";
+        public const string Fade = "fade";
+    }
+}
