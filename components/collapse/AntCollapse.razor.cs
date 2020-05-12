a0b32ae449e67b8fa8990e92b2cb4df47d4efda2
garvin <onadream@hotmail.com>
2020-05-12 23:52:33 +0800

    feat: add collapse

---
+++ components/collapse/AntCollapse.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public partial class AntCollapse : AntDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected ClassMapper ContentBoxClassMapper { get; set; } = new ClassMapper();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            string prefixCls = "ant-collapse";
+
+            ClassMapper.Add($"{prefixCls}").Add($"{prefixCls}-icon-position-left");//添加默认样式
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
