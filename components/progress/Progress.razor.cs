27dd4238335afba36fea2a78b719de6afdaa8fa8
Yuanpeng Ding <Brian_Ding@outlook.com>
2020-05-12 16:30:45 +0800

    feat(module: progress): add component progress

---
+++ components/progress/Progress.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public partial class Progress : AntDomComponentBase
+    {
+        private const string PrefixCls = "ant-progress";
+
+        #region Parameters
+
+
+
+        #endregion
+    }
+}
