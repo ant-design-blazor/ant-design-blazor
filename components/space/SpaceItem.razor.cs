155a21087ea63bc752ed3c8f4dc3d6ed2903923f
ElderJames <shunjiey@hotmail.com>
2020-05-15 18:52:46 +0800

    feat: add component space

---
+++ components/space/SpaceItem.razor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Components;
+
+namespace AntBlazor
+{
+    public partial class SpaceItem : ComponentBase
+    {
+        [CascadingParameter]
+        public Space Parent { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        private static readonly Hashtable _spaceSize = new Hashtable()
+        {
+            ["small"] = 8,
+            ["middle"] = 16,
+            ["large"] = 24
+        };
+
+        private string _marginStyle = "";
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+
+            if (Parent == null)
+                return;
+
+            var size = Parent.Size;
+            var direction = Parent.Direction;
+
+            var marginSize = size.IsIn("small", "middle", "large") ? _spaceSize[size] : size;
+
+            _marginStyle = direction == "horizontal" ? $"margin-right:{marginSize}px;" : $"margin-bottom:{marginSize}px;";
+        }
+    }
+}
