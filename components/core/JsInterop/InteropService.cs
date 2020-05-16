29c965f78dc1576c9ba289d64fae4db998ef78b1
James Yeung <shunjiey@hotmail.com>
2020-05-16 23:27:40 +0800

    docs: add demo code copy (#121)

---
+++ components/core/JsInterop/InteropService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace AntBlazor.JsInterop
+{
+    public class InteropService
+    {
+        private readonly IJSRuntime _js;
+
+        public InteropService(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async ValueTask Copy(string text)
+        {
+            await this.JsInvokeAsync(JSInteropConstants.copy, text);
+        }
+
+        private async Task<T> JsInvokeAsync<T>(string code, params object[] args)
+        {
+            try
+            {
+                return await _js.InvokeAsync<T>(code, args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private async Task JsInvokeAsync(string code, params object[] args)
+        {
+            try
+            {
+                await _js.InvokeVoidAsync(code, args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
