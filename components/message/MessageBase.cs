98159ff00ae140b04687d16286e1c56d4ed753a5
zxyao <zxyao145@gmail.com>
2020-05-19 20:08:49 +0800

    feat: add component message (#128)
    * feat: add component message
    
    * fix: then interface and demo bugs
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/message/MessageBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MessageBase : AntDomComponentBase
+    {
+        protected const string PrefixCls = "ant-message";
+    }
+}
