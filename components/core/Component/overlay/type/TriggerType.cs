3e86809a9ce2ba860d1b858940008d26755466b6
Zhuangkh <30818313+Zhuangkh@users.noreply.github.com>
2020-05-19 22:34:09 +0800

    feat:Add component Tooltip (#131)
    * feat:Add AntTooltip
    
    * Update Tooltip.razor.cs
    
    Co-authored-by: 笨木头 <musicvs@163.com>

--- components/core/Component/overlay/type/TriggerType.cs
+++ components/core/Component/overlay/type/TriggerType.cs
@@ -6,7 +6,8 @@ namespace AntBlazor
     {
         public static readonly TriggerType Click = new TriggerType(nameof(Click), 0);
         public static readonly TriggerType Hover = new TriggerType(nameof(Hover), 1);
-        public static readonly TriggerType ContextMenu = new TriggerType(nameof(ContextMenu), 2);
+        public static readonly TriggerType Focus = new TriggerType(nameof(Focus), 2);
+        public static readonly TriggerType ContextMenu = new TriggerType(nameof(ContextMenu), 3);
 
         private TriggerType(string name, int value) : base(name, value)
         {
---
+++ components/tooltip/Tooltip.razor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+using AntBlazor.JsInterop;
+using Microsoft.AspNetCore.Components.Rendering;
+using AntBlazor.Internal;
+using System.Linq;
+
+namespace AntBlazor
+{
+    public partial class Tooltip : OverlayTrigger
+    {
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool ArrowPointAtCenter { get; set; } = false;
+
+        [Parameter]
+        public double MouseEnterDelay { get; set; } = 0.1;
+
+        [Parameter]
+        public double MouseLeaveDelay { get; set; } = 0.1;
+
+        public Tooltip()
+        {
+            PrefixCls = "ant-tooltip";
+            Placement = PlacementType.TopCenter;
+        }
+
+        public override string GetOverlayEnterClass()
+        {
+            return string.Empty;
+        }
+
+        public override string GetPlacementClass()
+        {
+            if (!string.IsNullOrEmpty(PlacementCls))
+            {
+                return PlacementCls;
+            }
+
+            if (PlacementType.TopCenter.Equals(Placement))
+            {
+                return $"{PrefixCls}-placement-top";
+            }
+            else if (PlacementType.BottomCenter.Equals(Placement))
+            {
+                return $"{PrefixCls}-placement-bottom";
+            }
+            else
+            {
+                return $"{PrefixCls}-placement-{Placement.Name}";
+            }
+        }
+
+        public override async Task Show(int? overlayLeft = null, int? overlayTop = null)
+        {
+            if (Trigger.Contains(TriggerType.Hover))
+            {
+                await Task.Delay((int)(MouseEnterDelay * 1000));
+            }
+            await base.Show(overlayLeft, overlayTop);
+        }
+
+        public override async Task Hide(bool force = false)
+        {
+            if (Trigger.Contains(TriggerType.Hover))
+            {
+                await Task.Delay((int)(MouseLeaveDelay * 1000));
+            }
+            await base.Hide(force);
+        }
+    }
+}
