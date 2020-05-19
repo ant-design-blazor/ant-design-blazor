19ad30adbde5ed2b3e4a629116a470f0cc6920f4
Zhuangkh <30818313+Zhuangkh@users.noreply.github.com>
2020-05-19 22:34:09 +0800

    feat:Add component Tooltip (#131)
    * feat:Add AntTooltip
    
    * Update Tooltip.razor.cs
    
    Co-authored-by: 笨木头 <musicvs@163.com>

--- components/core/Component/overlay/type/PlacementType.cs
+++ components/core/Component/overlay/type/PlacementType.cs
@@ -4,16 +4,23 @@ namespace AntBlazor
 {
     public sealed class PlacementType : SmartEnum<PlacementType>
     {
-        public static readonly PlacementType Left = new PlacementType("left", "down", 0);
-        public static readonly PlacementType Right = new PlacementType("right", "down", 1);
+        public static readonly PlacementType TopLeft = new PlacementType("topLeft", "up", 0);
+        public static readonly PlacementType TopCenter = new PlacementType("topCenter", "up", 1);
+        public static readonly PlacementType TopRight = new PlacementType("topRight", "up", 2);
 
-        public static readonly PlacementType BottomLeft = new PlacementType("bottomLeft", "down", 2);
-        public static readonly PlacementType BottomCenter = new PlacementType("bottomCenter", "down", 3);
-        public static readonly PlacementType BottomRight = new PlacementType("bottomRight", "down", 4);
+        public static readonly PlacementType Left = new PlacementType("left", "down", 3);
+        public static readonly PlacementType LeftTop = new PlacementType("leftTop", "up", 4);
+        public static readonly PlacementType LeftBottom = new PlacementType("leftBottom", "down",5);
 
-        public static readonly PlacementType TopLeft = new PlacementType("topLeft", "up", 5);
-        public static readonly PlacementType TopCenter = new PlacementType("topCenter", "up", 6);
-        public static readonly PlacementType TopRight = new PlacementType("topRight", "up", 7);
+        public static readonly PlacementType Right = new PlacementType("right", "down", 6);
+        public static readonly PlacementType RightTop = new PlacementType("rightTop", "up", 7);
+        public static readonly PlacementType RightBottom = new PlacementType("rightBottom", "down", 8);
+
+        public static readonly PlacementType BottomLeft = new PlacementType("bottomLeft", "down", 9);
+        public static readonly PlacementType BottomCenter = new PlacementType("bottomCenter", "down", 10);
+        public static readonly PlacementType BottomRight = new PlacementType("bottomRight", "down", 11);
+
+
 
         public string SlideName { get; private set; }
 
