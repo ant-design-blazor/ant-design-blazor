d2f056ee41ff204a6706f852e1a75ad9e593f8d7
Bin Dong <popmessiah@hotmail.com>
2020-05-20 17:06:15 +0800

    fix: fix button type (#134)
    Completely remove "danger" from button type according to Ant Design 4.0 new design.

--- components/button/AntButton.razor.cs
+++ components/button/AntButton.razor.cs
@@ -66,7 +66,7 @@ namespace AntBlazor
             ClassMapper.Clear()
                 .Add("ant-btn")
                 .If($"{prefixName}-{this.Type}", () => !string.IsNullOrEmpty(Type))
-                 .If($"{prefixName}-danger", () => Danger)
+                 .If($"{prefixName}-dangerous", () => Danger)
                 .If($"{prefixName}-{Shape}", () => !string.IsNullOrEmpty(Shape))
                 .If($"{prefixName}-{sizeMap[this.Size]}", () => sizeMap.ContainsKey(Size))
                 .If($"{prefixName}-loading", () => Loading)
