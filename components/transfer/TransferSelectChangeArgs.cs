5ff88abb1ae654303feaec8e6aa3489aeceaddd8
颢源 <60841471+MountainTang@users.noreply.github.com>
2020-05-26 15:21:47 +0800

    feat: add transfer (#148)
    * transfer_brancn
    
    * fix: transfer demos and empty status
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/transfer/TransferSelectChangeArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class TransferSelectChangeArgs
+    {
+        public string[] SourceSelectedKeys { get; private set; }
+
+        public string[] TargetSelectedKeys { get; private set; }
+
+        public TransferSelectChangeArgs(string[] sourceSelectedKeys, string[] targetSelectedKeys)
+        {
+            this.SourceSelectedKeys = sourceSelectedKeys;
+            this.TargetSelectedKeys = targetSelectedKeys;
+        }
+    }
+}
---
+++ site/AntBlazor.Docs/Demos/Transfer/demo/large-data.md
@@ -0,0 +1,15 @@
+---
+order: 4
+debug: true
+title:
+  zh-CN: 大数据性能测试
+  en-US: Performance Test
+---
+
+## zh-CN
+
+2000 条数据。
+
+## en-US
+
+2000 items.
