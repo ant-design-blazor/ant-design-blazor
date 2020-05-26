0090e0d13ec41c8e5b722a93e8d477cbf9d51600
颢源 <60841471+MountainTang@users.noreply.github.com>
2020-05-26 15:21:47 +0800

    feat: add transfer (#148)
    * transfer_brancn
    
    * fix: transfer demos and empty status
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/transfer/TransferItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AntBlazor
+{
+    public class TransferItem
+    {
+        public string Key { get; set; } = "";
+
+        public string Title { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public bool Disabled { get; set; }
+
+        private readonly IDictionary<string, object> _properties = new Dictionary<string, object>();
+
+        public object this[string index]
+        {
+            get
+            {
+                return _properties[index];
+            }
+            set
+            {
+                _properties[index] = value;
+            }
+        }
+    }
+}
