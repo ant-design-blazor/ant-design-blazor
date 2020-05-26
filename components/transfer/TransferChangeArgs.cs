a2787f5673f743b11888656585b41ff6e8d89376
颢源 <60841471+MountainTang@users.noreply.github.com>
2020-05-26 15:21:47 +0800

    feat: add transfer (#148)
    * transfer_brancn
    
    * fix: transfer demos and empty status
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/transfer/TransferChangeArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class TransferChangeArgs
+    {
+        public string[] TargetKeys { get; private set; }
+
+        public string Direction { get; private set; }
+
+        public string[] MoveKeys { get; private set; }
+
+        public TransferChangeArgs(string[] targetKeys, string direction, string[] moveKeys)
+        {
+            this.TargetKeys = targetKeys;
+            this.Direction = direction;
+            this.MoveKeys = moveKeys;
+        }
+    }
+}
---
+++ components/transfer/TransferSearchArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntBlazor
+{
+    public class TransferSearchArgs
+    {
+        public string Direction { get; private set; }
+
+        public string Value { get; private set; }
+
+        public TransferSearchArgs(string direction, string value)
+        {
+            this.Direction = direction;
+            this.Value = value;
+        }
+    }
+}
