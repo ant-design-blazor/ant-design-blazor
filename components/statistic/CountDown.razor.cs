2647ca3cfaca5401aa227d663559b2bde574751f
大风 <46520648+hrx521@users.noreply.github.com>
2020-06-09 23:47:50 +0800

    feat: add component statistic (#205)
    * feat: add-Statistic
    
    * refactor: generic type
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/statistic/CountDown.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class CountDown : StatisticComponentBase<DateTime>
+    {
+        [Parameter] public string Format { get; set; } = @"hh\:mm\:ss";
+
+        [Parameter] public EventCallback OnFinish { get; set; }
+
+        private Timer _timer;
+
+        private const int REFRESH_INTERVAL = 1000 / 30;
+
+        private TimeSpan _countDown = TimeSpan.Zero;
+        private string _format = "";
+
+        protected override void OnInitialized()
+        {
+            _format = Format;
+            _countDown = Value - DateTime.Now;
+            _timer = new Timer(StartCountDownForTimeSpan);
+            _timer.Change(0, REFRESH_INTERVAL);
+        }
+
+        private void StartCountDownForTimeSpan(object o)
+        {
+            _countDown = _countDown.Add(TimeSpan.FromMilliseconds(-REFRESH_INTERVAL));
+            if (_countDown.Ticks <= 0)
+            {
+                _countDown = TimeSpan.Zero;
+                _timer.Dispose();
+                OnFinish.InvokeAsync(o);
+            }
+
+            InvokeStateHasChanged();
+        }
+    }
+}
---
+++ site/AntBlazor.Docs/Demos/Statistic/demo/card.md
@@ -0,0 +1,20 @@
+---
+order: 2
+title:
+  zh-CN: 在卡片中使用
+  en-US: In Card
+---
+
+## zh-CN
+
+在卡片中展示统计数值。
+
+## en-US
+
+Display statistic data in Card.
+
+<style>
+  [data-theme="dark"] .site-statistic-demo-card {
+    background: #303030;
+  }
+</style>
