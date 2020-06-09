a377fe747cac9d27f3cfb0a00496a42b17f0bb9b
大风 <46520648+hrx521@users.noreply.github.com>
2020-06-09 23:47:50 +0800

    feat: add component statistic (#205)
    * feat: add-Statistic
    
    * refactor: generic type
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/statistic/Statistic.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class Statistic<TValue> : StatisticComponentBase<TValue>
+    {
+        [Parameter] public string DecimalSeparator { get; set; } = ".";
+
+        [Parameter] public string GroupSeparator { get; set; } = ",";
+
+        [Parameter] public int Precision { get; set; } = -1;
+
+        private string IntegerPart { get { return Convert.ToDecimal(Value).ToString($"###{GroupSeparator}###"); } }
+
+        private string FractionalPart
+        {
+            get
+            {
+                string tem;
+                if (Precision > 0)
+                    tem = Math.Round(Convert.ToDecimal(Value), Precision).ToString().Contains('.')
+                        ? Math.Round(Convert.ToDecimal(Value), Precision).ToString().Split('.').Last().PadRight(Precision, '0')
+                        : string.Empty.PadRight(Precision, '0');
+                else if (Precision < 0)
+                    tem = Value.ToString().Contains('.') ? Value.ToString().Split('.').Last() : null;
+                else
+                    tem = null;
+                return string.IsNullOrEmpty(tem) ? null : DecimalSeparator + tem;
+            }
+        }
+    }
+}
