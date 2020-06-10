159b8a9fde2ee7f9c87a78a03a41fbace2876d14
TimChen <timchen@live.com>
2020-06-10 21:53:54 +0800

    feat: add page-header components

---
+++ components/page-header/PageHeader.razor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AntDesign.JsInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AntDesign
+{
+    public partial class PageHeader
+    {
+        #region Parameters
+
+        [Parameter]
+        public string BackIcon { get; set; } = "";
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Subtitle { get; set; }
+
+        [Parameter]
+        public EventCallback OnBack { get; set; }
+
+
+        [Parameter]
+        public RenderFragment PageHeaderContent { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderFooter { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderBreadcrumb { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderAvatar { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderTitle { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderSubtitle { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderTags { get; set; }
+
+        [Parameter]
+        public RenderFragment PageHeaderExtra { get; set; }
+
+        #endregion
+
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+
+        private async void OnBackClick(MouseEventArgs eventArgs)
+        {
+            if (OnBack.HasDelegate)
+            {
+                await OnBack.InvokeAsync(eventArgs);
+            }
+            else
+            {
+            }
+
+        }
+    }
+}
---
+++ site/AntBlazor.Docs/Demos/PageHeader/doc/index.en-US.md
@@ -0,0 +1,12 @@
+﻿---
+category: Components
+type: Navigation
+title: PageHeader
+cols: 1
+---
+
+A header with common actions and design elements built in.
+
+## When To Use
+
+PageHeader can be used to highlight the page topic, display important information about the page, and carry the action items related to the current page (including page-level operations, inter-page navigation, etc.) It can also be used as inter-page navigation.
