ac56d89db227931fa4e2c46c4609349ca078a7bf
TimChen <TimChen44@users.noreply.github.com>
2020-06-15 22:23:08 +0800

    feat: add Descriptions component (#224)
    * feat: add page-header components
    
    * feat: add page-header components
    
    * feat: add page-header components
    
    * docs: some bugs of PageHeader
    
    * refactor: clean code
    
    * feat: add Descriptions component
    
    * feat: add Descriptions component
    
    * feat: add Descriptions component
    
    * feat: add Descriptions component
    
    * feat: add Descriptions component
    
    * fix: demo
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/descriptions/DescriptionsLayout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
+{
+    public static class DescriptionsLayout
+    {
+        public const string Horizontal = "horizontal";
+        public const string Vertical = "vertical";
+
+    }
+}
---
+++ site/AntBlazor.Docs/Demos/Descriptions/doc/index.en-US.md
@@ -0,0 +1,12 @@
+﻿---
+category: Components
+type: Data Display
+title: Descriptions
+cols: 1
+---
+
+Display multiple read-only fields in groups.
+
+## When To Use
+
+Commonly displayed on the details page.
