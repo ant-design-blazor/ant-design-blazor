0acbf9a4f62ffe20ebbb9141d88d26d66e4f89f2
Yuanpeng Ding <Brian_Ding@outlook.com>
2020-06-18 12:55:48 +0800

    feat(module: anchor): add component anchor

---
+++ components/anchor/Anchor.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class Anchor : AntDomComponentBase, IAnchor
+    {
+        public List<AnchorLink> Links { get; } = new List<AnchorLink>();
+
+        #region Parameters
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        #endregion
+    }
+}
