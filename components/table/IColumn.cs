07d1c3f4d6a08d5a1144320f7435220697d5e3b7
James Yeung <shunjiey@hotmail.com>
2020-06-19 23:06:33 +0800

    fix(module: table): selection reset while page changing. (#236)
    * fix: selection

---
+++ components/table/IColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
+{
+    public interface IColumn
+    {
+        public ITable Table { get; set; }
+
+        public bool IsHeader { get; set; }
+
+        public bool IsPlaceholder { get; set; }
+
+        public bool IsColGroup { get; set; }
+
+        public int Index { get; set; }
+
+        public string Fixed { get; set; }
+
+        public string Title { get; set; }
+
+        public int Width { get; set; }
+    }
+}
--- components/table/Table.razor.cs
+++ components/table/Table.razor.cs
@@ -11,7 +11,15 @@ namespace AntDesign
         private static readonly TItem _fieldModel = (TItem)RuntimeHelpers.GetUninitializedObject(typeof(TItem));
 
         [Parameter]
-        public IEnumerable<TItem> DataSource { get; set; }
+        public IEnumerable<TItem> DataSource
+        {
+            get => _dataSource;
+            set
+            {
+                _total = value.Count();
+                _dataSource = value;
+            }
+        }
 
         [Parameter]
         public RenderFragment<TItem> ChildContent { get; set; }
@@ -25,26 +33,92 @@ namespace AntDesign
         [Parameter]
         public bool Loading { get; set; }
 
-        private readonly IList<ITableColumn> _columns = new List<ITableColumn>();
+        [Parameter]
+        public string ScrollX { get; set; }
 
-        public IRowSelection HeaderSelection { get; set; }
+        [Parameter]
+        public string ScrollY { get; set; }
 
-        public void AddColumn(ITableColumn column)
+        [Parameter]
+        public int ScrollBarWidth { get; set; } = 17;
+
+        public ColumnContext ColumnContext { get; set; } = new ColumnContext();
+
+        private IEnumerable<TItem> _dataSource;
+        private ISelectionColumn _headerSelection;
+
+        ISelectionColumn ITable.HeaderSelection
         {
-            _columns.Add(column);
+            get => _headerSelection;
+            set => _headerSelection = value;
+        }
+
+        void ITable.SelectionChanged(int[] checkedIndex)
+        {
+            if (SelectedRowsChanged.HasDelegate)
+            {
+                var list = new List<TItem>();
+                foreach (var index in checkedIndex)
+                {
+                    list.Add(DataSource.ElementAt(index));
+                }
+
+                SelectedRowsChanged.InvokeAsync(list);
+            }
+        }
+
+        void ITable.Refresh()
+        {
             StateHasChanged();
         }
 
-        void ITable.OnSelectionChanged(int[] checkedIndex)
+        private void SetClass()
         {
-            var list = new List<TItem>();
-            foreach (var index in checkedIndex)
-            {
-                list.Add(DataSource.ElementAt(index));
-            }
+            string prefixCls = "ant-table";
+            ClassMapper.Add(prefixCls)
+                .If($"{prefixCls}-fixed-header", () => ScrollY != null)
+                //.Add( "ant-table ant-table-ping-left ant-table-ping-right ")
+                .If($"{prefixCls}-fixed-column {prefixCls}-scroll-horizontal", () => ColumnContext.Columns.Any(x => x.Fixed.IsIn("left", "right")))
+                .If($"{prefixCls}-has-fix-left", () => ColumnContext.Columns.Any(x => x.Fixed == "left"))
+                .If($"{prefixCls}-has-fix-right {prefixCls}-ping-right ", () => ColumnContext.Columns.Any(x => x.Fixed == "right"))
+                ;
+        }
 
-            SelectedRows = list.ToArray();
-            SelectedRowsChanged.InvokeAsync(SelectedRows);
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            SetClass();
+        }
+
+        private void ChangeSelection(int[] indexes)
+        {
+            if (indexes == null || !indexes.Any())
+            {
+                this._headerSelection.RowSelections.ForEach(x => x.Check(false));
+                this._headerSelection.Check(false);
+            }
+            else
+            {
+                this._headerSelection.RowSelections.Where(x => !x.RowIndex.IsIn(indexes)).ForEach(x => x.Check(false));
+                this._headerSelection.RowSelections.Where(x => x.RowIndex.IsIn(indexes)).ForEach(x => x.Check(true));
+                this._headerSelection.Check(true);
+            }
+        }
+
+        public void SetSelection(string[] keys)
+        {
+            if (keys == null || !keys.Any())
+            {
+                this._headerSelection.RowSelections.ForEach(x => x.Check(false));
+                this._headerSelection.Check(false);
+            }
+            else
+            {
+                this._headerSelection.RowSelections.Where(x => !x.Key.IsIn(keys)).ForEach(x => x.Check(false));
+                this._headerSelection.RowSelections.Where(x => x.Key.IsIn(keys)).ForEach(x => x.Check(true));
+                this._headerSelection.Check(keys.Any());
+            }
         }
     }
 }
