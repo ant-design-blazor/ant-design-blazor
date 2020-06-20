4571d5eee819c93521e13895cb47bcf80ae5e1c7
henry.zhang <1002527441@qq.com>
2020-06-20 22:04:21 +0800

    fix: update with z-index

--- components/mentions/Mentions.razor.cs
+++ components/mentions/Mentions.razor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using AntDesign.JsInterop;
 using Microsoft.AspNetCore.Components;
@@ -20,25 +22,19 @@ namespace AntDesign
         [Parameter] public string Placement { get; set; } = "bottom";
         [Parameter] public string Direction { get; set; } = "ltr";
         [Parameter] public int Rows { get; set; } = 1;
-
         [Parameter] public bool Loading { get; set; } = false;
         [Parameter] public RenderFragment ChildContent { get; set; }
-
         [Parameter] public EventCallback<ChangeEventArgs> ValueChange { get; set; }
         [Parameter] public EventCallback<FocusEventArgs> OnFocus { get; set; }
         [Parameter] public EventCallback<FocusEventArgs> OnBlur { get; set; }
         [Parameter] public EventCallback<EventArgs> OnSearch { get; set; }
 
         [Inject]  private DomEventService DomEventService { get; set; }
-      
-
-        public IList<Option> Options { get; } = new List<Option>();
-
-
+        private List<MentionsOption> LstOriginalOptions { get; set; } = new List<MentionsOption>();
+        private List<MentionsOption> LstOptions { get; set; } = new List<MentionsOption>();
 
         private bool Focused { get; set; }
         private bool ShowMentions { get; set; }
-
 
         private void SetClassMap()
         {
@@ -57,28 +53,85 @@ namespace AntDesign
             base.OnInitialized();
         }
 
-
         private  void Keyup(KeyboardEventArgs args)
         {
             if (args == null) return;
             if (Prefix.Contains(args.Key, StringComparison.Ordinal))
             {
-       
+                ShowMentions = Prefix.Contains(args.Key, StringComparison.Ordinal);
+                StateHasChanged();
+                return;
             }
-        
-          
+
+            if (args.Code == "Space")
+            {
+                ShowMentions = false;
+                StateHasChanged();
+                return;
+            }
+
+
+        }
+
+        private void OnChange(ChangeEventArgs args)
+        {
+            Console.WriteLine(this.Value); 
             return;
         }
 
-        private void Keydown(KeyboardEventArgs args)
+        private void OnKeyPress(KeyboardEventArgs args)
         {
-            if (args == null) return;
-           ShowMentions = Prefix.Contains(args.Key, StringComparison.Ordinal);
           
-            StateHasChanged();
-       
+
+
+        
+            //this.LstOptions = IsIncluded(this.Value);
+
+          
 
             return;
+        }
+
+
+        /// <summary>
+        /// return a  new list of mentionsOption, when all item included the specificed text
+        /// </summary>
+        /// <param name="textValue"></param>
+        /// <returns></returns>
+        private List<MentionsOption> IsIncluded(string textValue)
+        {
+
+            if (string.IsNullOrEmpty(textValue))
+            {
+                return LstOriginalOptions;
+            }
+
+            List<MentionsOption> lst = new List<MentionsOption>();
+            foreach (var item in LstOriginalOptions)
+            {
+                if (item.Value.Contains(textValue, StringComparison.Ordinal))
+                {
+                    lst.Add(item);
+                }
+            }
+            return lst;
+        }
+
+
+
+        
+        public void AddOption(MentionsOption option)
+        {
+            if (option == null) return;
+            Console.WriteLine($"id={option.Id}");
+           var opt = LstOriginalOptions.Find(x => x.Id == option.Id);
+            if (opt == null)
+            {
+                LstOriginalOptions.Add(option);
+            }
+
+
+
         }
 
         private void Onchange(ChangeEventArgs args)
