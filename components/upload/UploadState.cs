00ac26bfaa7c2001169adfb43dbfc80584d9d386
Meeleem <adsioj@163.com>
2020-07-01 06:49:51 +0800

    feat: add upload component (#276)
    * fix: fix invalid parameter "StrokeWidth"
    
    * feat(module: upload): Classic file upload
    经典模式文件上传功能
    
    * fix( module: upload): fix Ext attribute, delete 'done' file function
    
    * docs(module: upload): Demo and doc
    
    * fix (file: interop.ts):  Indent changed to 2 characters
    
    * fix: menu
    
    * fix: delete md files
    
    * fix: docs
    
    * feat (module: upload): DefaultFileList
    
    * refactor: clean code
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/upload/UploadState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
+{
+    public enum UploadState
+    {
+        Success = 0,
+        Fail,
+        Uploading
+    }
+}
