76f438707a2d3538a745f635f0891c6b2a8e62a1
杨令阁 <adsioj@163.com>
2020-07-02 14:00:39 +0800

    fix(module: upload): button ref

--- components/upload/Upload.razor.cs
+++ components/upload/Upload.razor.cs
@@ -56,6 +56,8 @@ namespace AntDesign
 
         private ElementReference _file;
 
+        private ElementReference _btn;
+
         private string _fileId = Guid.NewGuid().ToString();
 
         protected override Task OnInitializedAsync()
