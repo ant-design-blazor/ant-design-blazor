b04589a0cf3ecd8e71c019fa603c4cb319c5ccb7
James Yeung <shunjiey@hotmail.com>
2020-07-05 00:08:33 +0800

    refactor(module: statistic): simplify format string for countdown (#309)
    * refactor(module: statistic): simplify format string for countdown
    
    * fix: wrong namespace

--- components/statistic/CountDown.razor.cs
+++ components/statistic/CountDown.razor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading;
 using Microsoft.AspNetCore.Components;
 
@@ -6,9 +7,11 @@ namespace AntDesign
 {
     public partial class CountDown : StatisticComponentBase<DateTime>
     {
-        [Parameter] public string Format { get; set; } = @"hh\:mm\:ss";
+        [Parameter]
+        public string Format { get; set; } = "hh:mm:ss";
 
-        [Parameter] public EventCallback OnFinish { get; set; }
+        [Parameter]
+        public EventCallback OnFinish { get; set; }
 
         private Timer _timer;
 
@@ -17,9 +20,24 @@ namespace AntDesign
         private TimeSpan _countDown = TimeSpan.Zero;
         private string _format = "";
 
+        /// <summary>
+        /// parse other characters in format string.
+        /// </summary>
+        /// <remarks>refer to https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-timespan-format-strings#other-characters</remarks>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        private static string ParseSpanTimeFormatString(string format)
+        {
+            if (string.IsNullOrWhiteSpace(format))
+            {
+                return format;
+            }
+            return Regex.Replace(format, "[^d|^h|^m|^s|^f|^F]+", "'$0'");
+        }
+
         protected override void OnInitialized()
         {
-            _format = Format;
+            _format = ParseSpanTimeFormatString(Format);
             _countDown = Value - DateTime.Now;
             _timer = new Timer(StartCountDownForTimeSpan);
             _timer.Change(0, REFRESH_INTERVAL);
@@ -32,7 +50,10 @@ namespace AntDesign
             {
                 _countDown = TimeSpan.Zero;
                 _timer.Dispose();
-                OnFinish.InvokeAsync(o);
+                if (OnFinish.HasDelegate)
+                {
+                    OnFinish.InvokeAsync(o);
+                }
             }
 
             InvokeStateHasChanged();
