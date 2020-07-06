f03a36ff70fae5961da60dcc62400da421688d4f
杨令阁 <adsioj@163.com>
2020-07-06 18:30:43 +0800

    fix(module: Table): when no rows were selected, page turning is abnormal

--- components/table/Table.razor.cs
+++ components/table/Table.razor.cs
@@ -109,6 +109,10 @@ namespace AntDesign
 
         private void ChangeSelection(int[] indexes)
         {
+            if(this._headerSelection == null)
+            {
+                return;
+            }
             if (indexes == null || !indexes.Any())
             {
                 this._headerSelection.RowSelections.ForEach(x => x.Check(false));
