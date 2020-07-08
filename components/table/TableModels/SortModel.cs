84a2ab1806c61f58e412a996b7583fa15937e1d9
James Yeung <shunjiey@hotmail.com>
2020-07-08 16:12:49 +0800

    feat(module: table): sortable (#319)
    * feat(module: table): add sorter
    
    * feat: add sorter tooltip
    
    * feat: add table change event

---
+++ components/table/TableModels/SortModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using AntDesign.Internal;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace AntDesign.TableModels
+{
+    public class SortModel<TField> : ITableSortModel
+    {
+        private readonly FieldIdentifier _fieldIdentifier;
+
+        public SortType SortType { get; private set; }
+
+        public int Priority { get; }
+
+        public string FieldName { get; }
+
+        public SortModel(FieldIdentifier fieldIdentifier, int priority, string sort)
+        {
+            this._fieldIdentifier = fieldIdentifier;
+            this.FieldName = fieldIdentifier.FieldName;
+            this.Priority = priority;
+            this.SortType = SortType.Parse(sort) ?? SortType.None;
+        }
+
+        IOrderedQueryable<TItem> ITableSortModel.Sort<TItem>(IQueryable<TItem> source)
+        {
+            if (SortType == SortType.None)
+            {
+                return source as IOrderedQueryable<TItem>;
+            }
+
+            var sourceExpression = Expression.Parameter(typeof(TItem));
+            if (_fieldIdentifier.TryGetValidateProperty(out var property))
+            {
+                var propertyExpression = Expression.Property(sourceExpression, property);
+
+                var lambda = Expression.Lambda<Func<TItem, TField>>(propertyExpression, sourceExpression);
+
+                if (SortType == SortType.Ascending)
+                {
+                    return source.OrderBy(lambda);
+                }
+                else
+                {
+                    return source.OrderByDescending(lambda);
+                }
+            }
+
+            return source as IOrderedQueryable<TItem>;
+        }
+
+        void ITableSortModel.SwitchSortType()
+        {
+            SortType = GetNextType();
+        }
+
+        void ITableSortModel.SetSortType(SortType sortType)
+        {
+            this.SortType = SortType;
+        }
+
+        void ITableSortModel.SetSortType(string sortType)
+        {
+            this.SortType = SortType.Parse(sortType);
+        }
+
+        SortType ITableSortModel.NextType()
+        {
+            return GetNextType();
+        }
+
+        private SortType GetNextType()
+        {
+            if (SortType == SortType.None)
+            {
+                return SortType.Ascending;
+            }
+            else if (SortType == SortType.Ascending)
+            {
+                return SortType.Descending; ;
+            }
+            else
+            {
+                return SortType.None;
+            }
+        }
+    }
+}
