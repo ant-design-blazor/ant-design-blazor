a60dd35e1eabfe73bd3a576b4d4858c79f63b09a
钟迪龙 <musicvs@163.com>
2020-07-15 08:53:40 +0800

    feat(module: form): add form-context demo, support FormProvider

---
+++ components/form/FormProvider.razor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AntDesign.Internal;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class FormProvider : AntComponentBase, IFormProvider
+    {
+        [Parameter]
+        public EventCallback<FormProviderFinishEventArgs> OnFormFinish { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        private Dictionary<string, IForm> _forms = new Dictionary<string, IForm>();
+
+        void IFormProvider.AddForm(IForm form)
+        {
+            _forms.Add(form.Name, form);
+
+            form.OnFinishEvent += OnFinish;
+            Console.WriteLine($"------------------AddForm:{_forms.Count}");
+        }
+
+        void IFormProvider.RemoveForm(IForm form)
+        {
+            _forms.Remove(form.Name);
+
+            form.OnFinishEvent += OnFinish;
+            Console.WriteLine($"----------------RemoveForm:{_forms.Count}");
+        }
+
+        private void OnFinish(IForm form)
+        {
+            Console.WriteLine("OnFinish");
+            var args = new FormProviderFinishEventArgs
+            {
+                Forms = _forms,
+                FinishForm = form
+            };
+
+            OnFormFinish.InvokeAsync(args);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            _forms.Values.ForEach(form =>
+            {
+                form.OnFinishEvent -= OnFinish;
+            });
+        }
+
+        public IForm GetForm(string formName)
+        {
+            if (_forms.TryGetValue(formName, out IForm form))
+            {
+                return form;
+            }
+
+            return null;
+        }
+
+        public bool TryGetForm(string formName, out IForm form)
+        {
+            if (_forms.TryGetValue(formName, out form))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ components/form/Internal/IFormProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign.Internal
+{
+    public interface IFormProvider
+    {
+        internal void AddForm(IForm form);
+        internal void RemoveForm(IForm form);
+    }
+}
