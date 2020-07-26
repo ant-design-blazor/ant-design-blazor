b92032dc9fcc7b34d7bf1d561905f1f2a45bacd6
ElderJames <shunjiey@hotmail.com>
2020-07-26 19:10:22 +0800

    docs: add home page

---
+++ site/AntBlazor.Docs/Services/MoreProps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign.Docs.Services
+{
+    public class MoreProps
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Date { get; set; }
+
+        public string Img { get; set; }
+
+        public string Href { get; set; }
+    }
+}
