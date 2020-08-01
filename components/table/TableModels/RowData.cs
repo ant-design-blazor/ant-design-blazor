27c410c1ae12c0e988d464d272a778b3d462a7f1
ElderJames <shunjiey@hotmail.com>
2020-08-01 23:12:38 +0800

    fix(module: table): clear cached items that are not on current page

--- components/table/TableModels/RowData.cs
+++ components/table/TableModels/RowData.cs
@@ -8,13 +8,16 @@ namespace AntDesign.TableModels
     {
         public int RowIndex { get; set; }
 
+        public int PageIndex { get; set; }
+
         public bool Selected { get; set; }
 
         public TItem Data { get; set; }
 
-        public RowData(int rowIndex, TItem data)
+        public RowData(int rowIndex, int pageIndex, TItem data)
         {
             this.RowIndex = rowIndex;
+            this.PageIndex = pageIndex;
             this.Data = data;
         }
     }
