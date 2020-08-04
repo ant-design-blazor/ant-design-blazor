27cc648b44e1e281ec267e771feac5b0fa990823
ElderJames <shunjiey@hotmail.com>
2020-08-05 00:36:45 +0800

    refactor: clean up

--- site/AntBlazor.Docs/Demos/Charts/ChartsDemoData.cs
+++ site/AntBlazor.Docs/Demos/Charts/ChartsDemoData.cs
@@ -1,94 +1,109 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace AntDesign.Charts
 {
     public static class ChartsDemoData
     {
-        public static async Task<FireworksSalesItem[]> FireworksSalesAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<FireworksSalesItem[]> FireworksSalesAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<FireworksSalesItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/fireworks-sales.json").ToString());
+            return await httpClient.GetFromJsonAsync<FireworksSalesItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/fireworks-sales.json").ToString());
         }
 
         public static async Task<SalesItem[]> SalesAsync(NavigationManager navigationManager, HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<SalesItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/sales.json").ToString());
+            return await httpClient.GetFromJsonAsync<SalesItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/sales.json").ToString());
         }
 
         public static async Task<GDPItem[]> GDPAsync(NavigationManager navigationManager, HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<GDPItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/GDP.json").ToString());
+            return await httpClient.GetFromJsonAsync<GDPItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/GDP.json").ToString());
         }
 
-        public static async Task<EmissionsItem[]> EmissionsAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<EmissionsItem[]> EmissionsAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<EmissionsItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/emissions.json").ToString());
+            return await httpClient.GetFromJsonAsync<EmissionsItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/emissions.json").ToString());
         }
 
         public static async Task<OilItem[]> OilAsync(NavigationManager navigationManager, HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<OilItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/oil.json").ToString());
+            return await httpClient.GetFromJsonAsync<OilItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/oil.json").ToString());
         }
 
         public static async Task<IMDBItem[]> IMDBAsync(NavigationManager navigationManager, HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<IMDBItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/IMDB.json").ToString());
+            return await httpClient.GetFromJsonAsync<IMDBItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/IMDB.json").ToString());
         }
 
-        public static async Task<SmokingRateItem[]> SmokingRateAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<SmokingRateItem[]> SmokingRateAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<SmokingRateItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/smoking-rate.json").ToString());
+            return await httpClient.GetFromJsonAsync<SmokingRateItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/smoking-rate.json").ToString());
         }
 
-        public static async Task<BasementProdItem[]> BasementProdAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<BasementProdItem[]> BasementProdAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<BasementProdItem[]>("https://gw.alipayobjects.com/os/basement_prod/a719cd4e-bd40-4878-a4b4-df8a6b531dfe.json");
+            return await httpClient.GetFromJsonAsync<BasementProdItem[]>(
+                "https://gw.alipayobjects.com/os/basement_prod/a719cd4e-bd40-4878-a4b4-df8a6b531dfe.json");
         }
 
-        public static async Task<JobpayingItem[]> JobpayingItemAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<JobpayingItem[]> JobpayingItemAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<JobpayingItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/jobpaying.json").ToString());
+            return await httpClient.GetFromJsonAsync<JobpayingItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/jobpaying.json").ToString());
         }
 
-        public static async Task<ContributionsItem[]> ContributionsItemAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<ContributionsItem[]> ContributionsItemAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
             var baseUrl = navigationManager.ToAbsoluteUri(navigationManager.BaseUri);
-            return await httpClient.GetFromJsonAsync<ContributionsItem[]>(new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/contributions.json").ToString());
+            return await httpClient.GetFromJsonAsync<ContributionsItem[]>(
+                new Uri(baseUrl, "_content/AntDesign.Docs/chartsdata/contributions.json").ToString());
         }
 
-        public static async Task<FertilityItem[]> FertilityItemAsync(NavigationManager navigationManager, HttpClient httpClient)
+        public static async Task<FertilityItem[]> FertilityItemAsync(NavigationManager navigationManager,
+            HttpClient httpClient)
         {
-            return await httpClient.GetFromJsonAsync<FertilityItem[]>("https://gw.alipayobjects.com/os/antvdemo/assets/data/fertility.json");
+            return await httpClient.GetFromJsonAsync<FertilityItem[]>(
+                "https://gw.alipayobjects.com/os/antvdemo/assets/data/fertility.json");
         }
     }
+
 #pragma warning disable IDE1006 // 命名样式
+
     public class FireworksSalesItem
     {
-        [JsonPropertyName("Date")]
-        public string Date { get; set; }
+        [JsonPropertyName("Date")] public string Date { get; set; }
 
         public int scales { get; set; }
-
     }
 
     public class SalesItem
     {
-
         public string 城市 { get; set; }
         public decimal 销售额 { get; set; }
     }
@@ -121,13 +136,17 @@ namespace AntDesign.Charts
         public object Revenue { get; set; }
         public object Rating { get; set; }
     }
+
     public class SmokingRateItem
     {
         public string iso3 { get; set; }
+
         [JsonPropertyName("change in female rate")]
         public object change_in_female_rate { get; set; }
+
         [JsonPropertyName("change in male rate")]
         public object change_in_male_rate { get; set; }
+
         public object female_2000 { get; set; }
         public object male_2000 { get; set; }
         public object female_2015 { get; set; }
@@ -149,12 +168,11 @@ namespace AntDesign.Charts
 
     public class BasementProdItem
     {
-        [JsonPropertyName("Month of Year")]
-        public object MonthofYear { get; set; }
-        public object Month_of_Year { get { return MonthofYear; } }
+        [JsonPropertyName("Month of Year")] public object MonthofYear { get; set; }
+
+        public object Month_of_Year => MonthofYear;
         public object District { get; set; }
         public object AQHI { get; set; }
-
     }
 
     public class JobpayingItem
