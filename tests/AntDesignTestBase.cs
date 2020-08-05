393bd2891b77782e821d46c6f58716038d30897c
Diego Frata <d@frata.io>
2020-08-05 10:06:14 +0100

    test(button): add some button tests (#460)
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ tests/AntDesignTestBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntDesign.Tests
+{
+    public class AntDesignTestBase : IDisposable
+    {
+        public TestContext Context { get; }
+        public TestNavigationManager NavigationManager { get; }
+
+        public AntDesignTestBase()
+        {
+            Context = new TestContext();
+            NavigationManager = new TestNavigationManager();
+
+            Context.Services.AddScoped<NavigationManager>(sp => NavigationManager);
+            Context.Services.AddAntDesign();
+        }
+
+        public void Dispose()
+        {
+            Context?.Dispose();
+        }
+    }
+}
