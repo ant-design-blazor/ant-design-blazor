c3086fbbf4b7c83b74309bc628ee007bc3af0ed3
zxyao <zxyao145@gmail.com>
2020-08-22 09:24:55 +0800

    feat(module: modal): support getting the result from confirm (#476)
    * feat(module: confirm): support blocking to get the result of confirm
    
    * refactor: change modalservice Confirm return value,add ConfirmAsync FUNC
    
    * feat: add ConfirmService
    
    * fix: button text
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/modal/config/ConfirmButtonOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using OneOf;
+
+namespace AntDesign
+{
+    /// <summary>
+    /// config the confirm button's properties
+    /// </summary>
+    public class ConfirmButtonOptions
+    {
+        /// <summary>
+        /// the leftmost button properties in LTR layout
+        /// </summary>
+        public ButtonProps Button1Props { get; set; }
+
+        /// <summary>
+        /// the secondary button properties in LTR layout
+        /// </summary>
+        public ButtonProps Button2Props { get; set; }
+
+        /// <summary>
+        /// the third button properties in LTR layout
+        /// </summary>
+        public ButtonProps Button3Props { get; set; }
+    }
+}
