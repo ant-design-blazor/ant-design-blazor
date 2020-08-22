f2c1af6e9ee50c9b0fd16727797e1489b60880df
zxyao <zxyao145@gmail.com>
2020-08-22 09:24:55 +0800

    feat(module: modal): support getting the result from confirm (#476)
    * feat(module: confirm): support blocking to get the result of confirm
    
    * refactor: change modalservice Confirm return value,add ConfirmAsync FUNC
    
    * feat: add ConfirmService
    
    * fix: button text
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

---
+++ components/modal/type/ConfirmButtons.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
+{
+
+    /// <summary>
+    /// Confirm footer buttons type
+    /// </summary>
+    public enum ConfirmButtons
+    {
+        /// <summary>
+        /// Only a OK button
+        /// </summary>
+        OK = 0,
+
+        #region two buttons
+
+        /// <summary>
+        /// two buttons: OK and a Cancel
+        /// </summary>
+        OKCancel = 10,
+
+        /// <summary>
+        /// two buttons: Yes and No 
+        /// </summary>
+        YesNo = 11,
+
+        /// <summary>
+        /// two buttons: Retry and Cancel
+        /// </summary>
+        RetryCancel = 12,
+
+        #endregion
+
+        #region three buttons
+
+        /// <summary>
+        /// three buttons: Abort, Retry and Ignore
+        /// </summary>
+        AbortRetryIgnore = 20,
+
+        /// <summary>
+        /// three buttons: Yes, No and Cancel
+        /// </summary>
+        YesNoCancel = 21,
+
+        #endregion
+    }
+}
