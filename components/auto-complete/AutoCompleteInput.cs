7afe43890bd5d39bfece6c0fea4cbb6a02026f6e
TimChen <TimChen44@users.noreply.github.com>
2020-08-26 22:37:08 +0800

    feat: auto complete refactor (#529)
    * feat: auto complete refactor
    
    * feat: autocomplete Refactor
    
    * feat: autocomplete property
    
    * feat: autocomplate
    
    * feat: autocomplete
    
    * feat: autocomplete
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/auto-complete/AutoCompleteInput.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AntDesign
+{
+    public class AutoCompleteInput<TValue> : Input<TValue>, IAutoCompleteInput
+    {
+
+        [CascadingParameter]
+        public IAutoCompleteRef AutoComplete { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (AutoComplete != null) AutoComplete?.SetInputComponent(this);
+        }
+
+        internal override async Task OnFocusAsync(FocusEventArgs e)
+        {
+            if (AutoComplete != null) await AutoComplete?.InputFocus(e);
+
+            await base.OnFocusAsync(e);
+
+        }
+
+        internal override async Task OnBlurAsync(FocusEventArgs e)
+        {
+            if (AutoComplete != null) await AutoComplete?.InputBlur(e);
+
+            await base.OnBlurAsync(e);
+        }
+
+        protected override async Task OnkeyDownAsync(KeyboardEventArgs args)
+        {
+            await base.OnkeyDownAsync(args);
+
+            if (AutoComplete != null) await AutoComplete?.InputKeyDown(args);
+        }
+
+
+        protected override async void OnInputAsync(ChangeEventArgs args)
+        {
+            base.OnInputAsync(args);
+
+            if (AutoComplete != null) await AutoComplete?.InputInput(args);
+        }
+
+
+        #region IAutoCompleteInput
+
+        public void SetValue(object value)
+        {
+            this.CurrentValue = (TValue)value;
+        }
+
+        #endregion
+    }
+}
