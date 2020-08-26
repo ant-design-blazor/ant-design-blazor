2a70867b027832716e8f1c7dc72840d070c6f736
TimChen <TimChen44@users.noreply.github.com>
2020-08-26 22:37:08 +0800

    feat: auto complete refactor (#529)
    * feat: auto complete refactor
    
    * feat: autocomplete Refactor
    
    * feat: autocomplete property
    
    * feat: autocomplate
    
    * feat: autocomplete
    
    * feat: autocomplete
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/auto-complete/AutoCompleteOptGroup.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using OneOf;
+
+namespace AntDesign
+{
+    public partial class AutoCompleteOptGroup : AntDomComponentBase
+    {
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public RenderFragment LabelFragment { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
