cbb1a15265d776f791d616be2f2e1d8bc9ac96d3
TimChen <TimChen44@users.noreply.github.com>
2020-08-26 22:37:08 +0800

    feat: auto complete refactor (#529)
    * feat: auto complete refactor
    
    * feat: autocomplete Refactor
    
    * feat: autocomplete property
    
    * feat: autocomplate
    
    * feat: autocomplete
    
    * feat: autocomplete
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/auto-complete/IAutoCompleteRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace AntDesign
+{
+    public interface IAutoCompleteRef
+    {
+        void SetInputComponent(IAutoCompleteInput input);
+
+        Task InputFocus(FocusEventArgs e);
+
+        Task InputBlur(FocusEventArgs e);
+
+        Task InputInput(ChangeEventArgs args);
+
+        Task InputKeyDown(KeyboardEventArgs args);
+    }
+}
