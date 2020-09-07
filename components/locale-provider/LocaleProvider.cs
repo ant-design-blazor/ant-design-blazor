9a515fb2d0b61e4a915b0227e05400de0d4283f1
ElderJames <shunjiey@hotmail.com>
2020-09-07 16:45:37 +0800

    fix: default language

--- components/locale-provider/LocaleProvider.cs
+++ components/locale-provider/LocaleProvider.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using AntDesign.Locales;
 
 namespace AntDesign
@@ -11,15 +14,28 @@ namespace AntDesign
     {
         public static Locale CurrentLocale => GetCurrentLocale();
 
+        public static string DefaultLanguage = "en-US";
+
         private static readonly ConcurrentDictionary<string, Locale> _localeCache = new ConcurrentDictionary<string, Locale>();
         private static Assembly _resourcesAssembly = typeof(LocaleProvider).Assembly;
 
+        private static readonly IDictionary<string, string> _availableResources = _resourcesAssembly
+               .GetManifestResourceNames()
+            .Select(x => Regex.Match(x, @"^.*locales\.(.+)\.json"))
+            .Where(x => x.Success)
+            .ToDictionary(x => x.Groups[1].Value, x => x.Value);
+
         public static Locale GetCurrentLocale()
         {
-            var current = CultureInfo.DefaultThreadCurrentCulture;
-            return _localeCache.GetOrAdd(current.Name, key =>
+            var currentCulture = CultureInfo.DefaultThreadCurrentCulture?.Name;
+            if (string.IsNullOrWhiteSpace(currentCulture) || !_availableResources.ContainsKey(currentCulture))
             {
-                string fileName = $"{_resourcesAssembly.GetName().Name}.locales.{key}.json";
+                currentCulture = DefaultLanguage;
+            }
+
+            return _localeCache.GetOrAdd(currentCulture, key =>
+            {
+                string fileName = _availableResources[key];
                 using var fileStream = _resourcesAssembly.GetManifestResourceStream(fileName);
                 if (fileStream == null) return null;
                 using var streamReader = new StreamReader(fileStream);
