a03df1c1369ef144361b1177b11b99e650344bb9
James Yeung <shunjiey@hotmail.com>
2020-09-07 22:46:50 +0800

    feat: support for globalization & localization (#578)
    * feat: add locale
    
    * feat: add locale interfaces
    
    * fix: add locale interfaces for components
    
    * fix: use local classes instead of interfaces
    
    * feat: implement the locale provider
    
    * feat: make some components to support localization
    
    * fix: default language
    
    * fix: test
    
    * chore: remove the useless reference

---
+++ components/locale-provider/Locale.cs
@@ -0,0 +1,39 @@
+﻿using AntDesign.Form.Locale;
+
+namespace AntDesign.Locales
+{
+    public class Locale
+    {
+        public PaginationLocale Pagination { get; set; }
+
+        public DatePickerLocale DatePicker { get; set; }
+
+        public TimePickerLocale TimePicker { get; set; }
+
+        public DatePickerLocale Calendar { get; set; }
+
+        public TableLocale Table { get; set; }
+
+        public ModalLocale Modal { get; set; }
+
+        public PopconfirmLocale Popconfirm { get; set; }
+
+        public TransferLocale Transfer { get; set; }
+
+        public SelectLocale Select { get; set; }
+
+        public UploadLocale Upload { get; set; }
+
+        public GlobalLocale Global { get; set; }
+
+        public PageHeaderLocale PageHeader { get; set; }
+
+        public EmptyLocale Empty { get; set; }
+
+        public IconLocale Icon { get; set; }
+
+        public TextLocale Text { get; set; }
+
+        public FormLocale Form { get; set; }
+    }
+}
---
+++ components/upload/UploadLocale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
+{
+    public class UploadLocale
+    {
+        public string Uploading { get; set; }
+
+        public string RemoveFile { get; set; }
+
+        public string UploadError { get; set; }
+
+        public string PreviewFile { get; set; }
+
+        public string DownloadFile { get; set; }
+    }
+}
