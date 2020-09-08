4b1243cec2a9408439d2070788ba3f380e926e89
James Yeung <shunjiey@hotmail.com>
2020-09-09 00:52:14 +0800

    fix(hosting): server prerendering issue (#583)
    * refactor: rename JSInterop constants
    
    * docs: fix typo
    
    * fix: server prerendering issue

--- components/core/JsInterop/JSInteropConstants.cs
+++ components/core/JsInterop/JSInteropConstants.cs
@@ -9,81 +9,80 @@ namespace AntDesign
     {
         private const string FUNC_PREFIX = "AntDesign.interop.";
 
-        public static string getDomInfo => $"{FUNC_PREFIX}getDomInfo";
+        public static string GetDomInfo => $"{FUNC_PREFIX}getDomInfo";
 
-        public static string triggerEvent => $"{FUNC_PREFIX}triggerEvent";
+        public static string TriggerEvent => $"{FUNC_PREFIX}triggerEvent";
 
-        public static string addFileClickEventListener => $"{FUNC_PREFIX}addFileClickEventListener";
+        public static string AddFileClickEventListener => $"{FUNC_PREFIX}addFileClickEventListener";
 
-        public static string removeFileClickEventListener => $"{FUNC_PREFIX}removeFileClickEventListener";
+        public static string RemoveFileClickEventListener => $"{FUNC_PREFIX}removeFileClickEventListener";
 
-        public static string clearFile => $"{FUNC_PREFIX}clearFile";
+        public static string ClearFile => $"{FUNC_PREFIX}clearFile";
 
-        public static string uploadFile => $"{FUNC_PREFIX}uploadFile";
+        public static string UploadFile => $"{FUNC_PREFIX}uploadFile";
 
-        public static string getObjectURL => $"{FUNC_PREFIX}getObjectURL";
+        public static string GetObjectURL => $"{FUNC_PREFIX}getObjectURL";
 
-        public static string getFileInfo => $"{FUNC_PREFIX}getFileInfo";
+        public static string GetFileInfo => $"{FUNC_PREFIX}getFileInfo";
 
-        public static string getBoundingClientRect => $"{FUNC_PREFIX}getBoundingClientRect";
+        public static string GetBoundingClientRect => $"{FUNC_PREFIX}getBoundingClientRect";
 
-        public static string addDomEventListener => $"{FUNC_PREFIX}addDomEventListener";
+        public static string AddDomEventListener => $"{FUNC_PREFIX}addDomEventListener";
 
-        public static string matchMedia => $"{FUNC_PREFIX}matchMedia";
+        public static string MatchMedia => $"{FUNC_PREFIX}matchMedia";
 
-        public static string copy => $"{FUNC_PREFIX}copy";
+        public static string Copy => $"{FUNC_PREFIX}copy";
 
-        public static string log => $"{FUNC_PREFIX}log";
+        public static string Log => $"{FUNC_PREFIX}log";
 
-        public static string focus => $"{FUNC_PREFIX}focus";
+        public static string Focus => $"{FUNC_PREFIX}focus";
 
-        public static string blur => $"{FUNC_PREFIX}blur";
+        public static string Blur => $"{FUNC_PREFIX}blur";
 
-        public static string backTop => $"{FUNC_PREFIX}BackTop";
+        public static string BackTop => $"{FUNC_PREFIX}BackTop";
 
-        public static string getFirstChildDomInfo => $"{FUNC_PREFIX}getFirstChildDomInfo";
+        public static string GetFirstChildDomInfo => $"{FUNC_PREFIX}getFirstChildDomInfo";
 
-        public static string addClsToFirstChild => $"{FUNC_PREFIX}addClsToFirstChild";
+        public static string AddClsToFirstChild => $"{FUNC_PREFIX}addClsToFirstChild";
 
-        public static string addDomEventListenerToFirstChild => $"{FUNC_PREFIX}addDomEventListenerToFirstChild";
+        public static string AddDomEventListenerToFirstChild => $"{FUNC_PREFIX}addDomEventListenerToFirstChild";
 
-        public static string addElementToBody => $"{FUNC_PREFIX}addElementToBody";
+        public static string AddElementToBody => $"{FUNC_PREFIX}addElementToBody";
 
-        public static string delElementFromBody => $"{FUNC_PREFIX}delElementFromBody";
+        public static string DelElementFromBody => $"{FUNC_PREFIX}delElementFromBody";
 
-        public static string addElementTo => $"{FUNC_PREFIX}addElementTo";
+        public static string AddElementTo => $"{FUNC_PREFIX}addElementTo";
 
-        public static string delElementFrom => $"{FUNC_PREFIX}delElementFrom";
+        public static string DelElementFrom => $"{FUNC_PREFIX}delElementFrom";
 
-        public static string getActiveElement => $"{FUNC_PREFIX}getActiveElement";
+        public static string GetActiveElement => $"{FUNC_PREFIX}getActiveElement";
 
-        public static string focusDialog => $"{FUNC_PREFIX}focusDialog";
+        public static string FocusDialog => $"{FUNC_PREFIX}focusDialog";
 
-        public static string getWindow = $"{FUNC_PREFIX}getWindow";
+        public static string GetWindow = $"{FUNC_PREFIX}getWindow";
 
-        public static string disableBodyScroll => $"{FUNC_PREFIX}disableBodyScroll";
+        public static string DisableBodyScroll => $"{FUNC_PREFIX}disableBodyScroll";
 
-        public static string enableBodyScroll => $"{FUNC_PREFIX}enableBodyScroll";
+        public static string EnableBodyScroll => $"{FUNC_PREFIX}enableBodyScroll";
 
         public static string CreateIconFromfontCN => $"{FUNC_PREFIX}createIconFromfontCN";
 
-        public static string getScroll => $"{FUNC_PREFIX}getScroll";
+        public static string GetScroll => $"{FUNC_PREFIX}getScroll";
 
-        public static string getInnerText => $"{FUNC_PREFIX}getInnerText";
+        public static string GetInnerText => $"{FUNC_PREFIX}getInnerText";
 
-        public static string getCursorXY => $"{FUNC_PREFIX}getCursorXY";
+        public static string GetCursorXY => $"{FUNC_PREFIX}getCursorXY";
 
-        public static string disposeObj => $"{FUNC_PREFIX}disposeObj";
+        public static string DisposeObj => $"{FUNC_PREFIX}disposeObj";
 
         #region Draggable Modal
 
-        public static string enableDraggable => $"{FUNC_PREFIX}enableDraggable";
+        public static string EnableDraggable => $"{FUNC_PREFIX}enableDraggable";
 
-        public static string disableDraggable => $"{FUNC_PREFIX}disableDraggable";
+        public static string DisableDraggable => $"{FUNC_PREFIX}disableDraggable";
 
-        public static string resetModalPosition => $"{FUNC_PREFIX}resetModalPosition";
+        public static string ResetModalPosition => $"{FUNC_PREFIX}resetModalPosition";
 
-        #endregion
-
+        #endregion Draggable Modal
     }
 }
