a5e203e60b986f621b7340052d5357feacf99685
Diego Frata <diego.frata@starlizard.com>
2020-09-11 07:37:38 +0100

    feat(module: tabs): add support to ForceRender (#589)
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ tests/tabs/TabsTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using AntDesign.JsInterop;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Moq;
+using Xunit;
+
+namespace AntDesign.Tests.tabs
+{
+    public class TabsTests : AntDesignTestBase
+    {
+        private IRenderedComponent<Tabs> CreateTabs(Action<RenderTreeBuilder> childContent)
+        {
+            var jsRuntime = new Mock<IJSRuntime>();
+            jsRuntime.Setup(u => u.InvokeAsync<Element>(JSInteropConstants.GetDomInfo, It.IsAny<object[]>()))
+                .ReturnsAsync(new Element());
+
+            Context.Services.AddScoped(_ => jsRuntime.Object);
+
+            var cut = Context.RenderComponent<Tabs>(tabs => tabs
+                .Add(x => x.DefaultActiveKey, "2")
+                .Add(b => b.ChildContent, b => childContent(b))
+            );
+
+            return cut;
+        }
+
+        private static RenderFragment CreateTabPanel(string key, Action<ComponentParameterBuilder<TabPane>> configure = null)
+        {
+            var tabPane1Builder = new ComponentParameterBuilder<TabPane>()
+                .Add(x => x.Key, key)
+                .Add(x => x.Tab, RenderFragmentHelper.ToRenderFragment($"Tab {key}"))
+                .Add(x => x.ChildContent, RenderFragmentHelper.ToRenderFragment($"Content {key}"));
+
+            configure?.Invoke(tabPane1Builder);
+
+            return tabPane1Builder.Build().ToComponentRenderFragment<TabPane>();
+        }
+
+        [Fact]
+        public void Should_only_render_the_active_pane_when_ForceRender_is_false()
+        {
+            var cut = CreateTabs(p =>
+            {
+                var tabPane1 = CreateTabPanel("1");
+                var tabPane2 = CreateTabPanel("2");
+
+                tabPane1(p);
+                tabPane2(p);
+            });
+
+            var renderedPanes = cut.FindAll(".ant-tabs-tabpane");
+
+            Assert.Equal(1, renderedPanes.Count);
+        }
+
+        [Fact]
+        public void Should_render_the_all_panes_when_ForceRender_is_true()
+        {
+            var cut = CreateTabs(p =>
+            {
+                var tabPane1 = CreateTabPanel("1", x => x.Add(y => y.ForceRender, true));
+                var tabPane2 = CreateTabPanel("2", x => x.Add(y => y.ForceRender, true));
+
+                tabPane1(p);
+                tabPane2(p);
+            });
+
+            var renderedPanes = cut.FindAll(".ant-tabs-tabpane");
+
+            Assert.Equal(2, renderedPanes.Count);
+        }
+
+        [Fact]
+        public void Should_preserve_tab_panels_once_they_have_been_rendered()
+        {
+            var cut = CreateTabs(p =>
+            {
+                var tabPane1 = CreateTabPanel("1");
+                var tabPane2 = CreateTabPanel("2");
+
+                tabPane1(p);
+                tabPane2(p);
+            });
+
+            var renderedPanes = cut.FindAll(".ant-tabs-tabpane", true);
+            Assert.Equal(1, renderedPanes.Count);
+
+            cut.SetParametersAndRender(p => p.Add(x => x.ActiveKey, "1"));
+
+            Assert.Equal(2, renderedPanes.Count);
+        }
+    }
+}
