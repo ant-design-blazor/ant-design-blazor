50a35669bbd6cb908c2068e0bb8cbf2fc1487d9b
ElderJames <shunjiey@hotmail.com>
2020-09-20 01:32:51 +0800

    fix(module: card): grid and tabs has wrong styles

---
+++ components/card/CardGrid.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class CardGrid : AntDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Hoverable { get; set; }
+
+        [CascadingParameter]
+        public Card Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Parent?.MarkHasGrid();
+
+            ClassMapper.Add("ant-card-grid")
+                .If("ant-card-hoverable", () => Hoverable);
+
+            base.OnInitialized();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
