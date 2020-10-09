585fb02b156db15527d4c7e6a6fc6b6d9f8cc286
TimChen <TimChen44@users.noreply.github.com>
2020-10-09 15:13:32 +0800

    refactor: tree (#672)
    * feat: refactor tree
    
    * feat: refactor tree
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/tree/TreeIndent.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class TreeIndent
+    {
+        /// <summary>
+        /// 树控件本身
+        /// </summary>
+        [CascadingParameter(Name = "Tree")]
+        public Tree TreeComponent { get; set; }
+
+        /// <summary>
+        /// 当前节点
+        /// </summary>
+        [CascadingParameter(Name = "SelfNode")]
+        public TreeNode SelfNode { get; set; }
+
+        [Parameter]
+        public int TreeLevel { get; set; }
+
+
+    }
+}
---
+++ site/AntBlazor.Docs/Demos/Components/Tree/demo/template.md
@@ -0,0 +1,14 @@
+---
+order: 10
+title:
+  zh-CN: 模板
+  en-US: Template
+---
+
+## zh-CN
+
+使用模板重新定义树中的各元素显示内容
+
+## en-US
+
+Use templates to redefine the display content of each element in the tree
