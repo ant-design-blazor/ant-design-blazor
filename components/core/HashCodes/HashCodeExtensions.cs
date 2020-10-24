8cb3816d9afd33babf1f267f96cf3cdf3490f741
xljiulang <366193849@qq.com>
2020-10-24 20:14:52 +0800

    移除内部Component~类

--- components/core/HashCodes/HashCodeExtensions.cs
+++ components/core/HashCodes/HashCodeExtensions.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Linq;
 using Microsoft.AspNetCore.Components;
 
 namespace AntDesign.Core.HashCodes
@@ -20,36 +19,13 @@ namespace AntDesign.Core.HashCodes
         /// <returns></returns>
         public static int GetParametersHashCode<TComponent>(this TComponent component) where TComponent : ComponentBase
         {
-            return Component<TComponent>.GetParametersHashCode(component);
-        }
-
-        /// <summary>
-        /// 提供组件参数哈希值计算
-        /// </summary>
-        /// <typeparam name="TComponent"></typeparam>
-        static class Component<TComponent>
-        {
-            private static readonly ParameterDescriptor<TComponent>[] _descriptors
-                = typeof(TComponent)
-                    .GetProperties()
-                    .Select(item => new ParameterDescriptor<TComponent>(item))
-                    .Where(item => item.IsParameter)
-                    .ToArray();
-
-            /// <summary>
-            /// 返回组件的参数的HashCode
-            /// </summary>
-            /// <param name="component"></param>
-            /// <returns></returns>
-            public static int GetParametersHashCode(TComponent component)
+            var hashCode = 0;
+            var descriptors = ParameterDescriptor<TComponent>.Descriptors;
+            foreach (var descriptor in descriptors)
             {
-                var hashCode = 0;
-                foreach (var descriptor in _descriptors)
-                {
-                    hashCode ^= descriptor.GetValueHashCode(component);
-                }
-                return hashCode;
+                hashCode ^= descriptor.GetValueHashCode(component);
             }
+            return hashCode;
         }
     }
 }
