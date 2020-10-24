5a9a8bb7ef5aae0ce4c49e15cef5d9cfac7db94e
xljiulang <366193849@qq.com>
2020-10-24 19:58:17 +0800

    为组件增加GetParametersHashCode()扩展方法，以支持控制组件的渲染方式； 为Table组件增加RenderMode的渲染模式参数；

---
+++ components/core/HashCodes/HashCodeExtensions.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign.Core.HashCodes
+{
+    /// <summary>
+    /// 提供组件参数的HashCode计算等功能
+    /// </summary>
+    static class HashCodeExtensions
+    {
+        /// <summary>
+        /// 计算所有参数的HashCode
+        /// </summary>
+        /// <typeparam name="TComponent"></typeparam>
+        /// <param name="component">组件</param>
+        /// <returns></returns>
+        public static int GetParametersHashCode<TComponent>(this TComponent component) where TComponent : ComponentBase
+        {
+            return Component<TComponent>.GetParametersHashCode(component);
+        }
+
+        /// <summary>
+        /// 提供组件参数哈希值计算
+        /// </summary>
+        /// <typeparam name="TComponent"></typeparam>
+        static class Component<TComponent>
+        {
+            private static readonly ParameterDescriptor<TComponent>[] _descriptors
+                = typeof(TComponent)
+                    .GetProperties()
+                    .Select(item => new ParameterDescriptor<TComponent>(item))
+                    .Where(item => item.IsParameter)
+                    .ToArray();
+
+            /// <summary>
+            /// 返回组件的参数的HashCode
+            /// </summary>
+            /// <param name="component"></param>
+            /// <returns></returns>
+            public static int GetParametersHashCode(TComponent component)
+            {
+                var hashCode = 0;
+                foreach (var descriptor in _descriptors)
+                {
+                    hashCode ^= descriptor.GetValueHashCode(component);
+                }
+                return hashCode;
+            }
+        }
+    }
+}
