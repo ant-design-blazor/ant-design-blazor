575a13f56cb04c325ba811f180c14cd64b10db62
xljiulang <366193849@qq.com>
2020-10-24 22:38:30 +0800

    修复HttpClientExtensions反序列化时没有判断内容是否为utf8编码的问题
    (cherry picked from commit f6e4903c9336d0f1a43f1da3fe148e08bb917f3f)

--- components/core/Extensions/HttpClientExtensions.cs
+++ components/core/Extensions/HttpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,14 +19,54 @@ namespace AntDesign.core.Extensions
             var res = await client.GetAsync(requestUri, cancellationToken);
             if (res.IsSuccessStatusCode)
             {
-                var json = await res.Content.ReadAsStreamAsync();
-                return await JsonSerializer.DeserializeAsync<TValue>(json, new JsonSerializerOptions()
+                var utf8Json = await res.Content.ReadAsByteArrayAsync(Encoding.UTF8);
+                return JsonSerializer.Deserialize<TValue>(utf8Json, new JsonSerializerOptions()
                 {
                     PropertyNameCaseInsensitive = true,
-                }, cancellationToken);
+                });
             }
 
             return default;
+        }
+
+
+        /// <summary>
+        /// 读取为二进制数组并转换为指定的编码
+        /// </summary>
+        /// <param name="httpContent"></param>
+        /// <param name="dstEncoding">目标编码</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns></returns>
+        private static async Task<byte[]> ReadAsByteArrayAsync(this HttpContent httpContent, Encoding dstEncoding)
+        {
+            var encoding = httpContent.GetEncoding();
+            var byteArray = await httpContent.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+            return encoding.Equals(dstEncoding)
+                ? byteArray
+                : Encoding.Convert(encoding, dstEncoding, byteArray);
+        }
+
+        /// <summary>
+        /// 获取编码信息
+        /// </summary>
+        /// <param name="httpContent"></param>
+        /// <returns></returns>
+        private static Encoding GetEncoding(this HttpContent httpContent)
+        {
+            var charSet = httpContent.Headers.ContentType?.CharSet;
+            if (string.IsNullOrEmpty(charSet) == true)
+            {
+                return Encoding.UTF8;
+            }
+
+            var span = charSet.AsSpan().TrimStart('"').TrimEnd('"');
+            if (span.Equals(Encoding.UTF8.WebName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Encoding.UTF8;
+            }
+
+            return Encoding.GetEncoding(span.ToString());
         }
     }
 }
