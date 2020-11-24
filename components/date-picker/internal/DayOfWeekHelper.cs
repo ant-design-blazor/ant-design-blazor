022d05ed926c205b758955327c0cba07abf078a0
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2020-11-24 14:06:15 +0100

    code cleanup
    use consts instead of magic numbers, add code comment and use dayOfWeek As param instead of the whole locale object. Remove no longed used method.

--- components/date-picker/internal/DayOfWeekHelper.cs
+++ components/date-picker/internal/DayOfWeekHelper.cs
@@ -1,47 +1,36 @@
 ﻿using System;
-using System.Globalization;
 
 namespace AntDesign
 {
     internal static class DayOfWeekHelper
     {
-        internal static int GetDiffForDayOfWeek(DatePickerLocale locale)
+        private const int DIFF_DEFAULT = 1;
+        private const int DIFF_SATURDAY = 1;
+        private const int DIFF_FRIDAY = 2;
+        private const int DIFF_THURSDAY = 3;
+        private const int DIFF_WEDNESDAY = 4;
+        private const int DIFF_TUESDAY = 5;
+        private const int DIFF_MONDAY = 6;
+        private const int DIFF_SUNDAY = 7;
+
+        /// <summary>
+        ///     Returns the amount of days that have to be added to the start date to get the correct first day of the week.
+        /// </summary>
+        /// <param name="firstDayOfWeek">First day of the week as defined in the current locale.</param>
+        /// <returns>Diff of days.</returns>
+        internal static int GetDiffForDayOfWeek(DayOfWeek firstDayOfWeek)
         {
-            switch (locale.FirstDayOfWeek)
+            switch (firstDayOfWeek)
             {
-                case DayOfWeek.Saturday: return 1 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Friday: return 2 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Thursday: return 3 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Wednesday: return 4 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Tuesday: return 5 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Monday: return 6 + (int)locale.FirstDayOfWeek;
-                case DayOfWeek.Sunday: return 7 + (int)locale.FirstDayOfWeek;
-                default: return 0;
+                case DayOfWeek.Saturday: return DIFF_SATURDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Friday: return DIFF_FRIDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Thursday: return DIFF_THURSDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Wednesday: return DIFF_WEDNESDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Tuesday: return DIFF_TUESDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Monday: return DIFF_MONDAY + (int)firstDayOfWeek;
+                case DayOfWeek.Sunday: return DIFF_SUNDAY + (int)firstDayOfWeek;
+                default: return DIFF_DEFAULT;
             }
         }
-
-        internal static string[] GetShortWeekDays(DatePickerLocale locale, CultureInfo cultureInfo)
-        {
-            DayOfWeek currentDay = DateTime.Now.DayOfWeek;
-            DateTime referenceDay = DateTime.Today;
-
-            if(locale.FirstDayOfWeek != currentDay)
-            {
-                int diff = locale.FirstDayOfWeek - currentDay;
-                referenceDay = referenceDay.AddDays(diff);
-            }
-
-            return new[] { referenceDay.GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(1).GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(2).GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(3).GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(4).GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(5).GetTwoLetterCode(cultureInfo),
-                           referenceDay.AddDays(6).GetTwoLetterCode(cultureInfo),
-            };
-        }
-
-        private static string GetTwoLetterCode(this DateTime today, CultureInfo cultureInfo)
-            => today.ToString("ddd", cultureInfo).Substring(0, 2);
     }
 }
