7cba869fd6f6747050cc83f85ff5c9f174a1d95d
Zonciu Liang <zonciu@zonciu.com>
2020-12-01 00:09:05 +0800

    docs: simplify form custom validators demo

--- site/AntBlazor.Docs/Demos/Components/Form/demo/customized-form-validator.md
+++ site/AntBlazor.Docs/Demos/Components/Form/demo/customized-form-validator.md
@@ -11,8 +11,13 @@ title:
 
 注意：使用`<Validator>`元素设置验证器时，需要将Form内其他组件用`<ChildContent>`包起来。
 
+自定义验证组件开发见文档：[ASP.NET Core Blazor 窗体和验证 - 验证器组件
+](https://docs.microsoft.com/zh-cn/aspnet/core/blazor/forms-validation?view=aspnetcore-5.0#validator-components)
+
 ## en-US
 
 Setting a custom validator using the Form Validator property or the `<Validator>` element overrides the built-in validator.
 
 Note: When using the `<Validator>` element to set the validator, you need to wrap the other components within the Form with `<ChildContent>`.
+
+For custom validator development, see document: [ASP.NET Core Blazor forms and validation - Validator components](https://docs.microsoft.com/en-US/aspnet/core/blazor/forms-validation?view=aspnetcore-5.0#validator-components)
--- site/AntBlazor.Docs/Demos/Components/Form/objects/CustomValidator.cs
+++ site/AntBlazor.Docs/Demos/Components/Form/objects/CustomValidator.cs
@@ -2,133 +2,26 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+using AntDesign.Docs.Demos.Components.Form.demo;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace AntDesign.Docs
 {
-    /// <summary>
-    /// Source sode from Microsoft.AspNetCore.Components.Forms.ObjectGraphDataAnnotationsValidator
-    /// </summary>
     public class CustomValidator : ComponentBase
     {
-        private static readonly object _validationContextValidatorKey = new object();
-
-        private static readonly object _validatedObjectsKey = new object();
-
-        private ValidationMessageStore _validationMessageStore;
-
         [CascadingParameter]
         internal EditContext EditContext { get; set; }
 
         protected override void OnInitialized()
         {
-            _validationMessageStore = new ValidationMessageStore(EditContext);
-
-            // Perform object-level validation (starting from the root model) on request
-            EditContext.OnValidationRequested += (sender, eventArgs) =>
+            var messages = new ValidationMessageStore(EditContext);
+            EditContext.OnFieldChanged += (sender, args) =>
             {
-                _validationMessageStore.Clear();
-                ValidateObject(EditContext.Model, new HashSet<object>());
+                messages.Clear();
+                messages.Add(args.FieldIdentifier, "Message from custom validator");
                 EditContext.NotifyValidationStateChanged();
             };
-
-            // Perform per-field validation on each field edit
-            EditContext.OnFieldChanged += (sender, eventArgs) =>
-                ValidateField(EditContext, _validationMessageStore, eventArgs.FieldIdentifier);
-        }
-
-        internal void ValidateObject(object value, HashSet<object> visited)
-        {
-            if (value is null)
-            {
-                return;
-            }
-
-            if (!visited.Add(value))
-            {
-                // Already visited this object.
-                return;
-            }
-
-            if (value is IEnumerable<object> enumerable)
-            {
-                var index = 0;
-                foreach (var item in enumerable)
-                {
-                    ValidateObject(item, visited);
-                    index++;
-                }
-
-                return;
-            }
-
-            var validationResults = new List<ValidationResult>();
-            ValidateObject(value, visited, validationResults);
-
-            // Transfer results to the ValidationMessageStore
-            foreach (var validationResult in validationResults)
-            {
-                if (!validationResult.MemberNames.Any())
-                {
-                    _validationMessageStore.Add(new FieldIdentifier(value, string.Empty), validationResult.ErrorMessage);
-                    continue;
-                }
-
-                foreach (var memberName in validationResult.MemberNames)
-                {
-                    var fieldIdentifier = new FieldIdentifier(value, memberName);
-                    _validationMessageStore.Add(fieldIdentifier, validationResult.ErrorMessage);
-                }
-            }
-        }
-
-        private void ValidateObject(object value, HashSet<object> visited, List<ValidationResult> validationResults)
-        {
-            var validationContext = new ValidationContext(value);
-            validationContext.Items.Add(_validationContextValidatorKey, this);
-            validationContext.Items.Add(_validatedObjectsKey, visited);
-            Validator.TryValidateObject(value, validationContext, validationResults, validateAllProperties: true);
-            foreach (var validationResult in validationResults)
-            {
-                validationResult.ErrorMessage += " -- by CustomValidator";
-            }
-        }
-
-        internal static bool TryValidateRecursive(object value, ValidationContext validationContext)
-        {
-            if (validationContext.Items.TryGetValue(_validationContextValidatorKey, out var result) && result is CustomValidator validator)
-            {
-                var visited = (HashSet<object>)validationContext.Items[_validatedObjectsKey];
-                validator.ValidateObject(value, visited);
-
-                return true;
-            }
-
-            return false;
-        }
-
-        private static void ValidateField(EditContext editContext, ValidationMessageStore messages, in FieldIdentifier fieldIdentifier)
-        {
-            // DataAnnotations only validates public properties, so that's all we'll look for
-            var propertyInfo = fieldIdentifier.Model.GetType().GetProperty(fieldIdentifier.FieldName);
-            if (propertyInfo != null)
-            {
-                var propertyValue = propertyInfo.GetValue(fieldIdentifier.Model);
-                var validationContext = new ValidationContext(fieldIdentifier.Model) {MemberName = propertyInfo.Name};
-                var results = new List<ValidationResult>();
-
-                Validator.TryValidateProperty(propertyValue, validationContext, results);
-                messages.Clear(fieldIdentifier);
-                messages.Add(fieldIdentifier, results.Select(result => result.ErrorMessage + " -- by CustomValidator"));
-
-                // We have to notify even if there were no messages before and are still no messages now,
-                // because the "state" that changed might be the completion of some async validation task
-                editContext.NotifyValidationStateChanged();
-            }
         }
     }
 }
