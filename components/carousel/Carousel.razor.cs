907498071e6a0f7c1fe803360c449be3fc7dd0e7
Zhaoli Song <song-zhaoli@outlook.com>
2020-12-23 16:31:37 +0800

    Issue#915,and fix the issue that can not render correctly when change carousel DotPosition

--- components/carousel/Carousel.razor.cs
+++ components/carousel/Carousel.razor.cs
@@ -11,16 +11,31 @@ namespace AntDesign
     public partial class Carousel : AntDomComponentBase
     {
         private const string PrefixCls = "ant-carousel";
-        private string _trackStyle;
-        private string _slickClonedStyle;
-        private string _slickListStyle;
+        private string TrackStyle
+        {
+            get
+            {
+                if (Effect == CarouselEffect.ScrollX && IsHorizontal)
+                {
+                    return $"width: {TotalWidth}px; opacity: 1; transform: translate3d(-{SlickWidth * (IndexOfSlick(ActiveSlick) + 1)}px, 0px, 0px);transition: -webkit-transform 500ms ease 0s;";
+                }
+                else if (Effect == CarouselEffect.ScrollX && !IsHorizontal)
+                {
+                    return $"height: {TotalHeight}px; opacity: 1; transform: translate3d(0px, -{SlickHeight * (IndexOfSlick(ActiveSlick) + 1)}px, 0px);transition: -webkit-transform 500ms ease 0s;";
+                }
+
+                return string.Empty;
+            }
+        }
+        internal string SlickClonedStyle => $"width: {SlickWidth}px;";
+        private string SlickListStyle => IsHorizontal ? string.Empty : $"height: {SlickHeight}px";
         private ElementReference _ref;
-        private int _slickWidth = -1;
-        private int _slickHeight = -1;
-        private int _totalWidth = -1;
-        private int _totalHeight = -1;
+        internal int SlickWidth { get; set; } = -1;
+        private int SlickHeight { get; set; } = -1;
+        private int TotalWidth => SlickWidth * (_slicks.Count * 2 + 1);
+        private int TotalHeight => SlickHeight * (_slicks.Count * 2 + 1);
         private List<CarouselSlick> _slicks = new List<CarouselSlick>();
-        private CarouselSlick _activeSlick;
+        internal CarouselSlick ActiveSlick { get; set; }
         private Timer _timer;
         private ClassMapper SlickSliderClassMapper { get; } = new ClassMapper();
         private bool IsHorizontal => DotPosition == CarouselDotPosition.Top || DotPosition == CarouselDotPosition.Bottom;
@@ -52,9 +67,9 @@ namespace AntDesign
 
         [Inject] public DomEventService DomEventService { get; set; }
 
-        public void Next() => GoTo(_slicks.IndexOf(_activeSlick) + 1);
+        public void Next() => GoTo(_slicks.IndexOf(ActiveSlick) + 1);
 
-        public void Previous() => GoTo(_slicks.IndexOf(_activeSlick) - 1);
+        public void Previous() => GoTo(_slicks.IndexOf(ActiveSlick) - 1);
 
         public void GoTo(int index)
         {
@@ -67,7 +82,7 @@ namespace AntDesign
                 index = _slicks.Count - 1;
             }
 
-            Activate(index);
+            Activate(_slicks[index]);
         }
 
         private void SetClass()
@@ -112,42 +127,13 @@ namespace AntDesign
         private async void Resize(JsonElement e = default)
         {
             DomRect listRect = await JsInvokeAsync<DomRect>(JSInteropConstants.GetBoundingClientRect, _ref);
-            if ((_slickWidth != (int)listRect.width && IsHorizontal)
-                || (_slickHeight != (int)listRect.height && !IsHorizontal)
-                || IsHorizontal && !string.IsNullOrEmpty(_slickListStyle)
-                || !IsHorizontal && string.IsNullOrEmpty(_slickListStyle))
+            if ((SlickWidth != (int)listRect.width && IsHorizontal)
+                || (SlickHeight != (int)listRect.height && !IsHorizontal)
+                || IsHorizontal && !string.IsNullOrEmpty(SlickListStyle)
+                || !IsHorizontal && string.IsNullOrEmpty(SlickListStyle))
             {
-                _slickWidth = (int)listRect.width;
-                _slickHeight = (int)listRect.height;
-                _totalWidth = _slickWidth * (_slicks.Count * 2 + 1);
-                _totalHeight = _slickHeight * (_slicks.Count * 2 + 1);
-                if (Effect == CarouselEffect.ScrollX)
-                {
-                    if (IsHorizontal)
-                    {
-                        _trackStyle = $"width: {_totalWidth}px; opacity: 1; transform: translate3d(-{_slickWidth}px, 0px, 0px); transition: -webkit-transform 500ms ease 0s;";
-                    }
-                    else
-                    {
-                        _trackStyle = $"height: {_totalHeight}px; opacity: 1; transform: translate3d(0px, -{_slickHeight}px, 0px); transition: -webkit-transform 500ms ease 0s;";
-                    }
-                }
-                else
-                {
-                    if (IsHorizontal)
-                    {
-                        _trackStyle = $"width: {_totalWidth}px; opacity: 1;";
-                    }
-                    else
-                    {
-                        _trackStyle = $"height: {_totalHeight}px; opacity: 1;";
-                    }
-                }
-
-                _slickListStyle = IsHorizontal ? string.Empty : $"height: {_slickHeight}px";
-                _slickClonedStyle = $"width: {_slickWidth}px;";
-
-                Activate(_slicks.IndexOf(_activeSlick));
+                SlickWidth = (int)listRect.width;
+                SlickHeight = (int)listRect.height;
                 StateHasChanged();
             }
         }
@@ -155,77 +141,46 @@ namespace AntDesign
         internal void RemoveSlick(CarouselSlick slick)
         {
             _slicks.Remove(slick);
+            StateHasChanged();
         }
 
         internal void AddSlick(CarouselSlick slick)
         {
             _slicks.Add(slick);
-            if (_activeSlick == null)
+            if (ActiveSlick == null)
             {
-                Activate(0);
+                Activate(_slicks[0]);
             }
+            StateHasChanged();
         }
 
-        private int Activate(int index, string transition = " transition: -webkit-transform 500ms ease 0s;")
+        internal int IndexOfSlick(CarouselSlick slick)
         {
-            if (Effect == CarouselEffect.ScrollX)
-            {
-                if (IsHorizontal)
-                {
-                    _trackStyle = $"width: {_totalWidth}px; opacity: 1; transform: translate3d(-{_slickWidth * (index + 1)}px, 0px, 0px);{transition}";
-                }
-                else
-                {
-                    _trackStyle = $"height: {_totalHeight}px; opacity: 1; transform: translate3d(0px, -{_slickHeight * (index + 1)}px, 0px);{transition}";
-                }
-            }
+            return _slicks.IndexOf(slick);
+        }
 
+        private void Activate(CarouselSlick slick)
+        {
+            this.ActiveSlick = slick;
+        }
+
+        private async void AutoplaySlick(object state)
+        {
+            var index = IndexOfSlick(ActiveSlick) + 1;
             if (index == _slicks.Count)
             {
                 index = 0;
             }
 
-            if (_slicks.Count > 0)
-            {
-                CarouselSlick slick = _slicks[index];
-                _slicks.ForEach(s =>
-                {
-                    if (s == slick)
-                    {
-                        _activeSlick = s;
-                        s.Activate();
-                    }
-                    else
-                    {
-                        s.Deactivate();
-                    }
-                });
-            }
-
-            return index;
-        }
-
-        private async void AutoplaySlick(object state)
-        {
-            int newIndex = _slicks.IndexOf(_activeSlick) + 1;
-
-            int realIndex = Activate(newIndex);
+            this.Activate(_slicks[index]);
 
             // The current thread is not associated with the Dispatcher.
             // Use InvokeAsync() to switch execution to the Dispatcher when triggering rendering or component state
             await InvokeAsync(() => StateHasChanged());
 
-            if (realIndex == 0 && Effect == CarouselEffect.ScrollX)
+            if (index == 0 && Effect == CarouselEffect.ScrollX)
             {
                 await Task.Delay((int)Autoplay.TotalMilliseconds / 2);
-                if (IsHorizontal)
-                {
-                    _trackStyle = $"width: {_totalWidth}px; opacity: 1; transform: translate3d(-{_slickWidth}px, 0px, 0px);";
-                }
-                else
-                {
-                    _trackStyle = $"height: {_totalHeight}px; opacity: 1; transform: translate3d(0px, -{_slickHeight}px, 0px);";
-                }
             }
 
             await InvokeAsync(() => StateHasChanged());
