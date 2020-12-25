48e04f255a1b59a265865152e92b840a2643f9e7
zxyao <zxyao145@gmail.com>
2020-12-26 00:30:34 +0800

    fix: pickup missing pr/799

--- components/modal/modalDialog/Modal.razor.cs
+++ components/modal/modalDialog/Modal.razor.cs
@@ -13,6 +13,8 @@ namespace AntDesign
     /// </summary>
     public partial class Modal
     {
+        internal static HashSet<Modal> ReusedModals = new HashSet<Modal>();
+
         #region Parameter
 
         /// <summary>
@@ -282,5 +284,19 @@ namespace AntDesign
         }
 
         #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task OnParametersSetAsync()
+        {
+            if (!DestroyOnClose && !ReusedModals.Contains(this))
+            {
+                ReusedModals.Add(this);
+            }
+
+            await base.OnParametersSetAsync();
+        }
     }
 }
--- components/modal/modalDialog/ModalServiceForModal.cs
+++ components/modal/modalDialog/ModalServiceForModal.cs
@@ -1,17 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
 
 namespace AntDesign
 {
     /// <summary>
     /// create and open a Modal dialog
     /// </summary>
-    public partial class ModalService
+    public partial class ModalService: IDisposable
     {
         internal event Func<ModalRef, Task> OnModalOpenEvent;
-
         internal event Func<ModalRef, Task> OnModalCloseEvent;
+
+        private readonly NavigationManager _navigationManager;
+        private readonly IJSRuntime _jsRuntime;
+        internal static HashSet<ModalRef> ReusedModals = new HashSet<ModalRef>();
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public ModalService(NavigationManager navigationManager, IJSRuntime jsRuntime)
+        {
+            _navigationManager = navigationManager;
+            _navigationManager.LocationChanged += NavigationManager_LocationChanged;
+            _jsRuntime = jsRuntime;
+        }
+
+        /// <summary>
+        /// Destroy all reused Modal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void NavigationManager_LocationChanged(object sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
+        {
+            if (Modal.ReusedModals.Count > 0)
+            {
+                // Since Modal cannot be captured, it can only be removed through JS
+                await _jsRuntime.InvokeVoidAsync(JSInteropConstants.DestroyAllDialog);
+                Modal.ReusedModals.Clear();
+            }
+        }
 
         /// <summary>
         /// Create and open a Modal
@@ -64,6 +95,7 @@ namespace AntDesign
         internal Task<ModalRef> CreateOrOpenModalAsync(ModalRef modalRef)
         {
             OnModalOpenEvent?.Invoke(modalRef);
+            ReusedModals.Add(modalRef);
             return Task.FromResult(modalRef);
         }
 
@@ -79,6 +111,15 @@ namespace AntDesign
                 return OnModalCloseEvent.Invoke(modalRef);
             }
             return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Implement the interface IDisposable
+        /// </summary>
+        public void Dispose()
+        {
+            _navigationManager.LocationChanged -= NavigationManager_LocationChanged;
+            GC.SuppressFinalize(this);
         }
     }
 }
