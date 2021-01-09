43664736f7dae9c33b0bffcbf6e6a32c195f5ad0
James Yeung <shunjiey@hotmail.com>
2021-01-09 23:57:31 +0800

    feat(module: table): fixed header & columns (#954)
    * feat(module: table): support fixed header & columns
    
    * fix layout of the fixed columns & header
    
    * fix scrollbar hidding
    
    * header and body sync scroll
    
    * fix column width calc
    
    * fix demo pagesize
    
    * fix docs

--- components/table/ColumnContext.cs
+++ components/table/ColumnContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AntDesign
@@ -10,6 +11,13 @@ namespace AntDesign
 
         private int CurrentColIndex { get; set; }
 
+        private ITable _table;
+
+        public ColumnContext(ITable table)
+        {
+            _table = table;
+        }
+
         public void AddHeaderColumn(IColumn column)
         {
             if (column == null)
@@ -19,6 +27,40 @@ namespace AntDesign
 
             column.ColIndex = CurrentColIndex++;
             Columns.Add(column);
+        }
+
+        public void AddColGroup(IColumn column)
+        {
+            if (column == null)
+            {
+                return;
+            }
+
+            if (++CurrentColIndex >= Columns.Count)
+            {
+                CurrentColIndex = 0;
+            }
+
+            column.ColIndex = CurrentColIndex;
+
+            if (_table.ScrollX != null && Columns.Any(x => x.Width == null))
+            {
+                var zeroWidthCols = Columns.Where(x => x.Width == null).ToArray();
+                var totalWidth = Columns.Where(x => x.Width != null).Sum(x => ((CssSizeLength)x.Width).Value);
+                foreach (var col in Columns.Where(x => x.Width == null))
+                {
+                    col.Width = $"{(((CssSizeLength)_table.ScrollX).Value - totalWidth + 3) / zeroWidthCols.Length}";
+                }
+            }
+
+            if (column.Width == null)
+            {
+                var col = Columns.FirstOrDefault(x => x.ColIndex == column.ColIndex);
+                if (col != null)
+                {
+                    column.Width = col.Width;
+                }
+            }
         }
 
         public void AddRowColumn(IColumn column)
