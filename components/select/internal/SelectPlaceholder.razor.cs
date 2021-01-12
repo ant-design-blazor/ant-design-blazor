82f09a5d19125f0e95f6cf58e6233ed751b73de7
ElderJames <shunjiey@hotmail.com>
2021-01-12 10:41:40 +0800

    feat: add tree-select

---
+++ components/select/internal/SelectPlaceholder.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class SelectPlaceholder
+    {
+        private static string Prefix => "ant-select";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
