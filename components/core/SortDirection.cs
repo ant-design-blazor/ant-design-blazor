080d4dda43d981c06f55a3c05995940771a2a8e3
cqgis <zhya@cqgis.com>
2021-01-24 13:52:57 +0800

    feat(module: table): add sort directions and default sort order (#778)
    * feat: table  column sort add sortway support singleness or mutiple
    
    * fix: error method name
    
    * add sort directions
    
    * delete useless property
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

--- components/core/SortDirection.cs
+++ components/core/SortDirection.cs
@@ -1,9 +1,36 @@
-﻿namespace AntDesign
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntDesign
 {
-    public enum SortDirection
+    public class SortDirection : EnumValue<SortDirection>
     {
-        None,
-        Ascending,
-        Descending
+        public static readonly SortDirection None;
+
+        public static readonly SortDirection Ascending = new SortDirection("ascend", 1);
+
+        public static readonly SortDirection Descending = new SortDirection("descend", 2);
+
+        public SortDirection(string name, int value) : base(name, value)
+        {
+        }
+
+        public static SortDirection Parse(string typeName)
+        {
+            return typeName switch
+            {
+                "ascend" => Ascending,
+                "descend" => Descending,
+                _ => None
+            };
+        }
+
+        public static class Preset
+        {
+            public static readonly SortDirection[] Default = new[] { Ascending, Descending, None };
+
+            public static readonly SortDirection[] TwoWay = new[] { Ascending, Descending, Ascending };
+        }
     }
 }
