0c13eb71985a048b2f08e1b96054d374ed9bb7ba
James Yeung <shunjiey@hotmail.com>
2021-02-21 21:29:56 -0600

    docs(module: table): add editable cell/row demo (#1152)
    * feat(module: table): add editable cell demo
    
    * add editable row de

--- components/table/Table.razor.cs
+++ components/table/Table.razor.cs
@@ -103,6 +103,9 @@ namespace AntDesign
         [Parameter]
         public SortDirection[] SortDirections { get; set; } = SortDirection.Preset.Default;
 
+        [Parameter]
+        public string TableLayout { get; set; }
+
         [Inject]
         public DomEventService DomEventService { get; set; }
 
@@ -120,8 +123,8 @@ namespace AntDesign
         private bool _hasFixRight;
         private bool _pingRight;
         private bool _pingLeft;
-        private bool _tableLayoutIsFixed;
         private int _treeExpandIconColumnIndex;
+        private string TableLayoutStyle => TableLayout == null ? "" : $"table-layout: {TableLayout};";
 
         private ElementReference _tableHeaderRef;
         private ElementReference _tableBodyRef;
@@ -254,6 +257,11 @@ namespace AntDesign
 
             SetClass();
 
+            if (ScrollX != null || ScrollY != null)
+            {
+                TableLayout = "fixed";
+            }
+
             InitializePagination();
 
             FlushCache();
@@ -336,7 +344,7 @@ namespace AntDesign
 
         void ITable.HasFixRight() => _hasFixRight = true;
 
-        void ITable.TableLayoutIsFixed() => _tableLayoutIsFixed = true;
+        void ITable.TableLayoutIsFixed() => TableLayout = "fixed";
 
         private async void OnResize(JsonElement _) => await SetScrollPositionClassName();
 
@@ -396,7 +404,6 @@ namespace AntDesign
         {
             if (!_isReloading)
             {
-                await this.SetScrollPositionClassName(true);
                 if (ScrollY != null && ScrollX != null)
                 {
                     await JsInvokeAsync(JSInteropConstants.UnbindTableHeaderAndBodyScroll, _tableBodyRef);
