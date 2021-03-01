c043c71447d4a8af98dec386102ba04a1dffa990
zxyao <zxyao145@gmail.com>
2021-03-01 23:45:30 +0800

    fix: animation classs name change of Modal, Message and Notification componments (#1186)
    * fix(module: modal): sync animation class name with official
    
    * fix(module: message): sync animation class name with official
    
    * fix(module: notification): sync animation class name with official

--- components/notification/type/AnimationType.cs
+++ components/notification/type/AnimationType.cs
@@ -6,8 +6,8 @@ namespace AntDesign
 {
     internal static class AnimationType
     {
-        public const string Enter = " ant-notification-fade-enter ant-notification-fade-enter-active";
+        public const string Enter = " ant-notification-fade-enter ant-notification-fade-enter-active ant-notification-fade";
 
-        public const string Leave = " ant-notification-fade-leave ant-notification-fade-leave-active";
+        public const string Leave = " ant-notification-fade-leave ant-notification-fade-leave-active ant-notification-fade";
     }
 }
