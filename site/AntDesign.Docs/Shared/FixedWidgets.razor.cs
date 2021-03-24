e12a2186950f04187ff941d05a33861a8fa841a3
ElderJames <shunjiey@hotmail.com>
2021-03-24 22:42:06 +0800

    feat: add themes

---
+++ site/AntDesign.Docs/Shared/FixedWidgets.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AntDesign.Docs.Localization;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign.Docs.Shared
+{
+    public partial class FixedWidgets
+    {
+        [Inject] public ILanguageService Language { get; set; }
+
+        private void ChangeTheme(string theme)
+        {
+        }
+    }
+}
