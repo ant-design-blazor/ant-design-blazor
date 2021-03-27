d0d1a39d7a26008cdcd6f4aa35ee73e706fd80d5
James Yeung <shunjiey@hotmail.com>
2021-03-27 18:11:41 +0800

    translate the comment

--- components/core/CloseEventArgs.cs
+++ components/core/CloseEventArgs.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace AntDesign
 {
     /// <summary>
-    /// 包含取消属性的事件参数，用于一些连锁事件时，前面的事件可以取消后面事件的触发，比如tag组件的关闭事件
+    /// Can be used to conditionally block closing events
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class CloseEventArgs<T> where T : EventArgs
@@ -20,8 +20,9 @@ namespace AntDesign
         }
 
         public T EventArgs { get; set; }
+
         /// <summary>
-        /// 设置为true时取消后续事件
+        /// If true, the component will be prevented from closing
         /// </summary>
         public bool Cancel { get; set; }
     }
