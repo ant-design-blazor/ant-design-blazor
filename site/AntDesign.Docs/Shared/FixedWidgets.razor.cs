f83450d700dfbbc6c9faeabdee4f2ffa9aa14622
ElderJames <shunjiey@hotmail.com>
2021-03-31 00:43:27 +0800

    add docs theming styles

--- site/AntDesign.Docs/Shared/FixedWidgets.razor.cs
+++ site/AntDesign.Docs/Shared/FixedWidgets.razor.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
 using AntDesign.Docs.Localization;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace AntDesign.Docs.Shared
 {
     public partial class FixedWidgets
     {
-        [Inject] public ILanguageService Language { get; set; }
+        [Inject] private ILanguageService Language { get; set; }
+        [Inject] private IJSRuntime JS { get; set; }
 
-        private void ChangeTheme(string theme)
+        private ElementReference _linkRef;
+
+        private string _themeFileUrl;
+
+        private async Task ChangeTheme(string theme)
         {
+            await JS.InvokeVoidAsync(JSInteropConstants.SetDomAttribute, "body", "data-theme", theme);
+            _themeFileUrl = $"_content/AntDesign.Docs/css/docs.{theme}.css";
+            await JS.InvokeVoidAsync(JSInteropConstants.AddElementToBody, _linkRef);
         }
     }
 }
