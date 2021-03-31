cb852a55bdac91fd4b308c2b12b9b159ddbdc6b8
ElderJames <shunjiey@hotmail.com>
2021-03-31 15:24:02 +0800

    fix docs style

--- site/AntDesign.Docs/Shared/FixedWidgets.razor.cs
+++ site/AntDesign.Docs/Shared/FixedWidgets.razor.cs
@@ -2,11 +2,11 @@
 using AntDesign.Docs.Localization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
 
 namespace AntDesign.Docs.Shared
 {
-    public partial class FixedWidgets : AntDomComponentBase
+    public partial class FixedWidgets
     {
         [Inject] private ILanguageService Language { get; set; }
         [Inject] private IJSRuntime JS { get; set; }
@@ -15,16 +15,16 @@ namespace AntDesign.Docs.Shared
 
         private string _themeFileUrl;
 
-        protected override void OnInitialized()
-        {
-            ClassMapper.Add("fixed-widgets")
-                .If("fixed-widgets-rtl", () => RTL);
-        }
 
         private async Task ChangeTheme(string theme)
         {
-            await JS.InvokeVoidAsync(JSInteropConstants.SetDomAttribute, "body", "data-theme", theme);
             _themeFileUrl = $"_content/AntDesign.Docs/css/docs.{theme}.css";
+
+            await JS.InvokeVoidAsync(JSInteropConstants.SetDomAttribute, "body", new Dictionary<string, string>
+            {
+                ["data-theme"] = theme
+            });
+
             await JS.InvokeVoidAsync(JSInteropConstants.AddElementToBody, _linkRef);
         }
     }
