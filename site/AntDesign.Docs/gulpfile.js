822ce2dd36e198830ba42d4eed3b47df6a1334d8
James Yeung <shunjiey@hotmail.com>
2021-03-31 15:56:06 +0800

    feat: add built-in themes (#1286)
    * feat: add themes
    
    * add docs theming styles
    
    * fix the RTL for FixedWidgets
    
    * fix aliyun theme
    
    * fix docs style

--- site/AntDesign.Docs/gulpfile.js
+++ site/AntDesign.Docs/gulpfile.js
@@ -4,10 +4,34 @@
 
 var rename = require('gulp-rename');
 
-gulp.task("default", function () {
-  return gulp.src('style/index.less')
+gulp.task("less-default", function () {
+  return gulp.src('docs.less')
     .pipe(less({ javascriptEnabled: true }))
     .pipe(cleanCss({ compatibility: 'ie8' }))
-    .pipe(rename('docs.css'))
+    .pipe(rename('docs.default.css'))
     .pipe(gulp.dest('wwwroot/css'));
 });
+
+gulp.task("less-aliyun", function () {
+  return gulp.src('docs.aliyun.less')
+    .pipe(less({ javascriptEnabled: true }))
+    .pipe(cleanCss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task("less-compact", function () {
+  return gulp.src('docs.compact.less')
+    .pipe(less({ javascriptEnabled: true }))
+    .pipe(cleanCss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task("less-dark", function () {
+  return gulp.src('docs.dark.less')
+    .pipe(less({ javascriptEnabled: true }))
+    .pipe(cleanCss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('wwwroot/css'));
+});
+
+
+gulp.task('default', gulp.parallel('less-default', 'less-aliyun', 'less-compact', 'less-dark'), function () { });
