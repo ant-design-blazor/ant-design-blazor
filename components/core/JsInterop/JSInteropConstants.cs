d64388c0abdca49b68babbc21db1d4f8f41fcc90
Andrzej Bakun <andrzej@neelyc.com.cy>
2021-04-13 07:12:18 +0200

    feat(module: select): Select responsive max tag count (#1338)
    * feat(module:select): add MaxTagTextLength, MaxTagCount & MaxTagPlaceHolder
    
    * feat(module:select): responsive maxTagCount using window.resize
    
    * docs(module:select): MaxTagCount functionality
    
    * fix docs
    
    Co-authored-by: ElderJames <shunjiey@hotmail.com>

--- components/core/JsInterop/JSInteropConstants.cs
+++ components/core/JsInterop/JSInteropConstants.cs
@@ -36,6 +36,7 @@ namespace AntDesign
         public static string Log => $"{FUNC_PREFIX}log";
 
         public static string Focus => $"{FUNC_PREFIX}focus";
+        public static string HasFocus => $"{FUNC_PREFIX}hasFocus";
 
         public static string Blur => $"{FUNC_PREFIX}blur";
 
