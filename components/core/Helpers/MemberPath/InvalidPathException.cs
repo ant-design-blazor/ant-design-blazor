3dab937d5f3b6c0beae7ee1da7060898b82083b7
Zonciu Liang <zonciu@zonciu.com>
2021-04-18 11:36:18 +0800

    feat(module: core): add member path helper

---
+++ components/core/Helpers/MemberPath/InvalidPathException.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace AntDesign.Core.Helpers.MemberPath
+{
+    public class InvalidPathException : Exception
+    {
+        public InvalidPathException()
+            : base("Path is invalid")
+        { }
+
+        public InvalidPathException(string? message)
+            : base(message)
+        { }
+    }
+}
---
+++ components/core/Helpers/MemberPath/PathCharState.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace AntDesign.Core.Helpers.MemberPath
+{
+    internal enum PathCharState
+    {
+        Begin,
+
+        MemberName,
+
+        // .
+        Dot,
+
+        // [
+        LeftBracket,
+
+        // ]
+        RightBracket,
+
+        // string key char
+        StringKey,
+
+        // number key char
+        NumberKey,
+
+        // '
+        SingleQuote,
+    }
+}
