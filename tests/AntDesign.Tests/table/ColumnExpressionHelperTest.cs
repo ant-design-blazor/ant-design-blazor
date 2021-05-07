a5a051b79e0d5a5bde6e92dfa5a4c798ca092225
Zonciu Liang <zonciu@zonciu.com>
2021-05-08 01:47:41 +0800

    fix(module: table): exception is thrown when the DataIndex ends with the get_Item method. (#1485)

---
+++ tests/AntDesign.Tests/table/ColumnExpressionHelperTest.cs
@@ -0,0 +1,46 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using AntDesign.core.Helpers;
+using AntDesign.Internal;
+using Xunit;
+
+namespace AntDesign.Tests.Table
+{
+    public class ColumnExpressionHelperTest
+    {
+        [Fact]
+        public void EndsWithGetItem()
+        {
+            var t1 = new T1()
+            {
+                T2 = new()
+                {
+                    Arr = new()
+                    {
+                        5,
+                        6,
+                        7
+                    }
+                }
+            };
+
+            var exp = typeof(T1).BuildAccessPropertyLambdaExpression("T2.Arr[2]");
+
+            var memberInfo = ColumnExpressionHelper.GetReturnMemberInfo(exp);
+            Assert.Equal(nameof(T2.Arr), memberInfo.Name);
+        }
+
+        public class T1
+        {
+            public T2 T2 { get; set; }
+        }
+
+        public class T2
+        {
+            public List<int> Arr { get; set; }
+        }
+    }
+}
