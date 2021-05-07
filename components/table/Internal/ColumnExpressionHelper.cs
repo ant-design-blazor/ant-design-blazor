d98b402b10283530d833644706e1d0c960798c24
Zonciu Liang <zonciu@zonciu.com>
2021-05-08 01:47:41 +0800

    fix(module: table): exception is thrown when the DataIndex ends with the get_Item method. (#1485)

--- components/table/Internal/ColumnExpressionHelper.cs
+++ components/table/Internal/ColumnExpressionHelper.cs
@@ -13,18 +13,31 @@ namespace AntDesign.Internal
         internal static MemberInfo GetReturnMemberInfo(LambdaExpression expression)
         {
             var accessorBody = expression.Body;
-
-            if (accessorBody is ConditionalExpression conditionalExpression)
+            while (true)
             {
-                accessorBody = conditionalExpression.IfTrue;
+                if (accessorBody is UnaryExpression unaryExpression)
+                {
+                    accessorBody = unaryExpression.Operand;
+                }
+                else if (accessorBody is ConditionalExpression conditionalExpression)
+                {
+                    accessorBody = conditionalExpression.IfTrue;
+                }
+                else if (accessorBody is MethodCallExpression methodCallExpression)
+                {
+                    accessorBody = methodCallExpression.Object;
+                }
+                else if (accessorBody is BinaryExpression binaryExpression)
+                {
+                    accessorBody = binaryExpression.Left;
+                }
+                else
+                {
+                    break;
+                }
             }
 
-            if (accessorBody is UnaryExpression unaryExpression && unaryExpression.NodeType == ExpressionType.Convert)
-            {
-                accessorBody = unaryExpression.Operand;
-            }
-
-            if (!(accessorBody is MemberExpression memberExpression))
+            if (accessorBody is not MemberExpression memberExpression)
             {
                 throw new ArgumentException($"The type of the provided expression {accessorBody.GetType().Name} is not supported, it should be {nameof(MemberExpression)}.");
             }
