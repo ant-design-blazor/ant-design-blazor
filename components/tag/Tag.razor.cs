45f5e3377ebe0ba34b2e02f09391a360ac19e04b
Patrick <42546986+MutatePat@users.noreply.github.com>
2021-05-16 17:40:52 +0100

    feat(module: tag): Add custom colors and Enum of Preset Colors (#1514)
    * merge
    
    * feat(tag): add custom colors and enum presets
    
    * Update package.json
    
    fix: remove vscode rubbish
    
    * feat: add global PresetColor, and new PresetColor param
    
    * feat: add preset color to Badge
    
    * feat: tag style calcuated on setter
    
    * fix: user style is appended to stringbuilder
    
    * empty commit
    
    Co-authored-by: Patryk Grzelak <pgrzelak@mutate.app>

--- components/tag/Tag.razor.cs
+++ components/tag/Tag.razor.cs
@@ -2,76 +2,66 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Text;
 
 namespace AntDesign
 {
     public partial class Tag : AntDomComponentBase
     {
-        /// <summary>
-        /// Tag content
-        /// </summary>
         [Parameter]
         public RenderFragment ChildContent { get; set; }
 
-        /// <summary>
-        /// Whether the Tag can be closed
-        /// </summary>
-        [Parameter]
-        public bool Closable { get; set; }
-
-        /// <summary>
-        /// Whether the Tag can be checked
-        /// </summary>
-        [Parameter]
-        public bool Checkable { get; set; }
-
-        /// <summary>
-        /// Checked status of Tag
-        /// </summary>
-        [Parameter]
-        public bool Checked { get; set; }
-
-        /// <summary>
-        /// Callback executed when Tag is checked/unchecked
-        /// </summary>
-        [Parameter]
-        public EventCallback<bool> CheckedChange { get; set; }
-
-        /// <summary>
-        /// Tag color. Can either be a predefined color (string)
-        /// or hex color.
-        /// </summary>
         [Parameter]
         public string Color
         {
             get => _color;
             set
             {
-                if (_color != value)
-                {
-                    _color = value;
-                    _presetColor = IsPresetColor(_color);
-                    if (_presetColor)
-                    {
-                        _style = Style;
-                    }
-                    else
-                    {
-                        _style = $"background-color: {_color};{Style}";
-                    }
-                }
+                _color = value;
+                _isPresetColor = IsPresetColor(_color);
+                _isCustomColor = !_isPresetColor; //if it's not a preset color, we can assume that the input is a HTML5 color or Hex or RGB value  
+                _style = GetStyle();    
             }
         }
 
-        /// <summary>
-        /// Set the tag's icon 
-        /// </summary>
+        [Parameter]
+        public PresetColor? PresetColor 
+        { 
+            get 
+            {
+                object result;
+
+                if (Enum.TryParse(typeof(PresetColor), _color, true, out result) == false) {
+                    return null;
+                }
+            
+                return (PresetColor)result;
+            }
+            set 
+            { 
+                Color = Enum.GetName(typeof(PresetColor), value).ToLowerInvariant();
+            } 
+        }
+
+        [Parameter]
+        public bool Closable { get; set; }
+        
+        [Parameter]
+        public bool Checkable { get; set; }
+
+        [Parameter]
+        public bool Visible { get; set; } = true;
+
+        [Parameter]
+        public bool Checked { get; set; }
+
         [Parameter]
         public string Icon { get; set; }
 
-        /// <summary>
-        /// Callback executed when tag is closed
-        /// </summary>
+        [Parameter]
+        public bool NoAnimation { get; set; }
+
         [Parameter]
         public EventCallback<MouseEventArgs> OnClose { get; set; }
 
@@ -81,21 +71,13 @@ namespace AntDesign
         [Parameter]
         public EventCallback<CloseEventArgs<MouseEventArgs>> OnClosing { get; set; }
 
-        /// <summary>
-        /// Callback executed when tag is clicked (it is not called 
-        /// when closing icon is clicked).
-        /// </summary>
+        [Parameter]
+        public EventCallback<bool> CheckedChange { get; set; }
+
         [Parameter]
         public EventCallback OnClick { get; set; }
-
-        /// <summary>
-        /// Whether the Tag is closed or not
-        /// </summary>
-        [Parameter]
-        public bool Visible { get; set; } = true;
-
-
-        private bool _presetColor;
+        private bool _isPresetColor;
+        private bool _isCustomColor;
         private bool _closed;
         private string _color;
         private string _style;
@@ -113,21 +95,37 @@ namespace AntDesign
                 return false;
             }
 
-            bool result = Regex.IsMatch(color, "^(pink|red|yellow|orange|cyan|green|blue|purple|geekblue|magenta|volcano|gold|lime|success|processing|error|warning|default)(-inverse)?$");
+            bool result = Regex.IsMatch(color, "^(pink|red|yellow|orange|cyan|green|blue|purple|geekblue|magenta|volcano|gold|lime)(-inverse)?$");
             return result;
         }
-        private string _prefix = "ant-tag";
+
         private void UpdateClassMap()
         {
-            this.ClassMapper.Add(_prefix)
-                .If($"{_prefix}-has-color", () => !string.IsNullOrEmpty(Color) && !_presetColor)
-                .If($"{_prefix}-hidden", () => Visible == false)
-                .GetIf(() => $"{_prefix}-{Color}", () => _presetColor)
-                .If($"{_prefix}-checkable", () => Checkable)
-                .If($"{_prefix}-checkable-checked", () => Checked)
-                .If($"{_prefix}-rtl", () => RTL)
-                .If($"{_prefix}-clickable", () => OnClick.HasDelegate)
+            string prefix = "ant-tag";
+            this.ClassMapper.Add(prefix)
+                .If($"{prefix}-has-color", () => _isCustomColor)
+                .If($"{prefix}-hidden", () => Visible == false)
+                .GetIf(() => $"{prefix}-{_color}", () => _isPresetColor)
+                .If($"{prefix}-checkable", () => Checkable)
+                .If($"{prefix}-checkable-checked", () => Checked)
+                .If($"{prefix}-rtl", () => RTL)
                 ;
+        }
+
+        private string GetStyle() {
+            StringBuilder styleBuilder = new StringBuilder();
+
+            styleBuilder.Append(Style);
+
+            if (!string.IsNullOrEmpty(Style) && !Style.EndsWith(";")) {
+                styleBuilder.Append(";");
+            }
+
+            if (_isCustomColor) {
+                styleBuilder.Append($"background-color: {_color};");
+            }
+
+            return styleBuilder.ToString();
         }
 
         private async Task UpdateCheckedStatus()
@@ -158,12 +156,12 @@ namespace AntDesign
                 return;
             }
 
-            this._closed = true;            
-
             if (OnClose.HasDelegate)
             {
                 await this.OnClose.InvokeAsync(e);
             }
+
+            this._closed = true;
         }
 
         private async Task ClickTag(MouseEventArgs e)
