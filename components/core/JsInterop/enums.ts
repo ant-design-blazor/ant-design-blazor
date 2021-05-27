2d693740c67f40fb2508f2af27feafa956e56cc8
Andrzej Bakun <andrzej@neelyc.com.cy>
2021-05-27 12:13:26 +0200

    fix(module: input): fix & feat & docs & tests: Input general fixes (#1530)
    * docs(module:input): api correction & comments
    
    * fix(module:input): clear icon in sync with antD
    
    * fix(module:input): add parameter bordered
    
    * fix(module:textarea): add parameter ShowCounter
    
    * fix(module:input): Focus with behaviors
    
    * docs: add missing docs, rearrange order
    
    * fix(module:search): update to match new standard look
    
    doc(module:search): add API & demos
    
    * fix(module:inputgroup): override style to remove whitespace\
    docs: same as react
    
    * fix(module:inputpassword): support custom icon
    docs: same as react
    
    * fix(module:input): add blur method
    docs: add blur method
    
    * fix(module:input): add readonly parameter
    
    * tests
    
    * fix
    
    * fix(module:input): sequence correction
    
    * Update components/core/Base/AntComponentBase.cs
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>
    
    * review fixes
    
    * fix: clean-up
    
    * tests: fix missing mock
    
    * fix: focus
    
    * fix(module:input): bind clear button to _inputValue
    
    * tests: clear button show/hide
    
    * tests: package update
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/core/JsInterop/enums.ts
@@ -0,0 +1,15 @@
+﻿export enum FocusBehavior {
+  /// <summary>
+  /// When focuses, cursor will move to the last character. 
+  /// This is default behavior.
+  /// </summary>
+  FocusAtLast,
+  /// <summary>
+  /// When focuses, cursor will move to the first character
+  /// </summary>
+  FocusAtFirst,
+  /// <summary>
+  /// When focuses, the content will be selected
+  /// </summary>
+  FocusAndSelectAll
+}
