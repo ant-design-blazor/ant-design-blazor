d366f10d13c072f2a307142e1a99ab2aee47e6ee
anddrzejb <andrzej@neelyc.com.cy>
2021-06-02 14:23:46 +0200

    fix:fix(module:dropdownbutton): add regular button behaviors and properties

--- components/dropdown/Dropdown.razor.cs
+++ components/dropdown/Dropdown.razor.cs
@@ -1,21 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
 using AntDesign.Internal;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace AntDesign
 {
     public partial class Dropdown : OverlayTrigger
     {
+        /// <summary>
+        /// Behave like a button. For `DropdownButton` is always `true`.
+        /// </summary>
         [Parameter]
-        public Func<RenderFragment, RenderFragment, RenderFragment> ButtonsRender { get; set; }
+        public bool IsButton { get; set; } = false;
+
+        internal Func<RenderFragment, RenderFragment, RenderFragment> ButtonsRender { get; set; }
+        internal bool Block { get; set; }
 
         private string _rightButtonIcon = "ellipsis";
         private string _buttonSize = AntSizeLDSType.Default;
-        private string _buttonType = ButtonType.Default;
+        private bool _danger;
+        private bool _ghost;
+        private bool _isLoading;
 
-        private RenderFragment _leftButton;
-        private RenderFragment _rightButton;
-
+        private string _buttonTypeRight = ButtonType.Default;
+        private string _buttonTypeLeft = ButtonType.Default;
         protected void ChangeRightButtonIcon(string icon)
         {
             _rightButtonIcon = icon;
@@ -30,11 +39,41 @@ namespace AntDesign
             StateHasChanged();
         }
 
-        protected void ChangeButtonType(string type)
+        protected void ChangeButtonDanger(bool danger)
         {
-            _buttonType = type;
+            _danger = danger;
 
             StateHasChanged();
         }
+
+        protected void ChangeButtonGhost(bool ghost)
+        {
+            _ghost = ghost;
+
+            StateHasChanged();
+        }
+
+        protected void ChangeButtonLoading(bool isLoading)
+        {
+            _isLoading = isLoading;
+
+            StateHasChanged();
+        }
+
+        protected void ChangeButtonType((string LeftButton, string RightButton) type)
+        {
+            (_buttonTypeLeft, _buttonTypeRight) = type;
+            StateHasChanged();
+        }
+
+        public override async Task OnClickDiv(MouseEventArgs args)
+        {
+            if (!IsButton)
+            {
+                await OnTriggerClick();
+                await OnClick.InvokeAsync(args);
+            }
+        }
+
     }
 }
