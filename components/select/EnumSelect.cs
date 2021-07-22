b0067ebd28335e4988a876daa802c0e2fa76f978
wangjun <wangjun@chemical.ai>
2021-07-22 10:48:09 +0800

    feat(module: select): add EnumSelect for select componet with enum

---
+++ components/select/EnumSelect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace AntDesign
+{
+    public class EnumSelect<TEnum> : Select<TEnum, EnumLookup<TEnum>>
+    {
+        public EnumSelect()
+        {
+            if (typeof(TEnum).IsEnum)
+            {
+                DataSource = Enum.GetValues(typeof(TEnum)).Cast<TEnum>()
+                .Select(t => new EnumLookup<TEnum>
+                {
+                    Value = t,
+                    Label = Enum.GetName(typeof(TEnum), t)
+                })
+                .OrderBy(t => t.Value)
+                .ToList();
+                ValueName = nameof(EnumLookup<TEnum>.Value);
+                LabelName = nameof(EnumLookup<TEnum>.Label);
+            }
+        }
+    }
+
+    public class EnumLookup<TEnum>
+    {
+        public TEnum Value { get; set; }
+
+        public string Label { get; set; }
+    }
+}
