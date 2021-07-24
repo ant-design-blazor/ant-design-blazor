780b765ed86b06fe41ce651b3841b5558f163ca0
zxyao <zxyao145@gmail.com>
2021-07-24 13:24:20 +0800

    chore: add comments for ComponentStatus

--- components/core/ComponentStatus.cs
+++ components/core/ComponentStatus.cs
@@ -13,41 +13,49 @@ namespace AntDesign
 
         /// <summary>
         /// 组件初始化中
+        /// Component initialization in progress
         /// </summary>
         Initing = 1,
 
         /// <summary>
         /// 组件初始化完毕
+        /// Component initialization completed
         /// </summary>
         Inited = 2,
 
         /// <summary>
-        /// 用于在 parameter 属性中设置
+        /// 用于在 parameter 属性中设置，表明组件正在第一次渲染中
+        /// Used to set in the parameter property, Indicates that the component is rendering for the first time
         /// </summary>
         Opening = 4,
 
         /// <summary>
-        /// 一旦打开时经历过一次 OnAfterRenderAsync，就变成此状态
+        /// 一旦打开时经历过一次 OnAfterRender/OnAfterRenderAsync，就变成此状态
+        /// Once the component has experienced OnAfterRender/OnAfterRenderAsync once when it is opened, it becomes this state
         /// </summary>
         Opened = 8,
 
         /// <summary>
-        /// 用于在 parameter 属性中设置
+        /// 用于在 parameter 属性中设置，表明组件正在关闭中
+        /// Used to set in the parameter property, indicating that the component is closing
         /// </summary>
         Closing = 16,
 
         /// <summary>
-        /// 一旦关闭时经历过一次 OnAfterRenderAsync，就变成此状态
+        /// 一旦关闭时经历过一次 OnAfterRender/OnAfterRenderAsync，就变成此状态
+        /// Once OnAfterRender/OnAfterRenderAsync is experienced once when closing, it becomes this state
         /// </summary>
         Closed = 32,
 
         /// <summary>
-        /// 组件销毁中
+        /// 用于在 parameter 属性中设置，表明组件正则销毁中、
+        /// Used to set in the parameter attribute, indicating that the component is in regular destruction
         /// </summary>
         Destroying = 64,
 
         /// <summary>
-        /// 组件销毁完毕
+        /// 一旦销毁时经历过一次 OnAfterRender，就变成此状态
+        /// Once OnAfterRender/OnAfterRenderAsync is experienced once during destruction, it becomes this state
         /// </summary>
         Destroyed = 128
     }
@@ -76,7 +84,7 @@ namespace AntDesign
         }
 
         /// <summary>
-        /// 
+        /// Is not ComponentStatus.Opening and is not ComponentStatus.Opened
         /// </summary>
         /// <param name="componentStatus"></param>
         /// <returns></returns>
@@ -94,6 +102,5 @@ namespace AntDesign
         {
             return componentStatus == ComponentStatus.Closing || componentStatus == ComponentStatus.Closed;
         }
-
     }
 }
