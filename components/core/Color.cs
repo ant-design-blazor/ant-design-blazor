c5cf333b736271ef2a3b668b6bbbf38819c9ecbc
James Yeung <shunjiey@hotmail.com>
2021-07-26 14:53:12 +0800

    separate the color

--- components/core/Color.cs
+++ components/core/Color.cs
@@ -2,9 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+
 namespace AntDesign
 {
-    public enum ButtonColor
+    /// <summary>
+    /// Ant Design Color: https://ant.design/docs/spec/colors
+    /// </summary>
+    public enum Color
     {
         Red1,
         Red2,
