af2a08ee17ed70d74e389e8db89050ec401e8cab
zxyao <zxyao145@gmail.com>
2021-08-21 14:54:03 +0800

    fix: blur active element when comfirm focus element is disabled

--- components/core/JsInterop/modules/components/modalHelper.ts
+++ components/core/JsInterop/modules/components/modalHelper.ts
@@ -3,16 +3,21 @@
 export class modalHelper {
   static focusDialog(selector: string, count: number = 0) {
     let ele = <HTMLElement>document.querySelector(selector);
-    if (ele && !ele.hasAttribute("disabled")) {
-      setTimeout(() => {
-        ele.focus();
-        let curId = "#" + domInfoHelper.getActiveElement();
-        if (curId !== selector) {
-          if (count < 10) {
-            this.focusDialog(selector, count + 1);
+    if (ele) {
+      if (ele.hasAttribute("disabled")) {
+        let htmlElement = <HTMLElement>document.activeElement;
+        htmlElement?.blur();
+      } else {
+        setTimeout(() => {
+          ele.focus();
+          let curId = "#" + domInfoHelper.getActiveElement();
+          if (curId !== selector) {
+            if (count < 10) {
+              this.focusDialog(selector, count + 1);
+            }
           }
-        }
-      }, 10);
+        }, 10);
+      }
     }
   }
 
