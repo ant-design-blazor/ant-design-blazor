b23081f0056d79491e249461f073a283971b9a36
Raffi Abberbock <rabberbock@gmail.com>
2021-09-01 10:38:59 -0400

    Use Min/Max for position
    Call SetStyle OnInitialize so two way binding initializes correctly

--- components/slider/Slider.razor.cs
+++ components/slider/Slider.razor.cs
@@ -166,6 +166,8 @@ namespace AntDesign
         [Parameter]
         public double Min { get; set; } = 0;
 
+        private double MinMaxDelta => Max - Min;
+
         /// <summary>
         /// dual thumb mode
         /// </summary>
@@ -407,6 +409,7 @@ namespace AntDesign
         protected override void OnInitialized()
         {
             base.OnInitialized();
+            SetStyle();
         }
 
         public async override Task SetParametersAsync(ParameterView parameters)
@@ -662,7 +665,7 @@ namespace AntDesign
                     }
                 }
 
-                double rightV = Max * handleNewPosition / sliderLength;
+                double rightV = (MinMaxDelta * handleNewPosition / sliderLength) + Min;
                 if (rightV < LeftValue)
                 {
                     _right = false;
@@ -709,7 +712,7 @@ namespace AntDesign
                     }
                 }
 
-                double leftV = Max * handleNewPosition / sliderLength;
+                double leftV = (MinMaxDelta * handleNewPosition / sliderLength) + Min;
                 if (leftV > RightValue)
                 {
                     _right = true;
@@ -727,11 +730,11 @@ namespace AntDesign
 
         private void SetStyle()
         {
-            _rightHandleStyle = string.Format(CultureInfo.CurrentCulture, RightHandleStyleFormat, Formatter.ToPercentWithoutBlank(RightValue / Max));
+            _rightHandleStyle = string.Format(CultureInfo.CurrentCulture, RightHandleStyleFormat, Formatter.ToPercentWithoutBlank((RightValue - Min) / MinMaxDelta));
             if (Range)
             {
-                _trackStyle = string.Format(CultureInfo.CurrentCulture, TrackStyleFormat, Formatter.ToPercentWithoutBlank(LeftValue / Max), Formatter.ToPercentWithoutBlank((RightValue - LeftValue) / Max));
-                _leftHandleStyle = string.Format(CultureInfo.CurrentCulture, LeftHandleStyleFormat, Formatter.ToPercentWithoutBlank(LeftValue / Max));
+                _trackStyle = string.Format(CultureInfo.CurrentCulture, TrackStyleFormat, Formatter.ToPercentWithoutBlank((LeftValue - Min) / MinMaxDelta), Formatter.ToPercentWithoutBlank((RightValue - LeftValue) / MinMaxDelta));
+                _leftHandleStyle = string.Format(CultureInfo.CurrentCulture, LeftHandleStyleFormat, Formatter.ToPercentWithoutBlank((LeftValue - Min) / MinMaxDelta));
             }
             else
             {
