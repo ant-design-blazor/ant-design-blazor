71b50d7a1a05af455e20dfcbd09886462c02bd30
anranruye <hehewewe@hotmail.com>
2021-10-04 16:18:03 +0800

    feat(module:table): virtualizing

---
+++ components/core/Virtualization/VirtualizeJsInterop.cs
@@ -0,0 +1,74 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#if NET5_0 || NET_6_0
+using System;
+#if NET6_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AntDesign
+{
+    internal class VirtualizeJsInterop : IAsyncDisposable
+    {
+        private const string JsFunctionsPrefix = "Blazor._internal.Virtualize";
+
+        private readonly IVirtualizeJsCallbacks _owner;
+
+        private readonly IJSRuntime _jsRuntime;
+
+        private DotNetObjectReference<VirtualizeJsInterop>? _selfReference;
+
+#if NET6_0_OR_GREATER
+        [DynamicDependency(nameof(OnSpacerBeforeVisible))]
+        [DynamicDependency(nameof(OnSpacerAfterVisible))]
+#endif
+        public VirtualizeJsInterop(IVirtualizeJsCallbacks owner, IJSRuntime jsRuntime)
+        {
+            _owner = owner;
+            _jsRuntime = jsRuntime;
+        }
+
+        public async ValueTask InitializeAsync(ElementReference spacerBefore, ElementReference spacerAfter)
+        {
+            _selfReference = DotNetObjectReference.Create(this);
+            await _jsRuntime.InvokeVoidAsync($"{JsFunctionsPrefix}.init", _selfReference, spacerBefore, spacerAfter);
+        }
+
+        [JSInvokable]
+        public void OnSpacerBeforeVisible(float spacerSize, float spacerSeparation, float containerSize)
+        {
+            _owner.OnBeforeSpacerVisible(spacerSize, spacerSeparation, containerSize);
+        }
+
+        [JSInvokable]
+        public void OnSpacerAfterVisible(float spacerSize, float spacerSeparation, float containerSize)
+        {
+            _owner.OnAfterSpacerVisible(spacerSize, spacerSeparation, containerSize);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_selfReference != null)
+            {
+#if NET6_0_OR_GREATER
+                try
+                {
+                    await _jsRuntime.InvokeVoidAsync($"{JsFunctionsPrefix}.dispose", _selfReference);
+                }
+                catch (JSDisconnectedException)
+                {
+                    // If the browser is gone, we don't need it to clean up any browser-side state
+                }
+#else
+                await _jsRuntime.InvokeVoidAsync($"{JsFunctionsPrefix}.dispose", _selfReference);
+#endif
+            }
+        }
+    }
+}
+#endif
