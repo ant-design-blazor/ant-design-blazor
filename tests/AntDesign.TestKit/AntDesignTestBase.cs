5c2f48f718a8fcf3685c8ee8ab3e87515be7760a
Andrzej Bakun <anddrzejb@poczta.fm>
2021-10-11 16:17:14 +0200

    fix(module: select): arrow down click does not auto close (#1977)
    * fix(module:select): arrow down click does not auto close
    
    * test case added

--- tests/AntDesign.TestKit/AntDesignTestBase.cs
+++ tests/AntDesign.TestKit/AntDesignTestBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Globalization;
+using AntDesign.JsInterop;
 using Bunit;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace AntDesign.Tests
 {
@@ -11,12 +13,14 @@ namespace AntDesign.Tests
         public TestContext Context => this;
         public NavigationManager NavigationManager => Services.GetRequiredService<NavigationManager>();
 
-        public AntDesignTestBase()
+        public Mock<IDomEventListener> MockedDomEventListener { get; set; } = new Mock<IDomEventListener>();
+
+        public AntDesignTestBase(bool useMoq = false)
         {
             Services.AddAntDesign();
 
             //Needed for Tests using Overlay
-            Services.AddScoped<AntDesign.JsInterop.DomEventService>(sp => new TestDomEventService(Context.JSInterop.JSRuntime));
+            Services.AddScoped<AntDesign.JsInterop.DomEventService>(sp => new TestDomEventService(Context.JSInterop.JSRuntime, MockedDomEventListener));
             JSInterop.SetupVoid(JSInteropConstants.OverlayComponentHelper.DeleteOverlayFromContainer, _ => true);
 
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("en-US");
