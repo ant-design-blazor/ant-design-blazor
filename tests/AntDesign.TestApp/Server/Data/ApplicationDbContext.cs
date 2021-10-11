f30303e10abb34e08639723ae87020f9d4f968a9
Guyiming <guyiming2011@126.com>
2021-10-11 23:01:27 +0800

    feat(module: tabs): Add AuthorizeReuseTabsRouteView (#1910)
    * 使MenItem同时支持Icon和IconFont，解决当使用iconfont.cn图标后，缩起内嵌菜单导致按钮样式不统一的问题。
    
    * 空值判断
    
    * add IconTemplate in MenuItem
    
    * code format
    
    * Add AuthorizeReuseTabsRouteView
    
    * add readme.md
    
    * remove comments
    
    * Add AuthorizeResuseTabsRouteView test demo
    
    * save
    
    * save
    
    * done
    
    * refactor the test app
    
    * fix test app
    
    * debug
    
    * debug
    
    * fix tab render
    
    * fix the copyright
    
    * fix package infomation
    
    * fix tab display order
    
    * fix content focerender
    
    Co-authored-by: BG345554 <ymgu@best-inc.com>
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ tests/AntDesign.TestApp/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AntDesign.TestApp.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntDesign.TestApp.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
