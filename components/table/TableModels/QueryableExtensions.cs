a27606fa3bacff73b9d3ac647d0badbdc1eacc43
James Yeung <shunjiey@hotmail.com>
2021-10-29 01:01:00 +0800

    fix(module: table): optimized query API for EF

---
+++ components/table/TableModels/QueryableExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntDesign.TableModels
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<TItem> ExecuteTableQuery<TItem>(this IQueryable<TItem> source, QueryModel<TItem> queryModel)
+        {
+            return queryModel.ExecuteQuery(source);
+        }
+
+        public static IQueryable<TItem> CurrentPagedRecords<TItem>(this IQueryable<TItem> source, QueryModel<TItem> queryModel)
+        {
+            return queryModel.CurrentPagedRecords(source);
+        }
+    }
+}
