d6f235a92385840f63b387a05f37659230d72a66
Hao Sun <54608128+anranruye@users.noreply.github.com>
2021-11-10 19:29:39 +0800

    fix(module: locale-provider): fix culture not found (#2094)
    * fix(module:locale-provider): fix culture-info not found
    
    * Update fallback strategy

--- components/locale-provider/LocaleProvider.cs
+++ components/locale-provider/LocaleProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -32,17 +33,14 @@ namespace AntDesign
 
             foreach (var resource in availableResources.ToArray())
             {
-                var parentCulture = CultureInfo.GetCultureInfo(resource.Key).Parent;
-                var parentCultureName = parentCulture.Name;
-
+                var parentCultureName = GetParentCultureName(resource.Key);
                 while (parentCultureName != string.Empty)
                 {
                     if (!availableResources.ContainsKey(parentCultureName))
                     {
                         availableResources.Add(parentCultureName, resource.Value);
                     }
-                    parentCulture = parentCulture.Parent;
-                    parentCultureName = parentCulture.Name;
+                    parentCultureName = GetParentCultureName(parentCultureName);
                 }
             }
 
@@ -56,20 +54,32 @@ namespace AntDesign
 
         public static Locale GetLocale(string cultureName)
         {
-            return GetLocale(CultureInfo.GetCultureInfo(cultureName));
-        }
-
-        public static Locale GetLocale(CultureInfo cultureInfo)
-        {
-            var cultureName = cultureInfo.Name;
             if (TryGetSpecifiedLocale(cultureName, out Locale locale)) return locale;
             // fallback to parent CultureInfo
-            var parentCulture = cultureInfo.Parent;
-            if (parentCulture.Name != string.Empty)
+            var parentCultureName = GetParentCultureName(cultureName);
+            if (parentCultureName != string.Empty)
             {
-                locale = GetLocale(parentCulture);
-                AddClonedLocale(cultureName, ref locale);
-                return locale;
+                if (TryGetSpecifiedLocale(parentCultureName, out locale))
+                {
+                    AddClonedLocale(cultureName, ref locale);
+                    return locale;
+                }
+                if (cultureName.Count(c => c == '-') == 2)
+                {
+                    var lang = cultureName[..cultureName.IndexOf('-')];
+                    var region = cultureName[cultureName.LastIndexOf('-')..];
+                    if (TryGetSpecifiedLocale(lang + region, out locale))
+                    {
+                        AddClonedLocale(cultureName, ref locale);
+                        return locale;
+                    }
+                }
+                parentCultureName = GetParentCultureName(parentCultureName);
+                if (parentCultureName != string.Empty && TryGetSpecifiedLocale(parentCultureName, out locale))
+                {
+                    AddClonedLocale(cultureName, ref locale);
+                    return locale;
+                }
             }
             // fallback to default language
             if (TryGetSpecifiedLocale(DefaultLanguage, out locale))
@@ -81,6 +91,11 @@ namespace AntDesign
             TryGetSpecifiedLocale("en-US", out locale);
             AddClonedLocale(cultureName, ref locale);
             return locale;
+        }
+
+        public static Locale GetLocale(CultureInfo cultureInfo)
+        {
+            return GetLocale(cultureInfo.Name);
         }
 
         private static void AddClonedLocale(string cultureName, ref Locale locale)
@@ -126,6 +141,20 @@ namespace AntDesign
             if (locale != null)
             {
                 _localeCache.AddOrUpdate(cultureName, locale, (name, original) => locale);
+            }
+        }
+
+        private static string GetParentCultureName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return "";
+            try
+            {
+                return CultureInfo.GetCultureInfo(name).Parent.Name;
+            }
+            catch (Exception)
+            {
+                if (name.Contains("-")) return name[0..name.LastIndexOf("-")];
+                else return "";
             }
         }
     }
