66f2bce3022bda5a71bcf13526406e094e27bd48
anranruye <hehewewe@hotmail.com>
2021-12-05 15:29:22 +0800

    add photino sample

---
+++ site/AntDesign.Docs.Desktop.Photino/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Photino.Blazor;
+
+namespace AntDesign.Docs.Desktop.Photino
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ComponentsDesktop.Run<Startup>("Ant Design of Blazor"
+                , "wwwroot/index.html"
+                , x: 450
+                , y: 100
+                , width: 1000
+                , height: 900);
+        }
+    }
+}
---
+++ site/AntDesign.Docs.Desktop.Photino/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace AntDesign.Docs.Desktop.Photino
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(sp => new HttpClient()
+            {
+                DefaultRequestHeaders =
+                {
+                    // Use to call the github API on server side
+                    {"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36 Edg/81.0.416.68"}
+                }
+            });
+
+            services.AddAntDesignDocs();
+        }
+
+        public void Configure(DesktopApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
