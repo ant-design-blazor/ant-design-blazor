5853db00d42268e65bf6171a6088d95f5eca94b5
zxyao <zxyao145@gmail.com>
2021-12-13 23:15:26 +0800

    chore: code format

--- tests/AntDesign.Tests.Js/tests/domInit.ts
+++ tests/AntDesign.Tests.Js/tests/domInit.ts
@@ -19,14 +19,11 @@ global.Node = window.Node;
 
 export class Guid {
   static newGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-      /[xy]/g,
-      function (c) {
-        var r = (Math.random() * 16) | 0,
-          v = c == 'x' ? r : (r & 0x3) | 0x8;
-        return v.toString(16);
-      }
-    );
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
 
   static randomId(prefix: string = 'id') {
