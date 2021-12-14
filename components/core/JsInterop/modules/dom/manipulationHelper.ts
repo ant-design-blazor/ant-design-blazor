1a1667d4cf198d332f83b6891baa46fea8dba155
zxyao <zxyao145@gmail.com>
2021-12-14 18:04:42 +0800

    fix(module: modal): error width for disable body scroll (#2163)
    * fix: error width for disableBodyScroll
    
    * test: add some test for manipulationHelper
    
    * test: unit test ifx
    
    * fix: code format
    
    * chore: code format

--- components/core/JsInterop/modules/dom/manipulationHelper.ts
+++ components/core/JsInterop/modules/dom/manipulationHelper.ts
@@ -3,6 +3,10 @@ import { styleHelper } from '../styleHelper'
 import { state } from '../stateProvider'
 import * as enums from '../enums';
 
+
+let cachedScrollBarSize: number | undefined = undefined;
+
+
 export class manipulationHelper {
   static addElementToBody(element) {
     document.body.appendChild(element);
@@ -181,10 +185,11 @@ export class manipulationHelper {
       oldBodyCache[key] = body.style[key];
     });
     state.oldBodyCacheStack.push(oldBodyCache);
+    const scrollBarSize = this.getScrollBarSize();
     styleHelper.css(body,
       {
         "position": "relative",
-        "width": this.hasScrollbar() ? "calc(100% - 17px)" : null,
+        "width": scrollBarSize > 0 ? "calc(100% - 17px)" : null,
         "overflow": "hidden"
       });
     styleHelper.addCls(document.body, "ant-scrolling-effect");
@@ -208,4 +213,51 @@ export class manipulationHelper {
     return document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight);
   }
 
+
+  /**
+   * getScrollBarSize
+   * source https://github.com/react-component/util/blob/master/src/getScrollBarSize.tsx
+   * 
+   * @param fresh force get scrollBar size and don't use cache
+   * @returns 
+   */
+   static getScrollBarSize = (fresh: boolean = false) => {
+    if (typeof document === "undefined") {
+      return 0;
+    }
+
+    if (fresh || cachedScrollBarSize === undefined) {
+      const inner = document.createElement("div");
+      inner.style.width = "100%";
+      inner.style.height = "200px";
+
+      const outer = document.createElement("div");
+      const outerStyle = outer.style;
+
+      outerStyle.position = "absolute";
+      outerStyle.top = "0";
+      outerStyle.left = "0";
+      outerStyle.pointerEvents = "none";
+      outerStyle.visibility = "hidden";
+      outerStyle.width = "200px";
+      outerStyle.height = "150px";
+      outerStyle.overflow = "hidden";
+
+      outer.appendChild(inner);
+
+      document.body.appendChild(outer);
+
+      const widthContained = inner.offsetWidth;
+      outer.style.overflow = "scroll";
+      let widthScroll = inner.offsetWidth;
+
+      if (widthContained === widthScroll) {
+        widthScroll = outer.clientWidth;
+      }
+
+      document.body.removeChild(outer);
+      cachedScrollBarSize = widthContained - widthScroll;
+    }
+    return cachedScrollBarSize;
+  };
 }
