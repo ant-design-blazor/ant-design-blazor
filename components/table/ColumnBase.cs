da92efc96b221ef755748cdc1a1410d0bcb51e9f
ElderJames <shunjiey@hotmail.com>
2022-03-04 17:59:13 +0800

    merge 0.10.4 to feature

--- components/table/ColumnBase.cs
+++ components/table/ColumnBase.cs
@@ -82,13 +82,16 @@ namespace AntDesign
             }
         }
 
-        private ColumnAlign _align = ColumnAlign.Left;
+        [Parameter]
+        public virtual RenderFragment<CellData> CellRender { get; set; }
 
         public int ColIndex { get; set; }
 
         protected bool AppendExpandColumn => Table.HasExpandTemplate && ColIndex == (Table.TreeMode ? Table.TreeExpandIconColumnIndex : Table.ExpandIconColumnIndex);
 
         private string _fixedStyle;
+
+        private ColumnAlign _align = ColumnAlign.Left;
 
         protected string FixedStyle => _fixedStyle;
 
