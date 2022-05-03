569c312b35a8399db328a3a3ba943ab32e606ffb
James Yeung <shunjiey@hotmail.com>
2022-05-04 00:23:51 +0800

    docs: add experimental category (#2420)
    * docs: add experimental category
    
    * add en-use doc

--- site/Shared/DemoComponent.cs
+++ site/Shared/DemoComponent.cs
@@ -4,6 +4,7 @@ namespace AntDesign.Docs
 {
     public class DemoComponent
     {
+        public string Category { get; set; }
         public string Title { get; set; }
 
         public string SubTitle { get; set; }
