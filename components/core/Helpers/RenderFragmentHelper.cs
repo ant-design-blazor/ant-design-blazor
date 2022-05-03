75685b0b5762859a540f34b516753cf6a881ec46
b_wind <bofwind@live.com>
2022-05-03 15:59:48 +0800

    remove unnecessary function in RenderFragnentHelper

--- components/core/Helpers/RenderFragmentHelper.cs
+++ components/core/Helpers/RenderFragmentHelper.cs
@@ -9,25 +9,5 @@ namespace AntDesign
     public static class RenderFragmentHelper
     {
         public static RenderFragment ToRenderFragment(this string value) => builder => builder.AddContent(1, value);
-
-        public static RenderFragment CreateChildContent<T>(IDictionary<string, object> extendedProperties = null) where T : IComponent
-        {
-            return Foo;
-            void Foo(RenderTreeBuilder builder)
-            {
-                builder.OpenComponent<T>(0);
-                if (extendedProperties != null)
-                {
-                    int num = 1;
-                    foreach (KeyValuePair<string, object> extendedProperty in extendedProperties)
-                    {
-                        builder.AddAttribute(num++, extendedProperty.Key, extendedProperty.Value);
-                    }
-                }
-
-                builder.CloseComponent();
-            }
-        }
-
     }
 }
