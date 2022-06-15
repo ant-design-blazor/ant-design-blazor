74a1b82fb1c7c0fb32f298cabc0378d636bd6674
rqx110 <rqx110@163.com>
2022-06-15 08:44:27 +0800

    render the icons in the prerendering stage

--- components/icon/Icon.razor.cs
+++ components/icon/Icon.razor.cs
@@ -73,6 +73,8 @@ namespace AntDesign
                 Spin = true;
             }
 
+            await SetupSvgImg();
+
             Button?.Icons.Add(this);
 
             ClassMapper.Add($"anticon")
