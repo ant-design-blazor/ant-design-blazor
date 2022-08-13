156afd71e78d360b97db884768290473815f47e2
WhyILoveSpringRoll <eebbk@163.com>
2022-08-13 12:27:20 +0800

    fix(module: cascader): cannot use allowclear to clear the content when searching (#2610)

--- components/cascader/Cascader.razor.cs
+++ components/cascader/Cascader.razor.cs
@@ -179,13 +179,16 @@ namespace AntDesign
         /// <summary>
         /// 清除已选择项
         /// </summary>
-        private void ClearSelected()
+        private async Task ClearSelected()
         {
             _selectedNodes.Clear();
             _hoverSelectedNodes.Clear();
             _displayText = string.Empty;
             SetValue(string.Empty);
             _dropdownOpened = false;
+            
+            _searchValue = string.Empty;
+            await this.FocusAsync(_inputRef);
         }
 
         /// <summary>
