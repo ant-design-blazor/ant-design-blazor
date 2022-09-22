3dbdfa59ea39eba9a0541b310aacd74656aa4ad0
Alex Kryvdyk <alex.kryvdyk@outlook.com>
2022-09-22 15:16:26 +0300

    fix:(module:datepicker): OnChange event behavior is incorrect

--- components/date-picker/internal/DatePickerStatus.cs
+++ components/date-picker/internal/DatePickerStatus.cs
@@ -6,7 +6,6 @@ namespace AntDesign.Internal
     {
         public string InitPicker { get; set; } = null;
         public bool IsValueSelected { get; set; }
-        public bool IsNewValueSelected { get; set; }
-        public DateTime? OldValue { get; set; }
+        public DateTime? SelectedValue { get; set; }
     }
 }
