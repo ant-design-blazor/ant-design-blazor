8a72a5e4b709f3d77c5d77333aa9499cba55baaf
丁某 <dingyanwu@gmail.com>
2022-11-10 16:00:25 +0800

    refactor mentions

--- components/core/JsInterop/modules/components/mentionsHelper.ts
+++ components/core/JsInterop/modules/components/mentionsHelper.ts
@@ -1,142 +1,73 @@
-﻿import { state } from "../stateProvider";
+﻿export class mentionsHelper {
 
-export class mentionsHelper {
+    private static isPopShowFlag: boolean;
 
-  static getCursorXY(element, objReference) {
-    state.objReferenceDict["mentions"] = objReference;
-    window.addEventListener("click", this.mentionsOnWindowClick);
-
-    const offset = this.getOffset(element);
-
-    return [offset.left, offset.top + offset.height + 14];
-  }
-
-  private static getOffset(elem) {
-    return (new InputCaret(elem)).getOffset();
-  }
-
-  private static mentionsOnWindowClick(e) {
-    let mentionsObj = state.objReferenceDict["mentions"];
-    if (mentionsObj) {
-      mentionsObj.invokeMethodAsync("CloseMentionsDropDown");
-    } else {
-      window.removeEventListener("click", this.mentionsOnWindowClick);
+    public static setPopShowFlag = function (show: boolean): void {
+        mentionsHelper.isPopShowFlag = show;
     }
-  }
 
-}
+    public static setEditorKeyHandler = function (Mentions: any, textArea: HTMLTextAreaElement): void {
 
-class InputCaret {
-  domInputor: any;
-
-  constructor(inputor) {
-    this.domInputor = inputor;
-  }
-
-  getPos = function () {
-    return this.domInputor.selectionStart;
-  };
-
-  getPosition = function (pos) {
-    var domInputor, end_range, format, html, mirror, start_range;
-    domInputor = this.domInputor;
-
-    format = function (value) {
-      value = value.replace(/<|>|`|"|&/g, '?').replace(/\r\n|\r|\n/g, "<br/>");
-      if (/firefox/i.test(navigator.userAgent)) {
-        value = value.replace(/\s/g, '&nbsp;');
-      }
-      return value;
-    };
-    if (!pos) {
-      pos = this.getPos();
+        textArea.onkeydown = async (ev): Promise<any> => {
+            //判断isPopShowFlag不能用异步方法
+            if (!mentionsHelper.isPopShowFlag) return;
+            if (ev.key == "ArrowUp") {
+                ev.preventDefault();
+                await Mentions.invokeMethodAsync("UpOption");
+            } else if (ev.key == "ArrowDown") {
+                ev.preventDefault();
+                await Mentions.invokeMethodAsync("NextOption");
+            }
+            else if (ev.key == "Enter") {
+                ev.preventDefault();
+                await Mentions.invokeMethodAsync("EnterOption");
+            }
+            //其他按键在c#中处理
+        }
     }
-    let inputorValue = domInputor.value;
-    start_range = inputorValue.slice(0, pos);
-    end_range = inputorValue.slice(pos);
-    html = "<span style='position: relative; display: inline;'>" + format(start_range) + "</span>";
-    html += "<span id='caret' style='position: relative; display: inline;'>|</span>";
-    html += "<span style='position: relative; display: inline;'>" + format(end_range) + "</span>";
-    mirror = new Mirror(domInputor);
-    return mirror.create(html).rect();
-  };
 
-  getOffset = function (pos = null) {
-    var offset, position, domInputor;
-    domInputor = this.domInputor;
+    public static getProp = function (e: HTMLElement, propName: string): any {
+        return e[propName];
+    }
 
-    var rect = domInputor.getBoundingClientRect();
+    public static getCursorXY = function (textArea: HTMLTextAreaElement) {
+        let format = function (value) {
+            value = value.replace(/<|>|`|"|&/g, '?').replace(/\r\n|\r|\n/g, "<br/>");
+            if (/firefox/i.test(navigator.userAgent)) {
+                value = value.replace(/\s/g, '&nbsp;');
+            }
+            return value;
+        };
+        let inputorValue = textArea.value;
+        let pos = textArea.selectionStart;
+        let start_range = inputorValue.slice(0, pos);
+        let end_range = inputorValue.slice(pos);
+        let html = format(start_range);
+        html += "<span id='caret' style='position: relative; display: inline;'>@</span>";
+        html += format(end_range);
 
-    offset = {
-      left: rect.left,
-      top: rect.top
+        let div_mirror = document.createElement("div");
+        div_mirror.className = "ant-mentions-measure"
+        div_mirror.style.position = "absolute";
+        div_mirror.style.left = "0";
+        div_mirror.style.top = "0";
+        div_mirror.style.zIndex = "-2000";
+        div_mirror.style.padding = document.defaultView.getComputedStyle(textArea, null).padding;
+        div_mirror.style.width = textArea.offsetWidth + "px";
+
+        div_mirror.innerHTML = html;
+        textArea.parentNode.append(div_mirror);
+
+        let flag: HTMLSpanElement = div_mirror.querySelector("#caret");
+        //  let flagPos = flag.getBoundingClientRect();
+        //  let textAreaPos = textArea.getBoundingClientRect();
+        //  let bodyPos = document.body.getBoundingClientRect();
+        let left = flag.offsetLeft - textArea.scrollLeft;
+        let top = flag.offsetTop - textArea.scrollTop + 16;
+
+        div_mirror.remove();
+        return [left, top];
     };
 
-    position = this.getPosition(pos);
-    return offset = {
-      left: offset.left + position.left - domInputor.scrollLeft,
-      top: offset.top + position.top - domInputor.scrollTop,
-      height: position.height
-    };
-  };
-
-}
-
-class Mirror {
-  domInputor: any;
-  css_attr: any;
-  constructor(inputor) {
-    this.domInputor = inputor;
-    this.css_attr = [];
-  }
-
-  create = function (html) {
-    this.$mirror = document.createElement("div");
-    //TODO: hard coded reference, try to make it more relative
-    (<any>window).AntDesign.interop.styleHelper.css(this.$mirror, this.mirrorCss());
-
-    this.$mirror.innerHTML = html;
-    this.domInputor.parentElement.append(this.$mirror);
-
-    return this;
-  };
-
-  mirrorCss = function () {
-    var css, _this = this;
-    css = {
-      position: 'absolute',
-      left: -9999,
-      top: 0,
-      zIndex: -20000
-    };
-
-    this.css_attr.push('width');
-
-    this.css_attr.forEach((p) => {
-      return css[p] = _this.domInputor.style[p]//_this.$inputor.css(p);
-    })
-
-    return css;
-  };
-
-  rect = function () {
-    var flag, pos, rect;
-    flag = this.$mirror.querySelector("#caret");
-    var oRect = flag.getBoundingClientRect();
-
-    pos = {
-      left: flag.offsetLeft,
-      top: flag.offsetTop
-    }; //$flag.position();
-
-    rect = {
-      left: pos.left,
-      top: pos.top,
-      height: oRect.height
-    };
-
-    this.$mirror.parentElement.removeChild(this.$mirror)
-    return rect;
-  };
 
 }
