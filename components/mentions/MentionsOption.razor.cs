43f9c40fae41f6f0866a67d9239050f10ef7a6a5
丁某 <dingyanwu518@hotmail.com>
2022-11-10 11:13:50 +0800

    重构了Mentions,现在支持自动过滤，上下选择，回车确认。

--- components/mentions/MentionsOption.razor.cs
+++ components/mentions/MentionsOption.razor.cs
@@ -7,63 +7,35 @@ namespace AntDesign
     public partial class MentionsOption
     {
         [CascadingParameter] public Mentions Mentions { get; set; }
-
         [Parameter] public string Value { get; set; }
-
-        [Parameter] public bool Disable { get; set; }
-
         [Parameter] public RenderFragment ChildContent { get; set; }
+        public bool Active => Mentions?.ActiveOptionValue == Value;
 
-        public bool Selected { get; set; }
-
-        public bool Active { get; set; }
-
-        protected override void Dispose(bool disposing)
-        {
-            Mentions?.RemoveOption(this);
-            base.Dispose(disposing);
-        }
-
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
             Mentions?.AddOption(this);
-
             SetClassMap();
-
-            base.OnInitialized();
+            await base.OnInitializedAsync();
         }
 
         private void SetClassMap()
         {
             var prefixCls = "ant-mentions-dropdown-menu-item";
-            this.ClassMapper.Clear()
-                .Add(prefixCls)
-                .If($"{prefixCls}-disable", () => this.Disable)
-                .If($"{prefixCls}-selected", () => this.Selected)
-                .If($"{prefixCls}-active", () => this.Active)
-                ;
-
+            this.ClassMapper.Clear().Add(prefixCls).If("ant-mentions-dropdown-menu-item-active", () => Active);
             InvokeStateHasChanged();
         }
 
-        internal void OnMouseEnter()
+        internal void OnMouseOver()
         {
-            this.Selected = true;
-        }
-
-        internal void OnMouseLeave()
-        {
-            this.Selected = false;
+            Mentions.ActiveOptionValue = Value;
         }
 
         internal async Task OnClick(MouseEventArgs args)
         {
             if (args.Button == 0)   //left click
             {
-                await Mentions.OnOptionClick(this);
+                await Mentions.ItemClick(Value);
             }
-
-            InvokeStateHasChanged();
         }
     }
 }
