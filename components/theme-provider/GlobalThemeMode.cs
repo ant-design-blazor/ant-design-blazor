9db1296348b94f52c57e44f2f354dfb517558339
YiLin <83648656+melinyi@users.noreply.github.com>
2022-11-16 13:59:58 +0800

    Add files via upload

---
+++ components/theme-provider/GlobalThemeMode.cs
@@ -0,0 +1,15 @@
+﻿namespace AntDesign
+{
+    public sealed class GlobalThemeMode : EnumValue<GlobalThemeMode>
+    {
+        public static readonly GlobalThemeMode Light = new GlobalThemeMode(nameof(Light).ToLowerInvariant(), 1);
+        public static readonly GlobalThemeMode Dark = new GlobalThemeMode(nameof(Dark).ToLowerInvariant(), 2);
+        public static readonly GlobalThemeMode Compact = new GlobalThemeMode(nameof(Compact).ToLowerInvariant(), 3);
+        public static readonly GlobalThemeMode Aliyun = new GlobalThemeMode(nameof(Aliyun).ToLowerInvariant(), 4);
+        public static readonly GlobalThemeMode Custom = new GlobalThemeMode(nameof(Custom).ToLowerInvariant(), 5);
+
+        private GlobalThemeMode(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
---
+++ components/theme-provider/GlobalThemeService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AntDesign
+{
+    public class GlobalThemeService
+    {
+        private IJSRuntime _jS;
+        public GlobalThemeService(IJSRuntime js)
+        {
+            _jS = js;
+        }
+
+        public async Task UseCustomTheme(string cssLinkPath, string cssLinkId = "antcss")
+        {
+            if (string.IsNullOrWhiteSpace(cssLinkId)) throw new Exception("未指定Css样式的 CssLinkId");
+            if (string.IsNullOrWhiteSpace(cssLinkPath)) throw new Exception("未指定Css样式的 链接");
+
+            var js = @$"document.getElementById(""{cssLinkId}"").setAttribute(""href"", ""{cssLinkPath}"");";
+            await _jS.InvokeVoidAsync("eval", js);
+        }
+
+        public async Task UseTheme(GlobalThemeMode themeMode, string cssLinkId = "antcss")
+        {
+            switch (themeMode.Name.ToLower())
+            {
+                case "light":
+                    await UseCustomTheme("/_content/AntDesign/css/ant-design-blazor.css", cssLinkId);
+                    break;
+                case "dark":
+                    await UseCustomTheme("/_content/AntDesign/css/ant-design-blazor.dark.css", cssLinkId);
+                    break;
+                case "compact":
+                    await UseCustomTheme("/_content/AntDesign/css/ant-design-blazor.compact.css", cssLinkId);
+                    break;
+                case "aliyun":
+                    await UseCustomTheme("/_content/AntDesign/css/ant-design-blazor.aliyun.css", cssLinkId);
+                    break;
+            }
+        }
+
+    }
+}
