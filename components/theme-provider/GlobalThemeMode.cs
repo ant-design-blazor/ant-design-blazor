be6b7bf84a2af039ee5b95afc507cf5199ddca4b
YiLin <83648656+melinyi@users.noreply.github.com>
2022-11-16 18:56:22 +0800

    Update GlobalThemeMode.cs

--- components/theme-provider/GlobalThemeMode.cs
+++ components/theme-provider/GlobalThemeMode.cs
@@ -6,7 +6,6 @@
         public static readonly GlobalThemeMode Dark = new GlobalThemeMode(nameof(Dark).ToLowerInvariant(), 2);
         public static readonly GlobalThemeMode Compact = new GlobalThemeMode(nameof(Compact).ToLowerInvariant(), 3);
         public static readonly GlobalThemeMode Aliyun = new GlobalThemeMode(nameof(Aliyun).ToLowerInvariant(), 4);
-        public static readonly GlobalThemeMode Custom = new GlobalThemeMode(nameof(Custom).ToLowerInvariant(), 5);
 
         private GlobalThemeMode(string name, int value) : base(name, value)
         {
