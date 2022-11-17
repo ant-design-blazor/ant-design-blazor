3c00e3753b2cf90916574c9c45373d0ac6d44c30
YiLin <83648656+melinyi@users.noreply.github.com>
2022-11-17 15:43:00 +0800

    Update GlobalThemeMode.cs

--- components/theme-provider/GlobalThemeMode.cs
+++ components/theme-provider/GlobalThemeMode.cs
@@ -2,12 +2,12 @@
 {
     public sealed class GlobalThemeMode : EnumValue<GlobalThemeMode>
     {
-        public static readonly GlobalThemeMode Light = new GlobalThemeMode(nameof(Light).ToLowerInvariant(), 1);
-        public static readonly GlobalThemeMode Dark = new GlobalThemeMode(nameof(Dark).ToLowerInvariant(), 2);
-        public static readonly GlobalThemeMode Compact = new GlobalThemeMode(nameof(Compact).ToLowerInvariant(), 3);
-        public static readonly GlobalThemeMode Aliyun = new GlobalThemeMode(nameof(Aliyun).ToLowerInvariant(), 4);
+        public static readonly GlobalThemeMode Light = new GlobalThemeMode(nameof(Light).ToLowerInvariant(), "/_content/AntDesign/css/ant-design-blazor.css");
+        public static readonly GlobalThemeMode Dark = new GlobalThemeMode(nameof(Dark).ToLowerInvariant(), "/_content/AntDesign/css/ant-design-blazor.dark.css");
+        public static readonly GlobalThemeMode Compact = new GlobalThemeMode(nameof(Compact).ToLowerInvariant(), "/_content/AntDesign/css/ant-design-blazor.compact.css");
+        public static readonly GlobalThemeMode Aliyun = new GlobalThemeMode(nameof(Aliyun).ToLowerInvariant(), "/_content/AntDesign/css/ant-design-blazor.aliyun.css");
 
-        private GlobalThemeMode(string name, int value) : base(name, value)
+        private GlobalThemeMode(string name, string value) : base(name, value)
         {
         }
     }
