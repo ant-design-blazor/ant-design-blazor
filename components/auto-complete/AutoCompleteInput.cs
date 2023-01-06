023a543bbdaff7f8aa3a450e4ca8def525a5cb08
James Yeung <shunjiey@hotmail.com>
2023-01-06 17:01:09 +0800

    fix(module: auto-complete): support input composition and debounce (#2988)

--- components/auto-complete/AutoCompleteInput.cs
+++ components/auto-complete/AutoCompleteInput.cs
@@ -37,7 +37,6 @@ namespace AntDesign
             if (Component != null) await Component?.InputFocus(e);
 
             await base.OnFocusAsync(e);
-
         }
 
         protected override async Task OnkeyDownAsync(KeyboardEventArgs args)
@@ -47,14 +46,19 @@ namespace AntDesign
             if (Component != null) await Component?.InputKeyDown(args);
         }
 
-
-        protected override async void OnInputAsync(ChangeEventArgs args)
+        protected override async Task OnInputAsync(ChangeEventArgs args)
         {
-            base.OnInputAsync(args);
+            await base.OnInputAsync(args);
 
             if (Component != null) await Component?.InputInput(args);
         }
 
+        protected override void OnValueChange(TValue value)
+        {
+            base.OnValueChange(value);
+
+            Component?.InputValueChange(value?.ToString());
+        }
 
         #region IAutoCompleteInput
 
@@ -63,6 +67,6 @@ namespace AntDesign
             this.CurrentValue = (TValue)value;
         }
 
-        #endregion
+        #endregion IAutoCompleteInput
     }
 }
