8c4053d10aaf4a41ba62e726e1a7ba198f8a2835
Leon Liu <Leon.Engineer@hotmail.com>
2023-01-17 18:30:20 +0800

    Add ITableGroupModel;

--- components/table/TableModels/QueryModel.cs
+++ components/table/TableModels/QueryModel.cs
@@ -17,10 +17,13 @@ namespace AntDesign.TableModels
 
         public IList<ITableFilterModel> FilterModel { get; private set; }
 
+        public IList<ITableGroupModel> GroupModel { get; private set; }
+
         public QueryModel()
         {
             this.SortModel = new List<ITableSortModel>();
             this.FilterModel = new List<ITableFilterModel>();
+            this.GroupModel = new List<ITableGroupModel>();
         }
 
         internal QueryModel(int pageIndex, int pageSize)
@@ -29,17 +32,19 @@ namespace AntDesign.TableModels
             this.PageIndex = pageIndex;
             this.SortModel = new List<ITableSortModel>();
             this.FilterModel = new List<ITableFilterModel>();
+            this.GroupModel = new List<ITableGroupModel>();
         }
 
 #if NET5_0_OR_GREATER
         [JsonConstructor]
 #endif
-        public QueryModel(int pageIndex, int pageSize, IList<ITableSortModel> sortModel, IList<ITableFilterModel> filterModel)
+        public QueryModel(int pageIndex, int pageSize, IList<ITableSortModel> sortModel, IList<ITableFilterModel> filterModel, IList<ITableGroupModel> groupModel)
         {
             this.PageIndex = pageIndex;
             this.PageSize = pageSize;
             this.SortModel = sortModel;
             this.FilterModel = filterModel;
+            this.GroupModel = groupModel;
         }
     }
 
@@ -52,7 +57,8 @@ namespace AntDesign.TableModels
 #if NET5_0_OR_GREATER
         [JsonConstructor]
 #endif
-        public QueryModel(int pageIndex, int pageSize, IList<ITableSortModel> sortModel, IList<ITableFilterModel> filterModel) : base(pageIndex, pageSize, sortModel, filterModel)
+        public QueryModel(int pageIndex, int pageSize, IList<ITableSortModel> sortModel, IList<ITableFilterModel> filterModel, IList<ITableGroupModel> groupModel) :
+            base(pageIndex, pageSize, sortModel, filterModel, groupModel)
         {
         }
 
@@ -64,6 +70,11 @@ namespace AntDesign.TableModels
         internal void AddFilterModel(ITableFilterModel model)
         {
             FilterModel.Add(model);
+        }
+
+        internal void AddGroupModel(ITableGroupModel model)
+        {
+            GroupModel.Add(model);
         }
 
         public IQueryable<TItem> ExecuteQuery(IQueryable<TItem> query)
@@ -81,13 +92,25 @@ namespace AntDesign.TableModels
             return query;
         }
 
+        public IQueryable<IEnumerable<TItem>> ExecuteGroup(IQueryable<TItem> query)
+        {
+            IQueryable<IEnumerable<TItem>> result;
+            foreach (var group in GroupModel.OrderBy(x => x.Priority))
+            {
+                query = group.GroupList(query);
+            }
+
+            return query;
+        }
+
         public IQueryable<TItem> CurrentPagedRecords(IQueryable<TItem> query) => query.Skip(OffsetRecords).Take(PageSize);
 
         public object Clone()
         {
             var sorters = this.SortModel.Select(x => x.Clone() as ITableSortModel).ToList();
             var filters = this.FilterModel.ToList();
-            return new QueryModel<TItem>(PageIndex, PageSize, sorters, filters);
+            var groups = this.GroupModel.ToList();
+            return new QueryModel<TItem>(PageIndex, PageSize, sorters, filters, groups);
         }
     }
 }
