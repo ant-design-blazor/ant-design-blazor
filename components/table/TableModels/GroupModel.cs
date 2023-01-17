c4b832c16aff514cd34aff1b85a2cad239ff7da1
Leon Liu <Leon.Engineer@hotmail.com>
2023-01-17 18:30:20 +0800

    Add ITableGroupModel;

--- components/table/TableModels/GroupModel.cs
+++ components/table/TableModels/GroupModel.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text.Json.Serialization;
