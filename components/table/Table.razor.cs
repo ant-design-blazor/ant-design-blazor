94e1f9612f2353cc87427049233ebdfffab299fb
Leon <Leon.Engineer@hotmail.com>
2023-01-24 01:24:51 +0800

    Add a apply button;

--- components/table/Table.razor.cs
+++ components/table/Table.razor.cs
@@ -516,10 +516,6 @@ namespace AntDesign
                 .If($"{prefixCls}-wrapper-rtl", () => RTL);
         }
 
-        private void OnSelectedGroupColumnsChange(IEnumerable<ITableGroupModel> groupModels)
-        {
-        }
-
         protected override void OnInitialized()
         {
             base.OnInitialized();
