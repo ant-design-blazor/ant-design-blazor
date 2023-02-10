dac5055c48ca389bf3d5b65828218497597bb9ae
m-khrapunov <93575828+m-khrapunov@users.noreply.github.com>
2023-02-10 13:25:37 +0300

    fix(module: select): no longer triggers OnSelectedItemsChanged (#3079)
    * fix OnSelectedItemsChanged triggers #3072
    
    * fix ValuesChanged invoke twice
    
    ---------
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

--- components/select/SelectBase.cs
+++ components/select/SelectBase.cs
@@ -461,8 +461,11 @@ namespace AntDesign
                 await UpdateOverlayPositionAsync();
             }
 
-            await OnSelectedItemsChanged.InvokeAsync(SelectedOptionItems.Select(s => s.Item));
-            await ValuesChanged.InvokeAsync(Values);
+            if (ValuesChanged.HasDelegate)
+                await ValuesChanged.InvokeAsync(Values);
+
+            if (OnSelectedItemsChanged.HasDelegate)
+                await OnSelectedItemsChanged.InvokeAsync(SelectedOptionItems.Select(s => s.Item));
         }
 
         /// <summary>
@@ -808,16 +811,7 @@ namespace AntDesign
                 }
             }
 
-            _selectedValues = newSelectedValues;
-            if (ValuesChanged.HasDelegate)
-            {
-                await ValuesChanged.InvokeAsync(newSelectedValues);
-            }
-            else
-            {
-                Values = newSelectedValues;
-                StateHasChanged();
-            }
+            Values = newSelectedValues;
         }
 
         protected void ClearSearch()
