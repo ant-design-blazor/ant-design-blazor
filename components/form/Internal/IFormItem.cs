14678b0530e41ce35df1da0800eede284e5270b5
Peter Ha <peter.ha@roininconsulting.net>
2023-08-02 10:14:55 -0700

    changed public to internal

--- components/form/Internal/IFormItem.cs
+++ components/form/Internal/IFormItem.cs
@@ -12,7 +12,7 @@ namespace AntDesign.Internal
 
         public RenderFragment FeedbackIcon { get; }
 
-        public bool IsRequiredByValidation { get; }
+        internal bool IsRequiredByValidation { get; }
 
         internal void AddControl<TValue>(AntInputComponentBase<TValue> control);
 
