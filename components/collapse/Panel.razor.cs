a6f001997c2e6784b9cd0482f81d8e8a8b472b96
James Yeung <shunjiey@hotmail.com>
2023-08-05 00:55:49 +0800

    feat(module: collapse): implement expand animation

--- components/collapse/Panel.razor.cs
+++ components/collapse/Panel.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AntDesign.JsInterop;
 using Microsoft.AspNetCore.Components;
 using OneOf;
 
@@ -7,8 +8,6 @@ namespace AntDesign
 {
     public partial class Panel : AntDomComponentBase
     {
-        #region Parameter
-
         [Parameter]
         public bool Active { get; set; }
 
@@ -36,21 +35,42 @@ namespace AntDesign
         [Parameter]
         public EventCallback<bool> OnActiveChange { get; set; }
 
-        #endregion Parameter
-
         [Parameter]
         public RenderFragment ChildContent { get; set; }
 
         [CascadingParameter]
-        public Collapse Collapse { get; set; }
+        private Collapse Collapse { get; set; }
+
+        private ElementReference _warpperRef;
+        private decimal _warpperHight;
+        private string _warpperStyle;
+
+        private bool _isActive = false;
+        private bool _isInactive = true;
+        private bool _isHidden = true;
+        private bool _isCollapsePrepare;
+        private bool _isCollapseStart;
+        private bool _isCollapseActive;
+
+        private ClassMapper _contentClassMapper = new ClassMapper();
 
         private void SetClassMap()
         {
-            ClassMapper.Clear()
+            ClassMapper
                 .Add("ant-collapse-item")
                 .If("ant-collapse-no-arrow", () => !this.ShowArrow)
                 .If("ant-collapse-item-active", () => this.Active)
                 .If("ant-collapse-item-disabled", () => this.Disabled);
+
+            _contentClassMapper
+                .Add("ant-collapse-content")
+                .If("ant-collapse-content-active", () => _isActive)
+                .If("ant-collapse-content-inactive", () => _isInactive)
+                .If("ant-collapse-content-hidden", () => _isHidden)
+                .If("ant-motion-collapse-enter ant-motion-collapse-enter-prepare ant-motion-collapse", () => _isCollapsePrepare)
+                .If("ant-motion-collapse-enter ant-motion-collapse-enter-start ant-motion-collapse", () => _isCollapseStart)
+                .If("ant-motion-collapse-enter ant-motion-collapse-enter-active ant-motion-collapse", () => _isCollapseActive)
+                ;
         }
 
         protected override async Task OnInitializedAsync()
@@ -58,6 +78,12 @@ namespace AntDesign
             this.Collapse?.AddPanel(this);
             SetClassMap();
             await base.OnInitializedAsync();
+            if (Active)
+            {
+                _isActive = true;
+                _isHidden = false;
+                _isInactive = false;
+            }
         }
 
         private void OnHeaderClick()
@@ -80,7 +106,15 @@ namespace AntDesign
             {
                 this.Active = active;
                 this.OnActiveChange.InvokeAsync(active);
-                StateHasChanged();
+
+                if (Active)
+                {
+                    InvokeAsync(HandleExpand);
+                }
+                else
+                {
+                    InvokeAsync(HandleCollapse);
+                }
             }
         }
 
@@ -97,5 +131,65 @@ namespace AntDesign
         }
 
         public void Toggle() => SetActive(!this.Active);
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                //await UpdateHeight();
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        private async Task UpdateHeight()
+        {
+            var rect = await JsInvokeAsync<HtmlElement>(JSInteropConstants.GetDomInfo, _warpperRef);
+            _warpperHight = rect.ScrollHeight;
+        }
+
+        private async Task HandleExpand()
+        {
+            _isActive = true;
+            _isInactive = false;
+            _isHidden = false;
+            _isCollapsePrepare = true;
+            //StateHasChanged();
+
+            CallAfterRender(async () =>
+            {
+                await UpdateHeight();
+
+                _isCollapsePrepare = false;
+                _isCollapseStart = true;
+                _warpperStyle = "height: 0px; opacity: 0;";
+
+                CallAfterRender(async () =>
+                {
+                    //await Task.Delay(100);
+                    _isCollapseStart = false;
+                    _isCollapseActive = true;
+                    StateHasChanged();
+                    await Task.Delay(100);
+
+                    _warpperStyle = $"height: {_warpperHight}px; opacity: 1;";
+                    StateHasChanged();
+                    await Task.Delay(450);
+
+                    _isCollapseActive = false;
+                    _warpperStyle = "";
+                    StateHasChanged();
+                });
+
+                StateHasChanged();
+            });
+        }
+
+        private async Task HandleCollapse()
+        {
+            _isActive = false;
+            _isInactive = true;
+            _isHidden = true;
+        }
     }
 }
