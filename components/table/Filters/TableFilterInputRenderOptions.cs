a2eb720d55765fdf92f38f1c31a079fd2254528e
m-khrapunov <93575828+m-khrapunov@users.noreply.github.com>
2023-10-19 19:31:04 +0300

    feat(module: table): make filter input focus on dropdown visible (#3450)
    * feat(module: select): add virtualization support (#2654)
    
    * add virtualization support
    
    * fix doc
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>
    
    * add(module: table): filter input focus on dropdown visible
    
    * fix netstandard 2.1 build error
    
    * Revert "fix netstandard 2.1 build error"
    
    This reverts commit 489c50ca9dddc20df3f8c5071d6ef528fedc925b.
    
    * Revert "add(module: table): filter input focus on dropdown visible"
    
    This reverts commit c0589e32a5e7f4f9bf44b59b5348a160ffb8e56a.
    
    * add(module: table): filter input focus on dropdown visible
    
    * clean code format and demo
    
    ---------
    
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

--- components/table/Filters/TableFilterInputRenderOptions.cs
+++ components/table/Filters/TableFilterInputRenderOptions.cs
@@ -3,11 +3,29 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace AntDesign.Filters;
 
-public readonly record struct TableFilterInputRenderOptions(TableFilter Filter, string PopupContainerSelector, string Format)
+public readonly record struct TableFilterInputRenderOptions(TableFilter Filter, string PopupContainerSelector, string Format, Expression InputRefExpression)
 {
     public object Value { get => Filter.Value; set => Filter.Value = value; }
     public TableFilterCompareOperator FilterCompareOperator => Filter.FilterCompareOperator;
+    public object InputRef
+    {
+        get => InputRefExpression is MemberExpression memberExpression &&
+                memberExpression.Member is FieldInfo fieldInfo &&
+                memberExpression.Expression is ConstantExpression constantExpression &&
+                constantExpression.Value is object constantValue ?
+                fieldInfo.GetValue(constantValue) : null;
+        set
+        {
+            if (InputRefExpression is MemberExpression memberExpression &&
+                memberExpression.Member is FieldInfo fieldInfo &&
+                memberExpression.Expression is ConstantExpression constantExpression &&
+                constantExpression.Value is object constantValue)
+                fieldInfo.SetValue(constantValue, value);
+        }
+    }
 }
