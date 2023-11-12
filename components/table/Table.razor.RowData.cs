5088ee60922315ad362abeeadb86ff4f1d0be36c
James Yeung <shunjiey@hotmail.com>
2023-11-13 00:31:56 +0800

    clean up

--- components/table/Table.razor.RowData.cs
+++ components/table/Table.razor.RowData.cs
@@ -23,5 +23,80 @@ namespace AntDesign
 
             _selection?.StateHasChanged();
         }
+
+        private RowData<TItem> GetGroupRowData(IGrouping<object, TItem> grouping, int index, int level, Dictionary<int, RowData<TItem>> rowCache = null)
+        {
+            int rowIndex = index + 1;
+
+            if (level == 0)
+            {
+                rowIndex += PageSize * (PageIndex - 1);
+            }
+
+            var groupRowData = new RowData<TItem>()
+            {
+                Key = grouping.Key.ToString(),
+                IsGrouping = true,
+                RowIndex = rowIndex,
+                DataItem = new TableDataItem<TItem>
+                {
+                    HasChildren = true,
+                    Table = this,
+                    Children = grouping
+                },
+                Children = grouping.Select((data, index) => GetRowData(data, index, level, rowCache)).ToDictionary(x => GetHashCode(x.Data), x => x)
+            };
+
+            return groupRowData;
+        }
+
+        private RowData<TItem> GetRowData(TItem data, int index, int level, Dictionary<int, RowData<TItem>> rowCache = null)
+        {
+            int rowIndex = index + 1;
+
+            if (level == 0)
+            {
+                rowIndex += PageSize * (PageIndex - 1);
+            }
+
+            var dataHashCode = GetHashCode(data);
+
+            if (!_dataSourceCache.TryGetValue(dataHashCode, out var currentDataItem) || currentDataItem == null)
+            {
+                currentDataItem = new TableDataItem<TItem>(data, this);
+                currentDataItem.SetSelected(SelectedRows.Contains(data), triggersSelectedChanged: false);
+                _dataSourceCache.Add(dataHashCode, currentDataItem);
+            }
+
+            // this row cache may be for children rows
+            rowCache ??= _rootRowDataCache;
+
+            if (!rowCache.TryGetValue(dataHashCode, out var currentRowData) || currentRowData == null)
+            {
+                currentRowData = new RowData<TItem>(currentDataItem)
+                {
+                    Expanded = DefaultExpandAllRows && level < DefaultExpandMaxLevel
+                };
+                rowCache.Add(dataHashCode, currentRowData);
+            }
+
+            currentRowData.Level = level;
+            currentRowData.RowIndex = rowIndex;
+            currentRowData.PageIndex = PageIndex;
+
+            if (currentDataItem.HasChildren && (level < DefaultExpandMaxLevel || currentRowData.Expanded))
+            {
+                foreach (var (item, i) in currentDataItem.Children.Select((item, index) => (item, index)))
+                {
+                    currentRowData.Children ??= [];
+                    if (currentRowData.Children.ContainsKey(GetHashCode(item)))
+                        continue;
+
+                    GetRowData(item, i, level + 1, currentRowData.Children);
+                }
+            }
+
+            return currentRowData;
+        }
     }
 }
