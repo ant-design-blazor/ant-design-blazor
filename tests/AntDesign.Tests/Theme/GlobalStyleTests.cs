07a69d4527e503c60630c4a79d7088867c3dae2a
yoli <yoli@chaldea.cn>
2023-12-02 22:55:58 +0800

    fix build error

--- tests/AntDesign.Tests/Theme/GlobalStyleTests.cs
+++ tests/AntDesign.Tests/Theme/GlobalStyleTests.cs
@@ -4,7 +4,7 @@
 
 using System;
 using Bunit;
-using CssInCs;
+using CssInCSharp;
 using Xunit;
 
 namespace AntDesign.Tests.Theme
@@ -20,7 +20,7 @@ namespace AntDesign.Tests.Theme
             var cut = RenderComponent<Style>(parameters => parameters
                 .Add(p => p.HashId, tokenHash.HashId)
                 .Add(p => p.TokenKey, tokenHash.TokenKey)
-                .Add(p => p.Path, new[] { "Shared", "ant" })
+                .Add(p => p.Path, $"{tokenHash.TokenKey}|Shared|ant")
                 .Add(p => p.StyleFn, () => new CSSObject { ["&"] = GlobalStyle.GenLinkStyle(_token) }));
             cut.MarkupMatches(@"<style data-css-hash:ignore data-token-hash:ignore data-cache-path:ignore>:where(.css-dev-only-do-not-override-zcfrx9) a{color:#1677ff;text-decoration:none;background-color:transparent;outline:none;cursor:pointer;transition:color 0.3s;-webkit-text-decoration-skip:objects;}:where(.css-dev-only-do-not-override-zcfrx9) a:hover{color:#69b1ff;}:where(.css-dev-only-do-not-override-zcfrx9) a:active{color:#0958d9;}:where(.css-dev-only-do-not-override-zcfrx9) a:active,:where(.css-dev-only-do-not-override-zcfrx9) a:hover{text-decoration:none;outline:0;}:where(.css-dev-only-do-not-override-zcfrx9) a:focus{text-decoration:none;outline:0;}:where(.css-dev-only-do-not-override-zcfrx9) a[disabled]{color:rgba(0, 0, 0, 0.25);cursor:not-allowed;}</style>");
         }
