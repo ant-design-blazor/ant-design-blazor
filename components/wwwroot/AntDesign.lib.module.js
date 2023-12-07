d6916ba92954bffe5cd9ae89c127f98db8385cbf
James Yeung <shunjiey@hotmail.com>
2023-12-07 13:26:31 +0800

    feat(core): setup js initializers

---
+++ components/wwwroot/AntDesign.lib.module.js
@@ -0,0 +1,33 @@
+﻿var beforeStartCalled = false;
+var afterStartedCalled = false;
+
+export function beforeWebStart() {
+    if (beforeStartCalled) {
+        return;
+    }
+
+    beforeStartCalled = true;
+    var customScript = document.createElement('script');
+    customScript.setAttribute('src', '_content/AntDesign/js/ant-design-blazor.js');
+    document.head.appendChild(customScript);
+
+    var customStyle = document.createElement('link');
+    customStyle.setAttribute('href', '_content/AntDesign/css/ant-design-blazor.css');
+    document.head.appendChild(customStyle);
+}
+
+export function beforeStart(options, extensions) {
+    if (beforeStartCalled) {
+        return;
+    }
+
+    beforeStartCalled = true;
+
+    var customScript = document.createElement('script');
+    customScript.setAttribute('src', '_content/AntDesign/js/ant-design-blazor.js');
+    document.head.appendChild(customScript);
+
+    var customStyle = document.createElement('link');
+    customStyle.setAttribute('href', '_content/AntDesign/css/ant-design-blazor.css');
+    document.head.appendChild(customStyle);
+}
