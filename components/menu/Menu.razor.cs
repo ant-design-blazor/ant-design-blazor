872623347873cae4896a7f94815af9ae28bb2f48
James Yeung <shunjiey@hotmail.com>
2024-01-07 22:03:31 +0800

    fix(module: menu): wrong judge (#3614)

--- components/menu/Menu.razor.cs
+++ components/menu/Menu.razor.cs
@@ -234,7 +234,7 @@ namespace AntDesign
         {
             base.OnInitialized();
 
-            if (InternalMode != MenuMode.Inline && _inlineCollapsed)
+            if (Mode != MenuMode.Inline && _inlineCollapsed)
                 throw new ArgumentException($"{nameof(Menu)} in the {Mode} mode cannot be {nameof(InlineCollapsed)}");
 
             InternalMode = Mode;
