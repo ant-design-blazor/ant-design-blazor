28e2da5b4733e458ec5f277b2d1f3bfff0fc7941
thirking <979339510@qq.com>
2024-02-20 23:50:25 +0800

    fix(module: table): default scrollbar style (#3668)
    * fix default scroll style
    
    * add client dimension service
    
    * update demo
    
    ---------
    
    Co-authored-by: thirking <king2381955@sina.com>
    Co-authored-by: James Yeung <shunjiey@hotmail.com>

---
+++ components/core/Services/ClientDimensionService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace AntDesign.core.Services;
+
+public class ClientDimensionService
+{
+    private double? _scrollBarSize;
+    private readonly IJSRuntime _js;
+
+    public ClientDimensionService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task<double> GetScrollBarSizeAsync()
+    {
+        _scrollBarSize ??= await _js.InvokeAsync<double>(JSInteropConstants.DomMainpulationHelper.GetScrollBarSize, false);
+
+        return _scrollBarSize.Value;
+    }
+}
