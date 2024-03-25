fc983cbef74a6724bdc816a5115e8b4b3ecda505
bgraham123 <121888320+bgraham123@users.noreply.github.com>
2024-03-25 09:32:54 -0400

    refactor: Update TextArea.razor.cs
    Remove TextAreaInfo class from TextArea.razor.cs and define in new file with namespace AntDesign.Internal

--- components/input/TextArea.razor.cs
+++ components/input/TextArea.razor.cs
@@ -325,15 +325,5 @@ namespace AntDesign
                 StateHasChanged();
             }
         }
-
-        public class TextAreaInfo
-        {
-            public double ScrollHeight { get; set; }
-            public double LineHeight { get; set; }
-            public double PaddingTop { get; set; }
-            public double PaddingBottom { get; set; }
-            public double BorderTop { get; set; }
-            public double BorderBottom { get; set; }
-        }
     }
 }
