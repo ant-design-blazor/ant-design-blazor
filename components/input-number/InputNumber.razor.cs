a0552a11c9b065cf96e5f6fb1a969190bd85a1f4
James Yeung <shunjiey@hotmail.com>
2024-06-04 00:33:18 +0800

    feat(module: table): add support custom attributes for filter inupt

--- components/input-number/InputNumber.razor.cs
+++ components/input-number/InputNumber.razor.cs
@@ -6,6 +6,7 @@ using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using AntDesign.Core.Helpers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using OneOf;
@@ -19,6 +20,9 @@ namespace AntDesign
 
         [Parameter]
         public Func<TValue, string> Formatter { get; set; }
+
+        [Parameter]
+        public string Format { get; set; }
 
         [Parameter]
         public Func<string, string> Parser { get; set; }
@@ -508,6 +512,10 @@ namespace AntDesign
             if (Formatter != null)
             {
                 return Formatter(Value);
+            }
+            else if (Format is { Length: > 0 })
+            {
+                return Formatter<TValue>.Format(value, Format);
             }
 
             if (EqualityComparer<TValue>.Default.Equals(value, default))
