4dc471546f2583cebf67a7a9c0c987bea72229b4
James Yeung <shunjiey@hotmail.com>
2024-06-19 22:22:39 +0800

    fix(module: table): create TItem instance with default constructor (#3916)

---
+++ components/core/Reflection/ExpressionActivator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AntDesign.Core.Reflection
+{
+    public static class ExpressionActivator<T>
+    {
+        private static Func<T> _createInstanceFunc;
+
+        static ExpressionActivator()
+        {
+            var constructor = typeof(T).GetConstructor(Type.EmptyTypes);
+            if (constructor == null)
+            {
+                _createInstanceFunc = () => default;
+            }
+
+            var newExpression = Expression.New(constructor);
+            _createInstanceFunc = Expression.Lambda<Func<T>>(newExpression).Compile();
+        }
+
+        public static T CreateInstance()
+        {
+            return _createInstanceFunc();
+        }
+    }
+}
