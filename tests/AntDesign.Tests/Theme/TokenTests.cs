c674d4f263e4c488674f5fbcfd1627e028c8211d
zhaowenkai <799480165@qq.com>
2024-07-18 18:15:07 +0800

    add token tests

--- tests/AntDesign.Tests/Theme/TokenTests.cs
+++ tests/AntDesign.Tests/Theme/TokenTests.cs
@@ -10,12 +10,30 @@ namespace AntDesign.Tests.Theme
     public class TokenTests : AntDesignTestBase
     {
         [Fact]
-        public void Gen_Token_Hash()
+        public void Gen_Default_Theme_Token_Hash()
         {
-            var token = Seed.DefaultSeedToken;
+            var token = Themes.Default.Derivative();
             var tokenHash = token.GetTokenHash(StyleVersion);
             tokenHash.ShouldNotBeNull();
             tokenHash.TokenKey.ShouldBe("18wx8kn");
+        }
+
+        [Fact]
+        public void Gen_Dark_Theme_Token_Hash()
+        {
+            var token = Themes.Dark.Derivative();
+            var tokenHash = token.GetTokenHash(StyleVersion);
+            tokenHash.ShouldNotBeNull();
+            tokenHash.TokenKey.ShouldBe("yhsvyb");
+        }
+
+        [Fact]
+        public void Gen_Compact_Theme_Token_Hash()
+        {
+            var token = Themes.Compact.Derivative();
+            var tokenHash = token.GetTokenHash(StyleVersion);
+            tokenHash.ShouldNotBeNull();
+            tokenHash.TokenKey.ShouldBe("yhsvyb");
         }
     }
 }
