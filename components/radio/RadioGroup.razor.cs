67720e4cde2f7073e1288ff5220bb0690a3db7df
James Yeung <shunjiey@hotmail.com>
2024-07-25 14:01:02 +0800

    fix(module: radio): can't refresh the UI in Modal

--- components/radio/RadioGroup.razor.cs
+++ components/radio/RadioGroup.razor.cs
@@ -147,6 +147,9 @@ namespace AntDesign
                     _ = radio.UnSelect();
                 }
             }
+
+            // refresh ui in modal
+            StateHasChanged();
         }
 
         internal async Task OnRadioChange(TValue value)
