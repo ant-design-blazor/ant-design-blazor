00335ef0aeb055b87288b894d9164b5fa536d646
James Yeung <shunjiey@hotmail.com>
2024-07-27 18:16:53 +0800

    feat(v5): add style generator into AntDesign package

---
+++ components/app/App.razor.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace AntDesign
+{
+    public partial class App : AntContainerComponentBase
+    {
+        [Parameter] public Type Component { get; set; }
+
+        protected override void OnInitialized()
+        {
+            ClassMapper.Add("ant-app");
+
+            base.OnInitialized();
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (Component != null)
+            {
+                builder.OpenComponent(0, Component);
+            }
+            else
+            {
+                builder.OpenElement(0, "div");
+            }
+
+            builder.AddAttribute(1, "class", ClassMapper.Class);
+            builder.AddAttribute(2, "style", Style);
+            builder.AddAttribute(3, "id", Id);
+
+            if (Component != null)
+            {
+                builder.AddAttribute(4, nameof(RefBack), Ref);
+                builder.CloseComponent();
+            }
+            else
+            {
+                builder.AddElementReferenceCapture(4, @ref => Ref = @ref);
+                builder.CloseElement();
+            }
+
+            base.BuildRenderTree(builder);
+        }
+    }
+}
