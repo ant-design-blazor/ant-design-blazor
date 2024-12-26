5d23cb4519385a127a8b2c08c9104c9bee4f4ab5
Arash zandi <ZandiArash@Hotmail.com>
2024-12-26 14:36:04 +0330

    Fixed error when fetching data for the table (dynamic data sample) in local

---
+++ site/AntDesign.Docs/Services/HttpClientHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AntDesign.Docs.Services
+{
+    public class HttpClientCustom : DelegatingHandler
+    {
+        private const string UserAgentValue = "BlazorApp";
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Add the User-Agent header if it's not already present
+            if (!request.Headers.Contains("User-Agent"))
+            {
+                request.Headers.TryAddWithoutValidation("User-Agent", UserAgentValue);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
