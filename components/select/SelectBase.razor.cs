ea6a9d0687c614298194bbaccdb55d1acb4c4c8c
shuangbaojun <190663855@qq.com>
2025-10-29 20:56:56 +0800

    When the Select component is in multiple selection mode, it throws an exception: “Value cannot be null.” #4708

--- components/select/SelectBase.razor.cs
+++ components/select/SelectBase.razor.cs
@@ -349,7 +349,7 @@ namespace AntDesign
                     }
                 }
 
-                if (_isNotifyFieldChanged && Form?.ValidateOnChange == true)
+                if (_isNotifyFieldChanged && Form?.ValidateOnChange == true && FieldIdentifier is { Model: not null, FieldName: not null })
                 {
                     EditContext?.NotifyFieldChanged(FieldIdentifier);
                 }
