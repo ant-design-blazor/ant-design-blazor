44f2b8da38c153673ad5599b039f3f436d3e4d64
ElderJames <shunjiey@hotmail.com>
2026-09-02 18:38:41 +0800

    add more

---
+++ tests/AntDesign.Tests/Core/Helpers/PropertyAccessHelperCoverageTests.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using AntDesign.Core.Helpers;
+using Xunit;
+
+namespace AntDesign.Tests.Core.Helpers
+{
+    public class PropertyAccessHelperCoverageTests
+    {
+        [Fact]
+        public void BuildsNonNullableAccessExpressionsFromTypesAndParameters()
+        {
+            var model = CreateModel();
+
+            var untyped = typeof(TestModel).BuildAccessPropertyLambdaExpression("Child.Name").Compile();
+            var typedFromPath = typeof(TestModel).BuildAccessPropertyLambdaExpression<TestModel, string>("Child.Name").Compile();
+            var typedFromParts = typeof(TestModel).BuildAccessPropertyLambdaExpression<TestModel, int>(new[] { "Point", "X" }).Compile();
+            var parameter = Expression.Parameter(typeof(TestModel));
+            var typedFromParameter = parameter.BuildAccessPropertyLambdaExpression<TestModel, int>(new[] { "OptionalPoint", "X" }).Compile();
+
+            Assert.Equal("nested", untyped.DynamicInvoke(model));
+            Assert.Equal("nested", typedFromPath(model));
+            Assert.Equal(7, typedFromParts(model));
+            Assert.Equal(11, typedFromParameter(model));
+        }
+
+        [Fact]
+        public void BuildsNullableAccessExpressionsForClassValueAndNullableValuePaths()
+        {
+            var model = CreateModel();
+            var missing = new TestModel();
+
+            var untyped = typeof(TestModel).BuildAccessNullablePropertyLambdaExpression("Child.Name").Compile();
+            var typedFromPath = typeof(TestModel).BuildAccessNullablePropertyLambdaExpression<TestModel, int?>("Child.Score").Compile();
+            var typedFromParts = typeof(TestModel).BuildAccessNullablePropertyLambdaExpression<TestModel, int?>(new[] { "OptionalPoint", "X" }).Compile();
+            var parameter = Expression.Parameter(typeof(TestModel));
+            var typedFromParameter = parameter.BuildAccessNullablePropertyLambdaExpression<TestModel, int?>(new[] { "Point", "X" }).Compile();
+
+            Assert.Equal("nested", untyped.DynamicInvoke(model));
+            Assert.Null(untyped.DynamicInvoke(missing));
+            Assert.Equal(23, typedFromPath(model));
+            Assert.Null(typedFromPath(missing));
+            Assert.Equal(11, typedFromParts(model));
+            Assert.Null(typedFromParts(missing));
+            Assert.Equal(7, typedFromParameter(model));
+        }
+
+        [Fact]
+        public void AccessPropertySupportsSeparatorsAndIndexableTypes()
+        {
+            var model = CreateModel();
+
+            Assert.Equal("nested", Compile<TestModel, string>(typeof(TestModel).AccessProperty("Child/Name", "/"))(model));
+            Assert.Equal("one", Compile<TestModel, string>(typeof(TestModel).AccessProperty(new[] { "Names[1]" }))(model));
+            Assert.Equal("zero", Compile<TestModel, string>(typeof(TestModel).AccessProperty("Names[0]"))(model));
+            Assert.Equal("value", Compile<TestModel, string>(typeof(TestModel).AccessProperty("Values[\"key\"]"))(model));
+            Assert.Equal("one", Compile<string[], string>(typeof(string[]).AccessProperty("[1]"))(model.Names));
+            Assert.Equal(7, Compile<TestModel, int>(Expression.Parameter(typeof(TestModel)).AccessProperty("Point.X"))(model));
+            Assert.Throws<InvalidOperationException>(() => typeof(TestModel).AccessProperty("Name[0]"));
+        }
+
+        [Fact]
+        public void AccessNullablePropertyChecksArrayListAndDictionaryBounds()
+        {
+            var model = CreateModel();
+
+            var arrayItem = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Names[1]"));
+            var arrayMissing = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Names[-1]"));
+            var listItem = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Labels[0]"));
+            var listMissing = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Labels[9]"));
+            var dictionaryItem = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Values[\"key\"]"));
+            var dictionaryMissing = Compile<TestModel, string>(typeof(TestModel).AccessNullableProperty("Values[\"missing\"]"));
+            var directArrayItem = Compile<string[], string>(typeof(string[]).AccessNullableProperty("[0]"));
+
+            Assert.Equal("one", arrayItem(model));
+            Assert.Null(arrayMissing(model));
+            Assert.Equal("label", listItem(model));
+            Assert.Null(listMissing(model));
+            Assert.Equal("value", dictionaryItem(model));
+            Assert.Null(dictionaryMissing(model));
+            Assert.Equal("zero", directArrayItem(model.Names));
+            Assert.Throws<InvalidOperationException>(() => typeof(string[]).AccessNullableProperty("[\"key\"]"));
+            Assert.Throws<InvalidOperationException>(() => typeof(List<string>).AccessNullableProperty("[\"key\"]"));
+            Assert.Throws<InvalidOperationException>(() => typeof(TestModel).AccessNullableProperty("Name[0]"));
+        }
+
+        [Fact]
+        public void DefaultIfNullHandlesValueNullableValueAndReferenceProperties()
+        {
+            var model = CreateModel();
+            var missing = new TestModel();
+
+            var valueFromStruct = Compile<TestPoint, int>(typeof(TestPoint).AccessPropertyDefaultIfNull("X", 41));
+            var nullableFromStruct = Compile<TestPoint, int>(typeof(TestPoint).AccessPropertyDefaultIfNull("Optional", 42));
+            var nullableResult = Compile<TestPoint, int?>(typeof(TestPoint).AccessPropertyDefaultIfNull("Optional", (int?)43));
+            var valueFromClass = Compile<TestModel, int>(typeof(TestModel).AccessPropertyDefaultIfNull("Number", 44));
+            var nestedValue = Compile<TestModel, int>(typeof(TestModel).AccessPropertyDefaultIfNull("Child/Score", "/", 45));
+            var reference = Compile<TestModel, string>(typeof(TestModel).AccessPropertyDefaultIfNull("Name", "fallback"));
+
+            Assert.Equal(7, valueFromStruct(model.Point));
+            Assert.Equal(42, nullableFromStruct(model.Point));
+            Assert.Equal(43, nullableResult(model.Point));
+            Assert.Equal(5, valueFromClass(model));
+            Assert.Equal(0, valueFromClass(missing));
+            Assert.Equal(23, nestedValue(model));
+            Assert.Equal(45, nestedValue(missing));
+            Assert.Equal("root", reference(model));
+            Assert.Equal("fallback", reference(missing));
+            Assert.Throws<InvalidOperationException>(() => typeof(TestPoint).AccessPropertyDefaultIfNull("Optional", (long?)1));
+            Assert.Throws<InvalidOperationException>(() => typeof(TestModel).AccessPropertyDefaultIfNull("Name", 1));
+        }
+
+        [Fact]
+        public void ExpressionConversionHelpersFindTheRootParameter()
+        {
+            var parameter = Expression.Parameter(typeof(TestModel));
+            var member = parameter.AccessProperty("Point.X");
+            var nullableMember = parameter.AccessNullableProperty("Child.Score");
+
+            var lambda = member.ToLambdaExpression();
+            var function = member.ToFuncExpression<TestModel, int>().Compile();
+            var nullableDelegate = nullableMember.ToDelegate();
+
+            Assert.Equal(7, lambda.Compile().DynamicInvoke(CreateModel()));
+            Assert.Equal(7, function(CreateModel()));
+            Assert.Equal(23, nullableDelegate.DynamicInvoke(CreateModel()));
+            Assert.Throws<NullReferenceException>(() => Expression.Constant(1).ToLambdaExpression());
+        }
+
+        [Theory]
+        [InlineData("Name]")]
+        [InlineData("Names[]")]
+        [InlineData("Names[abc]")]
+        [InlineData("Names[[0]")]
+        [InlineData("Names[0]]")]
+        public void InvalidIndexPathsAreRejected(string path)
+        {
+            Assert.Throws<InvalidOperationException>(() => typeof(TestModel).AccessProperty(path));
+        }
+
+        [Fact]
+        public void PublicEntryPointsValidateNullAndEmptyArguments()
+        {
+            Type nullType = null!;
+            Expression nullExpression = null!;
+            ParameterExpression nullParameter = null!;
+
+            Assert.Throws<ArgumentNullException>(() => nullType.BuildAccessPropertyLambdaExpression("Name"));
+            Assert.Throws<ArgumentException>(() => typeof(TestModel).BuildAccessPropertyLambdaExpression(Array.Empty<string>()));
+            Assert.Throws<ArgumentNullException>(() => nullParameter.BuildAccessPropertyLambdaExpression<TestModel, string>(new[] { "Name" }));
+            Assert.Throws<ArgumentException>(() => typeof(TestModel).AccessNullableProperty(Array.Empty<string>()));
+            Assert.Throws<ArgumentNullException>(() => nullExpression.AccessNullableProperty("Name"));
+            Assert.Throws<ArgumentException>(() => typeof(TestModel).AccessProperty(Array.Empty<string>()));
+            Assert.Throws<ArgumentNullException>(() => nullExpression.AccessProperty("Name"));
+            Assert.Throws<ArgumentNullException>(() => nullExpression.ToDelegate());
+            Assert.Throws<ArgumentNullException>(() => nullExpression.ToFuncExpression<TestModel, string>());
+        }
+
+        private static Func<TItem, TValue> Compile<TItem, TValue>(Expression body)
+        {
+            var parameterFinder = new ParameterFinder();
+            parameterFinder.Visit(body);
+            return Expression.Lambda<Func<TItem, TValue>>(body, parameterFinder.Parameter!).Compile();
+        }
+
+        private sealed class ParameterFinder : ExpressionVisitor
+        {
+            public ParameterExpression? Parameter { get; private set; }
+
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                Parameter ??= node;
+                return base.VisitParameter(node);
+            }
+        }
+
+        private static TestModel CreateModel()
+        {
+            return new TestModel
+            {
+                Name = "root",
+                Number = 5,
+                Child = new TestChild { Name = "nested", Score = 23 },
+                Point = new TestPoint { X = 7 },
+                OptionalPoint = new TestPoint { X = 11 },
+                Names = new[] { "zero", "one" },
+                Labels = new List<string> { "label" },
+                Values = new Dictionary<string, string> { ["key"] = "value" },
+            };
+        }
+
+        private sealed class TestModel
+        {
+            public string Name { get; set; } = null!;
+
+            public int Number { get; set; }
+
+            public TestChild Child { get; set; } = null!;
+
+            public TestPoint Point { get; set; }
+
+            public TestPoint? OptionalPoint { get; set; }
+
+            public string[] Names { get; set; } = null!;
+
+            public List<string> Labels { get; set; } = null!;
+
+            public Dictionary<string, string> Values { get; set; } = null!;
+        }
+
+        private sealed class TestChild
+        {
+            public string Name { get; set; } = null!;
+
+            public int Score { get; set; }
+        }
+
+        private struct TestPoint
+        {
+            public int X { get; set; }
+
+            public int? Optional { get; set; }
+        }
+    }
+}
---
+++ tests/AntDesign.Tests/Core/JsInterop/DomEventListenerCoverageTests.cs
@@ -0,0 +1,264 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AntDesign.Core.JsInterop.ObservableApi;
+using AntDesign.JsInterop;
+using Bunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Xunit;
+
+namespace AntDesign.Tests.Core.JsInterop;
+
+public sealed class DomEventListenerCoverageTests : TestContext
+{
+    public DomEventListenerCoverageTests()
+    {
+        JSInterop.Mode = JSRuntimeMode.Loose;
+    }
+
+    [Fact]
+    public void AddExclusive_RegistersOnce_ForwardsOptionsAndInvokesCallback()
+    {
+        var listener = CreateListener();
+        string? received = null;
+
+        listener.AddExclusive<string>("#button", "click", value => received = value, true, true);
+        listener.AddExclusive<string>("#button", "click", _ => { });
+
+        var invocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.AddDomEventListener);
+        invocation.Arguments.Take(3).Should().Equal("#button", "click", true);
+        invocation.Arguments[4].Should().Be(true);
+
+        var reference = invocation.Arguments[3].Should().BeOfType<DotNetObjectReference<Invoker<string>>>().Subject;
+        reference.Value.Invoke("payload");
+
+        received.Should().Be("payload");
+    }
+
+    [Fact]
+    public async Task AddExclusive_AsyncCallbackIsAwaited_AndRemoveAllowsRegistrationAgain()
+    {
+        var listener = CreateListener();
+        string? received = null;
+
+        listener.AddExclusive<string>("document", "keydown", async value =>
+        {
+            await Task.Yield();
+            received = value;
+        });
+
+        var addInvocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.AddDomEventListener);
+        var reference = addInvocation.Arguments[3].Should().BeOfType<DotNetObjectReference<AsyncInvoker<string>>>().Subject;
+        await reference.Value.Invoke("Enter");
+
+        listener.RemoveExclusive("document", "keydown");
+        listener.AddExclusive<string>("document", "keydown", _ => { });
+
+        received.Should().Be("Enter");
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.AddDomEventListener).Should().Be(2);
+        var removeInvocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.RemoveDomEventListener);
+        removeInvocation.Arguments.Take(2).Should().Equal("document", "keydown");
+        removeInvocation.Arguments[2].Should().BeSameAs(reference);
+    }
+
+    [Fact]
+    public void AddShared_RegistersOneJsListener_DispatchesCallbacksAndRemovesSubscriptions()
+    {
+        var store = new DomEventSubscriptionStore();
+        var listener = CreateListener(store);
+        int firstValue = 0;
+        int secondValue = 0;
+        Action<int> firstCallback = value => firstValue = value;
+        Action<int> secondCallback = value => secondValue = value;
+
+        listener.AddShared<int>("window", "resize", firstCallback, true);
+        listener.AddShared<int>("window", "resize", secondCallback);
+
+        var invocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.AddDomEventListener);
+        invocation.Arguments.Take(3).Should().Equal("window", "resize", true);
+        var reference = invocation.Arguments[3].Should().BeOfType<DotNetObjectReference<Invoker<string>>>().Subject;
+
+        reference.Value.Invoke("42");
+        firstValue.Should().Be(42);
+        secondValue.Should().Be(42);
+
+        listener.RemoveShared("window", "resize", firstCallback);
+        reference.Value.Invoke("21");
+        firstValue.Should().Be(42);
+        secondValue.Should().Be(21);
+
+        listener.DisposeShared();
+        store.Should().BeEmpty();
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.RemoveDomEventListener).Should().Be(1);
+    }
+
+    [Fact]
+    public async Task AddShared_AsyncOwnerDispatchesAllCallbacks_AndDisposalRemovesJsListener()
+    {
+        var store = new DomEventSubscriptionStore();
+        var listener = CreateListener(store);
+        int syncValue = 0;
+        int asyncValue = 0;
+        Action<int> syncCallback = value => syncValue = value;
+        Func<int, Task> asyncCallback = async value =>
+        {
+            await Task.Yield();
+            asyncValue = value;
+        };
+
+        listener.AddShared<int>("#panel", "scroll", asyncCallback);
+        listener.AddShared<int>("#panel", "scroll", syncCallback);
+
+        var invocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.AddDomEventListener);
+        var reference = invocation.Arguments[3].Should().BeOfType<DotNetObjectReference<AsyncInvoker<string>>>().Subject;
+        await reference.Value.Invoke("7");
+
+        syncValue.Should().Be(7);
+        asyncValue.Should().Be(7);
+
+        listener.RemoveShared("#panel", "scroll", syncCallback);
+        listener.RemoveShared("#panel", "scroll", asyncCallback);
+        listener.AddShared<int>("#panel", "scroll", syncCallback);
+        listener.Dispose();
+        listener.Dispose();
+
+        store.Should().BeEmpty();
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.RemoveDomEventListener).Should().Be(1);
+    }
+
+    [Fact]
+    public void AddEventListenerToFirstChild_DeserializesTypedAndJsonCallbacks()
+    {
+        var listener = CreateListener();
+        TestPayload? typedPayload = null;
+        JsonElement jsonPayload = default;
+
+        listener.AddEventListenerToFirstChild<TestPayload>("#host", "click", value => typedPayload = value, true);
+        listener.AddEventListenerToFirstChild("#other", "change", value => jsonPayload = value);
+        listener.AddEventListenerToFirstChild<TestPayload>("#host", "click", _ => { });
+
+        var invocations = JSInterop.Invocations
+            .Where(x => x.Identifier == JSInteropConstants.AddDomEventListenerToFirstChild)
+            .ToArray();
+        invocations.Should().HaveCount(2);
+        invocations[0].Arguments.Take(3).Should().Equal("#host", "click", true);
+
+        var typedReference = invocations[0].Arguments[3].Should().BeOfType<DotNetObjectReference<Invoker<string>>>().Subject;
+        var jsonReference = invocations[1].Arguments[3].Should().BeOfType<DotNetObjectReference<Invoker<string>>>().Subject;
+        typedReference.Value.Invoke("{\"Value\":9}");
+        jsonReference.Value.Invoke("{\"name\":\"test\"}");
+
+        typedPayload!.Value.Should().Be(9);
+        jsonPayload.GetProperty("name").GetString().Should().Be("test");
+    }
+
+    [Fact]
+    public async Task ResizeObserver_WhenSupported_RegistersDispatchesRemovesAndDisposes()
+    {
+        JSInterop.Setup<bool>(JSInteropConstants.ObserverConstants.Resize.IsResizeObserverSupported).SetResult(true);
+        var store = new DomEventSubscriptionStore();
+        var listener = CreateListener(store);
+        var element = new ElementReference("observed");
+        int syncCount = 0;
+        int asyncCount = 0;
+        Action<List<ResizeObserverEntry>> syncCallback = entries => syncCount = entries.Count;
+        Func<List<ResizeObserverEntry>, Task> asyncCallback = async entries =>
+        {
+            await Task.Yield();
+            asyncCount = entries.Count;
+        };
+
+        await listener.AddResizeObserver(element, asyncCallback);
+        await listener.AddResizeObserver(element, syncCallback);
+
+        var createInvocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Create);
+        var reference = createInvocation.Arguments[1]
+            .Should().BeOfType<DotNetObjectReference<AsyncInvoker<string>>>().Subject;
+        await reference.Value.Invoke("[{}]");
+
+        syncCount.Should().Be(1);
+        asyncCount.Should().Be(1);
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Observe).Should().Be(1);
+
+        await listener.RemoveResizeObserver(element, syncCallback);
+        await listener.RemoveResizeObserver(element, asyncCallback);
+        await listener.AddResizeObserver(element, syncCallback);
+        await listener.DisconnectResizeObserver(element);
+        await listener.AddResizeObserver(element, syncCallback);
+        await listener.DisposeResizeObserver(element);
+
+        store.Should().BeEmpty();
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Disconnect).Should().Be(1);
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Dispose).Should().Be(1);
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.IsResizeObserverSupported).Should().Be(1);
+    }
+
+    [Fact]
+    public async Task ResizeObserver_WhenUnsupported_UsesSharedWindowResizeListener()
+    {
+        JSInterop.Setup<bool>(JSInteropConstants.ObserverConstants.Resize.IsResizeObserverSupported).SetResult(false);
+        var listener = CreateListener();
+        var element = new ElementReference("fallback");
+        int syncCount = 0;
+        int asyncCount = 0;
+
+        await listener.AddResizeObserver(element, entries => syncCount = entries.Count);
+        await listener.AddResizeObserver(element, entries =>
+        {
+            asyncCount = entries.Count;
+            return Task.CompletedTask;
+        });
+
+        var addInvocation = JSInterop.Invocations.Single(x => x.Identifier == JSInteropConstants.AddDomEventListener);
+        addInvocation.Arguments.Take(3).Should().Equal("window", "resize", false);
+        var reference = addInvocation.Arguments[3].Should().BeOfType<DotNetObjectReference<Invoker<string>>>().Subject;
+        reference.Value.Invoke("{}");
+
+        syncCount.Should().Be(1);
+        asyncCount.Should().Be(1);
+
+        await listener.DisconnectResizeObserver(element);
+        await listener.DisposeResizeObserver(element);
+        listener.Dispose();
+
+        JSInterop.Invocations.Should().NotContain(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Disconnect);
+        JSInterop.Invocations.Should().NotContain(x => x.Identifier == JSInteropConstants.ObserverConstants.Resize.Dispose);
+        JSInterop.Invocations.Count(x => x.Identifier == JSInteropConstants.RemoveDomEventListener).Should().Be(1);
+    }
+
+    [Fact]
+    public void DisposeExclusive_RemovesEveryRegisteredListenerOnlyOnce()
+    {
+        var listener = CreateListener();
+        listener.AddExclusive<int>("#one", "click", _ => { });
+        listener.AddExclusive<int>("#two", "focus", _ => { });
+
+        listener.DisposeExclusive();
+        listener.DisposeExclusive();
+
+        var removals = JSInterop.Invocations
+            .Where(x => x.Identifier == JSInteropConstants.RemoveDomEventListener)
+            .ToArray();
+        removals.Should().HaveCount(2);
+        removals.Select(x => (x.Arguments[0], x.Arguments[1])).Should()
+            .BeEquivalentTo(new[] { ((object)"#one", (object)"click"), ("#two", "focus") });
+    }
+
+    private DomEventListener CreateListener(DomEventSubscriptionStore? store = null)
+    {
+        return new DomEventListener(JSInterop.JSRuntime, store ?? new DomEventSubscriptionStore());
+    }
+
+    private sealed class TestPayload
+    {
+        public int Value { get; set; }
+    }
+}
